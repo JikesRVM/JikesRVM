db21f1260bead5136119e0e0771beeb9774cb017
Bowen Alpern <alpernb@us.ibm.com>
2001-10-08 01:32:10 +0000

    javadoc stuff

--- rvm/src/vm/scheduler/VM_ProcessorLock.java
+++ rvm/src/vm/scheduler/VM_ProcessorLock.java
@@ -3,71 +3,94 @@
  */
 //$Id$
 /**
- * @author Bowen Alpern
  *
- * Alternative (to Java monitors) light weight synchronization
- * mechanism to implement thread scheduling (see VM_Processor) and Java
- * monitors (see VM_Lock).  These locks should not be used where Java
- * monitors would suffice.   They are intended to be held only briefly!
+ * <p> Alternative (to Java monitors) light-weight synchronization
+ * mechanism to implement thread scheduling (@link VM_Processor) and
+ * Java monitors (@link VM_Lock).  These locks should not be used
+ * where Java monitors would suffice.  They are intended to be held
+ * only briefly!
  *
- * Acquiring or releasing a lock involves atomically reading and
- * setting the lock's latestContender field.  If this field is null,
- * the lock is unowned.  Otherwise, the field points to the virtual
- * processor that owns the lock, or, if MCS locking is being used, to
- * the last vp on a circular queue of virtual processors spinning until
- * they get the lock, or it is IN_FLUX.  The latter value indicates
- * that the circular spin queue is being updated.
+ * <p> Normally, contending <code>VM_Processor</code>s will spin on
+ * this processor lock's <code>latestContender</code> field.  If
+ * <code>MCS_Locking</code> is set, the processors spin on processor
+ * local data.  This is loosely based on an idea in Mellor-Crummey and
+ * Scott's paper in ASPLOS-IV (1991).  MCS locking is indicated by
+ * setting the preprocessor directive RVM_FOR_MCS_PROCESSOR_LOCKS to
+ * 1.  Possible project: determine those conditions under which MCS
+ * locking performs better than spinning on a global address.
  *
- * Contention is best handled by doing something else.  To support
- * this, tryLock() obtains the lock (and returns true) if the lock is
- * unowned (and there is no spurious microcontention).  Otherwise, it
- * returns false.
+ * <p> Acquiring or releasing a lock involves atomically reading and
+ * setting the lock's <code>latestContender</code> field.  If this
+ * field is null, the lock is unowned.  Otherwise, the field points to
+ * the virtual processor that owns the lock, or, if MCS locking is
+ * being used, to the last vp on a circular queue of virtual
+ * processors spinning until they get the lock, or, if MCS locking is
+ * being used and the circular spin queue is being updated, to
+ * <code>IN_FLUX</code>.
  *
- * Only when "doing something else" is not an attractive option
+ * <p> Contention is best handled by doing something else.  To support
+ * this, <code>tryLock</code> obtains the lock (and returns true) if
+ * the lock is unowned (and there is no spurious microcontention).
+ * Otherwise, it returns false.
+ *
+ * <p> Only when "doing something else" is not an attractive option
  * (locking global thread queues, unlocking a thick lock with threads
  * waiting, avoiding starvation on a thread that has issued several
  * tryLocks, etc.) should lock() be called.  Here, any remaining
- * contention is handled by spinning on a local flag.  This is loosely
- * based on an idea in Mellor-Crummey and Scott's paper in ASPLOS-IV
- * (1991).
+ * contention is handled by spinning on a local flag.
  *
- * To add itself to the circular waiting queue, a processor must
+ * <p> To add itself to the circular waiting queue, a processor must
  * succeed in setting the latestContender field to IN_FLUX.  A backoff
- * strategy is used to reduce contention for this field.  This strategy
- * has both a pseudo-random (to prevent two or more virtual processors
- * from backing off in lock step) and an exponential component (to deal
- * with really high contention).
+ * strategy is used to reduce contention for this field.  This
+ * strategy has both a pseudo-random (to prevent two or more virtual
+ * processors from backing off in lock step) and an exponential
+ * component (to deal with really high contention).
  *
- * Releasing a lock entails either atomically setting the latestContender
- * field to null (if this processor is the latestContender), or releasing
- * the first virtual processor on the circular spin queue.  In the
- * latter case, the latestContender field must be set to IN_FLUX.  To
- * give unlock() priority over lock(), the backoff strategy is not used
- * for unlocking: if a vp fails to set set the field to IN_FLUX, it
- * tries again immediately.
+ * <p> Releasing a lock entails either atomically setting the
+ * latestContender field to null (if this processor is the
+ * latestContender), or releasing the first virtual processor on the
+ * circular spin queue.  In the latter case, the latestContender field
+ * must be set to IN_FLUX.  To give unlock() priority over lock(), the
+ * backoff strategy is not used for unlocking: if a vp fails to set
+ * set the field to IN_FLUX, it tries again immediately.
  *
- * Usage: system locks should only be used when synchronized methods
- * cannot.  Do not allocate anything, trigger a type cast, or call any
- * method that might allow a thread switch between lock and unlock.  
+ * <p> Usage: system locks should only be used when synchronized
+ * methods cannot.  Do not do anything, (such as trigger a type cast,
+ * allocate an object, or call any method of a class that does not
+ * implement VM_Uninterruptible) that might allow a thread switch or
+ * trigger a garbage collection between lock and unlock.
  *
- */
-
+ * @author Bowen Alpern
+ * @see VM_Processor
+ * @see VM_Lock */
 public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible {
 
+//-#if RVM_FOR_MCS_PROCESSOR_LOCKS
   /**
-   * The state of the processor lock:
-   *    null, if the lock is not owned;
-   *    the processor that owns the lock, if no processors are waithing;
-   *    the last in a circular chain of processors waiting to own the lock; or
-   *    IN_FLUX, if the circular chain is being edited.
-   *
-   * Only the first two states are possible unless VM.BuildForMCSProcessorLocks
-   * is true.
+   * Contending <code>VM_Processor</code>s spin on processor local addresses.
+   */
+  public static final boolean MCS_Locking = true;
+//-#else
+  /**
+   * Contending <code>VM_Processor</code>s spin on a globally shared address.
+   */
+  public static final boolean MCS_Locking = false;
+//-#endif
+  
+  /**
+   * The state of the processor lock.
+   * <ul>
+   * <li> <code>null</code>, if the lock is not owned;
+   * <li> the processor that owns the lock, if no processors are waithing;
+   * <li> the last in a circular chain of processors waiting to own the lock; or
+   * <li> <code>IN_FLUX</code>, if the circular chain is being edited.
+   * </ul>
+   * Only the first two states are possible unless MCS locking is implemented.
    */
   VM_Processor latestContender;
 
   /**
-   * Aquire a processor lock.
+   * Acquire a processor lock.
    */
   public void lock () {
     if (VM.BuildForSingleVirtualProcessor) return;
@@ -85,7 +108,7 @@ public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible 
 	} else {
 	  continue; // don't handle contention
 	}
-      } else if (VM.BuildForMCSProcessorLocks && VM_Magic.objectAsAddress(p) != IN_FLUX) { // lock is owned, but not being changed
+      } else if (MCS_Locking && VM_Magic.objectAsAddress(p) != IN_FLUX) { // lock is owned, but not being changed
 	if (VM_Magic.attempt(this, VM_Entrypoints.latestContenderOffset, VM_Magic.objectAsAddress(p), IN_FLUX)) {
 	  VM_Magic.isync(); // so subsequent instructions wont see stale values
 	  break; 
@@ -94,7 +117,7 @@ public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible 
       handleMicrocontention(attempts++);
     } while (true);
     // i owns the lock
-    if (VM.VerifyAssertions && !VM.BuildForMCSProcessorLocks) VM_Scheduler.assert(VM.NOT_REACHED);
+    if (VM.VerifyAssertions && !MCS_Locking) VM_Scheduler.assert(VM.NOT_REACHED);
     i.awaitingProcessorLock = this;
     if (p.awaitingProcessorLock != this) { // make i first (and only) waiter on the contender chain
       i.contenderLink = i;
@@ -110,7 +133,8 @@ public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible 
   }
 
   /**
-   * Conditionally aquire a processor lock.
+   * Conditionally acquire a processor lock.
+   * @return whether acquisition succeeded
    */
   boolean tryLock () {
     if (VM.BuildForSingleVirtualProcessor) return true;
@@ -129,9 +153,9 @@ public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible 
    */
   public void unlock () {
     if (VM.BuildForSingleVirtualProcessor) return;
-    VM_Magic.sync(); // commit changes while lock was held so they are visiable to the next processor that aquires the lock
+    VM_Magic.sync(); // commit changes while lock was held so they are visiable to the next processor that acquires the lock
     VM_Processor i = VM_Processor.getCurrentProcessor();
-    if (!VM.BuildForMCSProcessorLocks) {
+    if (!MCS_Locking) {
       if (VM.VerifyAssertions) i.lockCount -= 1;
       VM_Magic.setIntAtOffset(this, VM_Entrypoints.latestContenderOffset, 0);  // latestContender = null;
       return;
@@ -170,30 +194,37 @@ public final class VM_ProcessorLock implements VM_Constants, VM_Uninterruptible 
   }
 
   /**
-   * A deprecated synonym for unlock (was to avoid the "sync" of unlock)
+   * A synonym for unlock.
+   * @depricated (was to avoid the "sync" of unlock)
    */
   void release () { unlock(); }
 
   /**
-   * An attempt to lock or unlock a processor lock has failed, 
-   * presumably due to contention with another processor.  Here 
-   * we backoff a little to increase the likelyhood that a 
-   * subsequent retry will succeed.
+   * An attempt to lock or unlock a processor lock has failed,
+   * presumably due to contention with another processor.  Backoff a
+   * little to increase the likelihood that a subsequent retry will
+   * succeed.
    */
   private static void handleMicrocontention (int n) {
-    if (n < 0) return; // method call overhead is delay enough
+    VM_Magic.pragmaNoInline();
+    if (n <= 0) return;                                  // method call overhead is delay enough
     int pid    =  VM_Processor.getCurrentProcessorId();
     if (pid < 0) pid = - pid;                            // native processors have negative ids
     delayIndex = (delayIndex + pid) % delayCount.length;
     int delay  = delayCount[delayIndex]*delayMultiplier; // pseudorandom backoff component
-    delay     += delayBase<<n;                           // exponential backoff component
-    for (int i = delay; --i > 0; ) ; // delay a different amount of time on each processor
+    delay     += delayBase << (n-1);                     // exponential backoff component
+    for (int i=delay; i>0; i--) ;                        // delay a different amount of time on each processor
   }
 
   private static final int   delayMultiplier = 10;
   private static final int   delayBase       = 64;
   private static       int   delayIndex;
   private static       int[] delayCount = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-  private static final int   IN_FLUX = -1; // for MCS locks, indicates the value of the lock is changing
+
+  /**
+   * For MCS locking, indicates that another processor is changing the
+   * state of the circular waiting queue.
+   */
+  private static final int   IN_FLUX = -1;
 
 }
