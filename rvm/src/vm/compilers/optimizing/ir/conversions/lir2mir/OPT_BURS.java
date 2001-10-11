c208027e093e5686b9623a24551b8fcae1310e00
Dave Grove <groved@us.ibm.com>
2001-10-11 18:31:35 +0000

    add mechanism to control evaluation order of children

--- rvm/src/vm/compilers/optimizing/ir/conversions/lir2mir/OPT_BURS.java
+++ rvm/src/vm/compilers/optimizing/ir/conversions/lir2mir/OPT_BURS.java
@@ -343,13 +343,32 @@ final class OPT_BURS implements OPT_Operators {
       if (child2 != null && !child2.isTreeRoot()) {
 	// k has two children; use register labeling to
 	// determine order that minimizes register pressure
-	if (child2.numRegisters() > child1.numRegisters()) {
-	  generateTree(child2, burs);
-	  generateTree(child1, burs);
+	if (k.isSuperNodeRoot()) {
+	  byte act = burs.action[k.rule(k.getNonTerminal())];
+	  if ((act & burs.LEFT_CHILD_FIRST) != 0) {
+	    // rule selected forces order of evaluation
+	    generateTree(child1, burs);
+	    generateTree(child2, burs);
+	  } else if ((act & burs.RIGHT_CHILD_FIRST) != 0) {
+	    // rule selected forces order of evaluation
+	    generateTree(child2, burs);
+	    generateTree(child1, burs);
+	  } else if (child2.numRegisters() > child1.numRegisters()) {
+	    generateTree(child2, burs);
+	    generateTree(child1, burs);
+	  } else {
+	    generateTree(child1, burs);
+	    generateTree(child2, burs);
+	  }
 	} else {
-	  generateTree(child1, burs);
-	  generateTree(child2, burs);
-	} 
+	  if (child2.numRegisters() > child1.numRegisters()) {
+	    generateTree(child2, burs);
+	    generateTree(child1, burs);
+	  } else {
+	    generateTree(child1, burs);
+	    generateTree(child2, burs);
+	  } 
+	}
       } else {
 	generateTree(child1, burs);
       }
