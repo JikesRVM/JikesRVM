4cdc5990b070e99e00d648a7f7ffa7681c7d4b89
Stephen Fink <sjfink@us.ibm.com>
2001-10-18 20:39:35 +0000

    conditional move transformation

--- rvm/src/vm/compilers/optimizing/optimizations/control/OPT_BranchOptimizations.java
+++ rvm/src/vm/compilers/optimizing/optimizations/control/OPT_BranchOptimizations.java
@@ -4,6 +4,9 @@
 //$Id$
 
 import instructionFormats.*;
+import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 /**
  * Perform simple peephole optimizations for branches.
@@ -16,6 +19,10 @@ public final class OPT_BranchOptimizations
   extends OPT_BranchOptimizationDriver {
 
   private OPT_BranchOptimizations () { }
+
+  public boolean printingEnabled(OPT_Options options, boolean before) {
+    return false;
+  }
 
   /** 
    * @param level the minimum optimization level at which the branch 
@@ -113,8 +120,7 @@ public final class OPT_BranchOptimizations
       bb.recomputeNormalOut(ir); // fix the CFG 
       return true;
     }
-    if (!_restrictCondBranchOpts &&
-	IfCmp.conforms(targetInst)) {
+    if (!_restrictCondBranchOpts && IfCmp.conforms(targetInst)) {
       // unconditional branch to a conditional branch.
       // If the Goto is the only branch instruction in its basic block
       // and the IfCmp is the only non-GOTO branch instruction
@@ -159,12 +165,13 @@ public final class OPT_BranchOptimizations
    *	  GOTO B			   A: ...
    *   A: ...
    * 4) special case to generate Boolean compare opcode
-   * 5)   IF .. GOTO A	     replaced by  IF .. GOTO B
+   * 5) special case to generate conditional move sequence
+   * 6)   IF .. GOTO A	     replaced by  IF .. GOTO B
    *   A: LABEL	     
    *   	  BBEND
    *   B: 
+   * 7)  fallthrough to a goto: replicate goto to enable other optimizations.
    * </pre>
-   * 6)  fallthrough to a goto: replicate goto to enable other optimizations.
    *
    * <p> Precondition: IfCmp.conforms(cb)
    *
@@ -204,6 +211,11 @@ public final class OPT_BranchOptimizations
     // attempt to generate boolean compare.
     if (generateBooleanCompare(ir, bb, cb, targetBlock)) {
       // generateBooleanCompare does all necessary CFG fixup.
+      return true;
+    }
+    // attempt to generate a sequence using conditional moves
+    if (generateCondMove(ir, bb, cb)) {
+      // generateCondMove does all necessary CFG fixup.
       return true;
     }
     if (Goto.conforms(targetInst)) {
@@ -411,6 +423,320 @@ public final class OPT_BranchOptimizations
   }
 
   /**
+   * Attempt to generate a straight-line sequence using conditional move
+   * instructions, to replace a diamond control flow structure.
+   *
+   * <p>Suppose we have the following code, where e{n} is an expression:
+   * <pre>
+   * if (a op b) {
+   *   x = e2;
+   *   y = e3;
+   * } else {
+   *   z = e4;
+   *   x = e5;
+   * }
+   * </pre>
+   * We would transform this to:
+   * <pre>
+   * t1 = a;
+   * t2 = b;
+   * t3 = e2;
+   * t4 = e3;
+   * t5 = e4;
+   * t6 = e5;
+   * COND MOVE [if (t1 op t2) x := t3 else x := t6 ];
+   * COND MOVE [if (t1 op t2) y := t4 else y := y];
+   * COND MOVE [if (t1 op t2) z := z  else z := t5];
+   * </pre>
+   *
+   * <p>Note that we rely on other optimizations (eg. copy propagation) to 
+   * clean up some of this unnecessary mess.
+   *
+   * <p>Note that in this example, we've increased the shortest path by 2
+   * expression evaluations, 2 moves, and 3 cond moves, but eliminated one 
+   * conditional branch.
+   *
+   * <p>We apply a cost heuristic to guide this transformation: 
+   * We will eliminate a conditional branch iff it increases the shortest
+   * path by no more than 'k' operations.  Currently, we count each 
+   * instruction (alu, move, or cond move) as 1 evaluation.
+   * The parameter k is specified by OPT\_Options.COND_MOVE_CUTOFF.
+   *
+   * <p> In the example above, since we've increased the shortest path by
+   * 6 instructions, we will only perform the transformation if k >= 7.
+   *
+   * <p> TODO items
+   * <ul>
+   * <li> consider smarter cost heuristics
+   * <li> enhance downstream code generation to avoid redundant evaluation
+   * of condition codes.
+   * </ul>
+   *
+   * @param ir governing IR
+   * @param bb basic block of cb
+   * @param cb conditional branch instruction
+   * @returns true if the transformation succeeds, false otherwise
+   */
+  private boolean generateCondMove(OPT_IR ir, OPT_BasicBlock bb, 
+                                   OPT_Instruction cb) {
+    if (!VM.BuildForIA32) return false;
+    if (!IfCmp.conforms(cb)) return false;
+
+    // see if bb is the root of an if-then-else.
+    OPT_Diamond diamond = OPT_Diamond.buildDiamond(bb);
+    if (diamond == null) return false;
+    OPT_BasicBlock taken = diamond.getTaken();
+    OPT_BasicBlock notTaken= diamond.getNotTaken();
+
+    // do not perform the transformation if either branch of the diamond
+    // has a taboo instruction (eg., a PEI, store or divide).
+    if (taken != null && hasCMTaboo(taken)) return false;
+    if (notTaken != null && hasCMTaboo(notTaken)) return false;
+
+    // count the number of expression evaluations in each side of the
+    // diamond
+    int takenCost = 0;
+    int notTakenCost = 0;
+    if (taken != null) takenCost = evaluateCost(taken);
+    if (notTaken != null) notTakenCost = evaluateCost(notTaken);
+    
+    // evaluate whether it's profitable.
+    int shortestCost = Math.min(takenCost,notTakenCost);
+    int xformCost = 2 * (takenCost + notTakenCost);
+    int k = ir.options.COND_MOVE_CUTOFF;
+    if (xformCost - shortestCost > k) return false;
+
+    // Perform the transformation!
+    doCondMove(ir, diamond, cb);
+    return true;
+  }
+
+  /**
+   * Do any of the instructions in a basic block preclude eliminating the
+   * basic block with conditional moves?
+   */
+  private boolean hasCMTaboo(OPT_BasicBlock bb) {
+    for (Enumeration e = bb.forwardRealInstrEnumerator();
+         e.hasMoreElements(); ) {
+      OPT_Instruction s = (OPT_Instruction)e.nextElement();
+      if (s.isBranch()) continue;
+      if (s.isPEI()) return true;
+      if (s.isStore()) return true;
+      if (s.getNumberOfDefs() != 1) return true;
+      if (s.isThrow()) return true;
+      if (s.isDynamicLinkingPoint()) return true;
+      // the following excludes divides
+      if (GuardedBinary.conforms(s)) return true;
+    }
+    return false;
+  }
+  /**
+   * Evaluate the cost of a basic block, in number of real instructions. 
+   */
+  private int evaluateCost(OPT_BasicBlock bb) {
+    int result = 0;
+    for (Enumeration e = bb.forwardRealInstrEnumerator();
+         e.hasMoreElements(); ) {
+      OPT_Instruction s = (OPT_Instruction)e.nextElement();
+      if (!s.isBranch()) result++;
+    }
+    return result;
+  }
+
+  /**
+   * For each real non-branch instruction s in bb, 
+   * <ul>
+   * <li> Copy s to s', and store s' in the returned array
+   * <li> Insert the function s->s' in the map
+   * </ul>
+   */
+  private OPT_Instruction[] copyAndMapInstructions(OPT_BasicBlock bb,
+                                                   HashMap map) {
+    int count = 0;
+    // first count the number of instructions
+    for (Enumeration e = bb.forwardRealInstrEnumerator();
+         e.hasMoreElements(); ) {
+      OPT_Instruction s = (OPT_Instruction)e.nextElement();
+      if (s.isBranch()) continue;
+      count++;
+    }
+    // now copy.
+    OPT_Instruction[] result = new OPT_Instruction[count];
+    int i = 0;
+    for (Enumeration e = bb.forwardRealInstrEnumerator(); 
+         e.hasMoreElements(); ) {
+      OPT_Instruction s = (OPT_Instruction)e.nextElement();
+      if (s.isBranch()) continue;
+      OPT_Instruction sprime = s.copyWithoutLinks();
+      result[i++] = sprime;
+      map.put(s,sprime);
+    }
+    return result;
+  }
+
+  /**
+   * For each in a set of instructions, rewrite every def to use a new
+   * temporary register.  If a rewritten def is subsequently used, then
+   * use the new temporary register instead.
+   */
+  private void rewriteWithTemporaries(OPT_Instruction[] set, OPT_IR ir) {
+
+    // Maintain a mapping holding the new name for each register
+    HashMap map = new HashMap();
+    for (int i=0; i<set.length; i++) {
+      OPT_Instruction s = set[i];
+
+      // rewrite the uses to use the new names
+      for (Enumeration e = s.getUses(); e.hasMoreElements(); ) {
+        OPT_Operand use = (OPT_Operand)e.nextElement();
+        if (use != null && use.isRegister()) {
+          OPT_Register r = use.asRegister().register;
+          OPT_Register temp = (OPT_Register)map.get(r);
+          if (temp != null) {
+            use.asRegister().register = temp;
+          }
+        }
+      }
+
+      if (VM.VerifyAssertions) VM.assert(s.getNumberOfDefs() == 1);
+
+      OPT_Operand def = (OPT_Operand)s.getDefs().nextElement();
+      OPT_RegisterOperand rDef = def.asRegister();
+      OPT_RegisterOperand temp = ir.regpool.makeTemp(rDef);
+      map.put(rDef.register,temp.register);
+      s.replaceOperand(def,temp);
+    }
+  }
+
+  /**
+   * Insert each instruction in a list before instruction s
+   */ 
+  private void insertBefore(OPT_Instruction[] list, OPT_Instruction s) {
+    for (int i=0; i<list.length; i++) {
+      OPT_Instruction x = list[i];
+      s.insertBefore(x);
+    }
+  }
+
+  /**
+   * Perform the transformation to replace conditional branch with a
+   * sequence using conditional moves.
+   *
+   * @param ir governing IR
+   * @param diamond the IR diamond structure to replace
+   * @param cb conditional branch instruction at the head of the diamond
+   */
+  private void doCondMove(OPT_IR ir, OPT_Diamond diamond, OPT_Instruction cb) {
+    OPT_BasicBlock taken = diamond.getTaken();
+    OPT_BasicBlock notTaken = diamond.getNotTaken();
+
+    // for each non-branch instruction s in the diamond, 
+    // copy s to a new instruction s'
+    // and store a mapping from s to s'
+    HashMap takenInstructions = new HashMap();
+    OPT_Instruction[] takenInstructionList = copyAndMapInstructions
+      (taken, takenInstructions);
+
+    HashMap notTakenInstructions = new HashMap();
+    OPT_Instruction[] notTakenInstructionList =
+      copyAndMapInstructions(notTaken, notTakenInstructions);
+    
+    // Extract the values and condition from the conditional branch.
+    OPT_Operand val1 = IfCmp.getVal1(cb);
+    OPT_Operand val2 = IfCmp.getVal2(cb);
+    OPT_ConditionOperand cond = IfCmp.getCond(cb);
+
+    // Copy val1 and val2 to temporaries, just in case they're defined in
+    // the diamond.  If they're not defined in the diamond, copy prop
+    // should clean these moves up.
+    OPT_RegisterOperand tempVal1 = ir.regpool.makeTemp(val1);
+    OPT_Operator op = OPT_IRTools.getMoveOp(tempVal1.type, false);
+    cb.insertBefore(Move.create(op,tempVal1.copyRO(),val1.copy()));
+    OPT_RegisterOperand tempVal2 = ir.regpool.makeTemp(val2);
+    op = OPT_IRTools.getMoveOp(tempVal2.type, false);
+    cb.insertBefore(Move.create(op,tempVal2.copyRO(),val2.copy()));
+
+    // For each instruction in each temporary set, rewrite it to def a new
+    // temporary, and insert it before the branch.
+    rewriteWithTemporaries(takenInstructionList,ir);
+    rewriteWithTemporaries(notTakenInstructionList,ir);
+    insertBefore(takenInstructionList,cb);
+    insertBefore(notTakenInstructionList,cb);
+    
+    // For each register defined in the TAKEN branch, save a mapping to
+    // the corresponding conditional move.
+    HashMap takenMap = new HashMap();
+    
+    // Now insert conditional moves to replace each instruction in the diamond.
+    // First handle the taken branch.
+    if (taken != null) {
+      for (Enumeration e = taken.forwardRealInstrEnumerator(); 
+           e.hasMoreElements();) {
+        OPT_Instruction s = (OPT_Instruction)e.nextElement(); 
+        if (s.isBranch()) continue;
+        OPT_Operand def = (OPT_Operand)s.getDefs().nextElement();
+        OPT_Instruction tempS = (OPT_Instruction)takenInstructions.get(s);
+        OPT_RegisterOperand temp = (OPT_RegisterOperand)
+          tempS.getDefs().nextElement();
+        op = OPT_IRTools.getCondMoveOp(def.asRegister().type,
+                                                    false);
+        OPT_Instruction cmov = CondMove.create(op,def.asRegister() ,
+                                               tempVal1.copy(),
+                                               tempVal2.copy(),
+                                               cond.copy().asCondition(),
+                                               temp.copy(),
+                                               def.copy());
+        takenMap.put(def.asRegister().register,cmov);
+        cb.insertBefore(cmov);
+        s.remove();
+      }
+    }
+    // For each register defined in the NOT-TAKEN branch, save a mapping to
+    // the corresponding conditional move.
+    HashMap notTakenMap = new HashMap();
+    // Next handle the not taken branch.
+    if (notTaken != null) {
+      for (Enumeration e = notTaken.forwardRealInstrEnumerator(); 
+           e.hasMoreElements();) {
+        OPT_Instruction s = (OPT_Instruction)e.nextElement(); 
+        if (s.isBranch()) continue;
+        OPT_Operand def = (OPT_Operand)s.getDefs().nextElement();
+        OPT_Instruction tempS = (OPT_Instruction)notTakenInstructions.get(s);
+        OPT_RegisterOperand temp = (OPT_RegisterOperand)
+          tempS.getDefs().nextElement();
+
+        OPT_Instruction prevCmov = (OPT_Instruction)takenMap.get(def.asRegister
+                                                                 ().register);
+        if (prevCmov != null) {
+          // if this register was also defined in the taken branch, change
+          // the previous cmov with a different 'False' Value
+          CondMove.setFalseValue(prevCmov, temp.copy());
+          notTakenMap.put(def.asRegister().register,prevCmov);
+        } else {
+          // create a new cmov instruction
+          op = OPT_IRTools.getCondMoveOp(def.asRegister().type, false);
+          OPT_Instruction cmov = CondMove.create(op,def.asRegister(),
+                                                 tempVal1.copy(),
+                                                 tempVal2.copy(),
+                                                 cond.copy().asCondition().
+                                                 flipCode(),
+                                                 def.copy(),
+                                                 temp.copy());
+          cb.insertBefore(cmov);
+          notTakenMap.put(def.asRegister().register,cmov);
+          s.remove();
+        }
+      }
+    }
+
+    // Finally, mutate the conditional branch into a GOTO and recompute
+    // CFG.
+    OPT_BranchOperand target = diamond.getBottom().makeJumpTarget();
+    Goto.mutate(cb,GOTO,target);
+    diamond.getTop().recomputeNormalOut(ir); // fix the CFG 
+  }
+
+  /**
    * Attempt to generate a boolean compare opcode from a conditional branch
    *
    * <pre>
@@ -526,67 +852,4 @@ public final class OPT_BranchOptimizations
                                 // jb is the exit node.
     return true;
   }
-  /*
-   * Case 1)     bb.-----------.              bb.-----------------------.
-   *               | if(v1~v2) |                | BOOLEAN_CMP x=v1,v2,~ |
-   *               | goto?     |                | return x              |
-   *               `-,-------.-'                `-----------.-----------'
-   *       ---.    T/         \F    ,---      ---.          |          ,---
-   *           `v  v           v  v'              `v        |        v'
-   *       tb.---------.   .----------.fb    tb.----------. | .----------.fb
-   *        | return 1 |   | return 0 |   =>   | return 1 | | | return 0 |
-   *        `------.---'   `---,------'        `------.---' | `---,------'
-   *                \         /                        \    |    /
-   *                 v       v                          v   v   v
-   *           exit.-----------.                  exit.-----------.
-   *               |           |                      |           |
-   *               `-----------'                      `-----------'
-   *
-   * Case 2)     bb.-----------.              bb.------------------------.
-   *               | if(v1~v2) |                | BOOLEAN_CMP x=v1,v2,!~ |
-   *               | goto?     |                | return x               |
-   *               `-,-------.-'                `-----------.------------'
-   *       ---.    T/         \F    ,---      ---.          |          ,---
-   *           `v  v           v  v'              `v        |        v'
-   *       tb.---------.   .----------.fb    tb.----------. | .----------.fb
-   *        | return 0 |   | return 1 |   =>   | return 0 | | | return 1 |
-   *        `------.---'   `---,------'        `------.---' | `---,------'
-   *                \         /                        \    |    /
-   *                 v       v                          v   v   v
-   *           exit.-----------.                  exit.-----------.
-   *               |           |                      |           |
-   *               `-----------'                      `-----------'
-   *
-   * Case 3)     bb.-----------.              bb.-----------------------.
-   *               | if(v1~v2) |                | BOOLEAN_CMP x=v1,v2,~ |
-   *               | goto?     |                | goto                  |
-   *               `-,-------.-'                `-----------.-----------'
-   *       ---.    T/         \F    ,---      ---.          |          ,---
-   *           `v  v           v  v'              `v        |        v'
-   *         tb.-------.   .-------.fb          tb.-------. | .-------.fb
-   *           | x = 1 |   | x = 0 |      =>      | x = 1 | | | x = 0 |
-   *           | goto? |   | goto? |              | goto? | | | goto? |
-   *           `---.---'   `---,---'              `---.---' | `---,---'
-   *                \         /                        \    |    /
-   *                 v       v                          v   v   v
-   *             jb.-----------.                    jb.-----------.
-   *               |           |                      |           |
-   *               `-----------'                      `-----------'
-   *
-   * Case 4)     bb.-----------.              bb.------------------------.
-   *               | if(v1~v2) |                | BOOLEAN_CMP x=v1,v2,!~ |
-   *               | goto?     |                | goto                   |
-   *               `-,-------.-'                `-----------.------------'
-   *       ---.    T/         \F    ,---      ---.          |          ,---
-   *           `v  v           v  v'              `v        |        v'
-   *         tb.-------.   .-------.fb          tb.-------. | .-------.fb
-   *           | x = 0 |   | x = 1 |      =>      | x = 0 | | | x = 1 |
-   *           | goto? |   | goto? |              | goto? | | | goto? |
-   *           `---.---'   `---,---'              `---.---' | `---,---'
-   *                \         /                        \    |    /
-   *                 v       v                          v   v   v
-   *             jb.-----------.                    jb.-----------.
-   *               |           |                      |           |
-   *               `-----------'                      `-----------'
-   */
 }
