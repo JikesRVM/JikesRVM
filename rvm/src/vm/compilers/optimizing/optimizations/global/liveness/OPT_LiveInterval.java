766dbe76107ebca4846199bb6600709c6cee6885
Stephen Fink <sjfink@us.ibm.com>
2001-10-31 13:18:14 +0000

    moved here from ir/instruction

---
+++ rvm/src/vm/compilers/optimizing/optimizations/global/liveness/OPT_LiveInterval.java
@@ -0,0 +1,221 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$
+
+/**
+ * This class contains useful methods for managing liveIntervals.
+ *
+ * @see OPT_IRMap
+ *
+ * @author Michael Hind
+ * @author Mauricio Serrano
+ */
+final class OPT_LiveInterval {
+
+  private static final boolean DEBUG = false;
+
+  /**
+   * This method iterates over each element in the the passed live set.
+   * For each element, it checks if an existing live interval node for
+   * the basic block passed exists.  If one does not exist, it creates
+   * a node with the end instruction being "inst".  If one already exists
+   * no action is taken.
+   *
+   * @param set  the set of registers, encoded as a OPT_LiveSet object 
+   * @param block the basic block
+   * @param inst the intruction where the register's live range ends, 
+   *             null represents the end of the basic block
+   */
+  static public void createEndLiveRange(OPT_LiveSet set, 
+					OPT_BasicBlock block, 
+					OPT_Instruction inst) {
+    if (DEBUG) {
+      if (inst == null) {
+	System.out.println("The following are live on exit of block "+ 
+			   block.getNumber()  +"\n"+ set);
+      } else {
+	System.out.println("The following are live ending at inst\n  "+ inst
+			   +" for block "+  block.getNumber() +"\n"+ set);
+      }
+    }
+
+    OPT_LiveSetEnumerator enum = set.enumerator();
+    while (enum.hasMoreElements()) {
+      OPT_RegisterOperand regOp = (OPT_RegisterOperand) enum.nextElement();
+      createEndLiveRange(regOp.register, block, inst);
+    }
+  }
+
+  /**
+   * This method checks if an existing unresolved live interval node,
+   * i.e., one that has an end instruction, but no beginning instruction,
+   * is present for the register and basic block passed.  If one does not exist,
+   * it creates a node with the end instruction being "inst".  
+   * If one already exists no action is taken.
+   *
+   * @param set  the set of registers, encoded as a OPT_LiveSet object 
+   * @param block the basic block
+   */
+  static public void createEndLiveRange(OPT_Register reg, 
+					OPT_BasicBlock block,
+					OPT_Instruction inst) { 
+
+    if (DEBUG) {
+      System.out.println("Marking Register "+ reg
+			 +"'s live range as ENDing at instruction\n   "+
+			 inst +" in block #"+ block.getNumber());
+      printLiveIntervalList(block);
+    }
+
+    if (!containsUnresolvedElement(block, reg)) {
+      OPT_LiveIntervalElement elem = 
+	new OPT_LiveIntervalElement(reg, null, inst);
+
+      // add elem to the list for the basic block
+      block.prependLiveIntervalElement(elem);
+    }
+  }
+
+  /**
+   * This method finds the LiveInterval node for the register and basic block
+   * passed.  It then sets the begin instruction to the instruction passed
+   * and moves the node to the proper place on the list block list.
+   * (The block list is sorted by "begin" instruction.
+   *
+   * @param reg   the register of interest
+   * @param inst  the "begin" instruction
+   * @param block the basic block of interest
+   */
+  static public void setStartLiveRange(OPT_Register reg, 
+				       OPT_Instruction inst, 
+				       OPT_BasicBlock block) {
+    if (DEBUG) {
+      System.out.println("Marking Register "+ reg
+			 +"'s live range as STARTing at instruction\n   "+
+			 inst +" in block #"+ block.getNumber());
+    }
+
+    OPT_LiveIntervalElement prev = null; 
+    OPT_LiveIntervalElement elem = 
+      (OPT_LiveIntervalElement) block.getFirstLiveIntervalElement();
+    while (elem != null) {
+      if (elem.getRegister() == reg && elem.getBegin() == null) {
+	break;
+      }
+
+      prev = elem;
+      elem = (OPT_LiveIntervalElement) elem.getNext(); 
+    }
+
+    if (elem != null) {
+      elem.setBegin(inst);
+
+      // we want the list sorted by "begin" instruction.  Since
+      // we are *assuming* that we are called in a traversal that is
+      // visiting instructions backwards, the instr passed will always
+      // be the most recent.  Thus, we move "elem" to the front of the list.
+      if (prev != null) {
+	// remove elem from current position
+	prev.setNext(elem.next);
+
+	// add it to the begining
+	block.prependLiveIntervalElement(elem);
+      }
+
+      // if prev == null, the element is already first in the list!
+    } else {
+      // if we didn't find it, it means we have a def that is not later
+      // used, i.e., a dead assignment.  This may exist because the 
+      // instruction has side effects such as a function call or a PEI
+      // In this case, we create a LiveIntervalElement node with begining
+      // and ending instruction "inst"
+      OPT_LiveIntervalElement newElem = new OPT_LiveIntervalElement(reg,
+								    inst,
+								    inst);
+      block.prependLiveIntervalElement(newElem);
+    }
+
+    if (DEBUG) {
+      System.out.println("after add");
+      printLiveIntervalList(block);
+    }
+  }
+
+  /**
+   * This method finds any LiveInterval node that does not have a start
+   * instruction (it is null) and moves this node to the front of the list.
+   *
+   * @param block the basic block of interest
+   */
+  static public void moveUpwardExposedRegsToFront(OPT_BasicBlock block) {
+
+    OPT_LiveIntervalElement prev = 
+      (OPT_LiveIntervalElement) block.getFirstLiveIntervalElement();
+    if (prev == null)
+      return;
+
+    // The first element is already at the front, so move on to the next one
+    OPT_LiveIntervalElement elem = prev.getNext();
+    
+    while (elem != null) {
+      if (elem.getBegin() == null) {
+	// remove elem from current position
+	prev.setNext(elem.getNext());
+
+	// add it to the begining, se
+	block.prependLiveIntervalElement(elem);
+
+	// the next victum is the *new* one after prev
+	elem = (OPT_LiveIntervalElement) prev.getNext(); 
+      } else {
+	prev = elem;
+	elem = (OPT_LiveIntervalElement) elem.getNext(); 
+      }
+    }
+  }
+
+  /** 
+   * Check to see if an unresolved LiveIntervalElement node for the register
+   * passed exists for the basic block passed.
+   *
+   * @param block the block
+   * @param reg   the register of interest
+   * @return <code>true</code> if it does or <code>false</code>
+   *         if it does not
+   */
+  static private boolean containsUnresolvedElement(OPT_BasicBlock block, 
+						   OPT_Register reg) {
+
+    if (DEBUG) {
+      System.out.println("containsUnresolvedElement called, block: "+ block
+			 +" register: "+ reg);
+      printLiveIntervalList(block);
+    }
+
+    for (OPT_LiveIntervalElement elem = block.getFirstLiveIntervalElement();
+	 elem != null;
+	 elem = (OPT_LiveIntervalElement) elem.getNext()) {
+      // if we got an element, down case it to OPT_LiveIntervalElement
+      if (elem.getRegister() == reg && elem.getBegin() == null) {
+	return true;
+      }
+    }
+    return false;
+  }
+
+  /** 
+   * Print the live intervals for a block.
+   *
+   * @param block the block
+   */
+  static public void printLiveIntervalList(OPT_BasicBlock block) {
+    System.out.println("Live Interval List for "+ block);
+    for (OPT_LiveIntervalElement elem 
+	   = (OPT_LiveIntervalElement) block.getFirstLiveIntervalElement();
+	 elem != null;
+	 elem = (OPT_LiveIntervalElement) elem.getNext()) {
+      System.out.println("  "+ elem);
+    }
+  }
+}
---
+++ rvm/src/vm/compilers/optimizing/optimizations/global/liveness/OPT_LiveIntervalElement.java
@@ -0,0 +1,94 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$
+
+/**
+ * This class defines a LiveInterval node created by Live Variable analysis
+ * and used in Linear Scan.
+ * 
+ * @see OPT_IRMap
+ * @see OPT_LinearScan
+ * 
+ * @author Michael Hind
+ * @author Mauricio Serrano
+ */
+final class OPT_LiveIntervalElement {
+
+  /**
+   * register that this live interval is for
+   */
+  private OPT_Register register;
+
+  /**
+   * instruction where the live interval begins 
+   * (null if alive at basic block entry)  
+   */
+  private OPT_Instruction begin;
+
+  /**
+   * instruction where the live interval ends
+   * (null if alive at basic block exit)
+   */
+  private OPT_Instruction end;
+
+  /**
+   * The basic block holding this live interval element
+   */
+  private OPT_BasicBlock bb;
+
+  /**
+   * LiveIntervalElements are linked in a singly-linked list; this is the
+   * next pointer.
+   */ 
+  OPT_LiveIntervalElement next;
+  
+  /**
+   * Use this constructor when the live interval spans a basic block
+   * boundary.
+   * 
+   * @param reg The OPT_Register whose live interval we are representing
+   */
+  OPT_LiveIntervalElement(OPT_Register reg) {
+    register = reg;
+    begin = null;
+    end = null;
+  }
+ 
+  /**
+   * Use this constructur when the live interval is within a basic block
+   * 
+   * @param reg   the OPT_Register whose live interval we are representing
+   * @param begin the definition of the register
+   * @param end   the last use of the register
+   */
+  OPT_LiveIntervalElement(OPT_Register reg, 
+			  OPT_Instruction begin, 
+			  OPT_Instruction end) {
+    register = reg;
+    this.begin = begin;
+    this.end = end;
+  }
+ 
+  public String toString() {
+    return "Reg: "+ register +"\n     Begin: "+ begin +"\n     End:   "+ end;
+  }
+
+  /*
+   * Getters and setters for instance fields
+   */
+
+  public OPT_Instruction getBegin()           { return begin; }
+  public void setBegin(OPT_Instruction begin) { this.begin = begin; }
+
+  public OPT_Instruction getEnd()             { return end; }
+
+  public OPT_Register getRegister()           { return register; }
+  public void setRegister(OPT_Register r)     { register = r; }
+
+  OPT_LiveIntervalElement getNext()           { return next; }
+  void setNext(OPT_LiveIntervalElement Next)  { next = Next; }
+
+  OPT_BasicBlock getBasicBlock()              { return bb; }
+  void setBasicBlock(OPT_BasicBlock bb)       { this.bb = bb; }
+}
