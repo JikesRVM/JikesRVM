5af2a2e4cc9cc2b07ba33475dca7078280a2dbf1
Stephen Fink <sjfink@us.ibm.com>
2001-10-31 13:18:19 +0000

    fix exception for remove() method

--- rvm/src/vm/compilers/optimizing/util/set/OPT_EmptyIterator.java
+++ rvm/src/vm/compilers/optimizing/util/set/OPT_EmptyIterator.java
@@ -7,31 +7,20 @@
  * @author Mauricio J. Serrano
  * @author John Whaley
  */
-class OPT_EmptyIterator
-    implements java.util.Iterator {
+class OPT_EmptyIterator implements java.util.Iterator {
 
-  /**
-   * put your documentation comment here
-   * @return 
-   */
   public boolean hasNext () {
     return  false;
   }
 
-  /**
-   * put your documentation comment here
-   * @return 
-   */
   public Object next () {
     throw  new java.util.NoSuchElementException();
   }
 
-  /**
-   * put your documentation comment here
-   */
   public void remove () {
-    throw  new java.util.UnsupportedOperationException();
+    throw new OPT_OptimizingCompilerException("EmptyIterator.remove called");
   }
+
   public static OPT_EmptyIterator INSTANCE = new OPT_EmptyIterator();
 }
 
