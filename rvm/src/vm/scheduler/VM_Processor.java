143a1e1b2d8d282da3371b34d718d6ce72e1578c
Bowen Alpern <alpernb@us.ibm.com>
2001-10-31 14:26:09 +0000

    fixed bug I introduced last week that allowed a thread running in phantom mode to get onto a ready queue.

--- rvm/src/vm/scheduler/VM_Processor.java
+++ rvm/src/vm/scheduler/VM_Processor.java
@@ -374,24 +374,13 @@ final class VM_Processor implements VM_Uninterruptible,  VM_Constants, VM_GCCons
    * Add a thread to this processor's transfer queue.
    */ 
   private void transferThread (VM_Thread t) {
-    if (this != getCurrentProcessor() || t.isGCThread) {
+    if (this != getCurrentProcessor() || t.isGCThread || (t.beingDispatched && t != VM_Thread.getCurrentThread())) {
       transferMutex.lock();
       transferQueue.enqueue(t);
       transferMutex.unlock();
     } else if (t.isIdleThread) {
       idleQueue.enqueue(t);
     } else {
-      //      if (VM.VerifyAssertions) VM.assert(t.beingDispatched == false || t == VM_Thread.getCurrentThread()); // local queue: no other dispatcher should be running on thread's stack
-      if (t.beingDispatched && t != VM_Thread.getCurrentThread()) {
-	VM_Scheduler.trace("VM_Processor", "getRunnableThread: readyQueue BUG", t.getIndex());
-	VM.sysWrite("current thread: ");
-	VM_Thread.getCurrentThread().dump();
-	VM.sysWrite("offending thread: ");
-	t.dump();
-	VM.assert(this == getCurrentProcessor());
-	VM_Scheduler.dumpVirtualMachine();
-	VM.sysExit(555);
-      }
       readyQueue.enqueue(t);
     }
   }
@@ -421,17 +410,7 @@ final class VM_Processor implements VM_Uninterruptible,  VM_Constants, VM_GCCons
     if (!readyQueue.isEmpty()) {
       VM_Thread t = readyQueue.dequeue();
       if (trace) VM_Scheduler.trace("VM_Processor", "getRunnableThread: readyQueue", t.getIndex());
-      //      if (VM.VerifyAssertions) VM.assert(t.beingDispatched == false || t == VM_Thread.getCurrentThread()); // local queue: no other dispatcher should be running on thread's stack
-      if (t.beingDispatched && t != VM_Thread.getCurrentThread()) {
-	VM_Scheduler.trace("VM_Processor", "getRunnableThread: readyQueue BUG", t.getIndex());
-	VM.sysWrite("current thread: ");
-	VM_Thread.getCurrentThread().dump();
-	VM.sysWrite("offending thread: ");
-	t.dump();
-	VM.assert(this == getCurrentProcessor());
-	VM_Scheduler.dumpVirtualMachine();
-	VM.sysExit(666);
-      }
+      if (VM.VerifyAssertions) VM.assert(t.beingDispatched == false || t == VM_Thread.getCurrentThread()); // local queue: no other dispatcher should be running on thread's stack
       return t;
     }
 
