08de9f21772fde9d6099f3e5502abaa7df7598ba
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2001-11-02 14:04:06 +0000

    implement VM_BaselineConstants instead of VM_Constants

--- rvm/src/vm/memoryManagers/watson/concurrent/VM_RCBarriers.java
+++ rvm/src/vm/memoryManagers/watson/concurrent/VM_RCBarriers.java
@@ -6,7 +6,7 @@
 /**
  * @author David Bacon
  */
-class VM_RCBarriers implements VM_Constants {
+class VM_RCBarriers implements VM_BaselineConstants {
 
     static final boolean DONT_BARRIER_BLOCK_CONTROLS = true;
 
