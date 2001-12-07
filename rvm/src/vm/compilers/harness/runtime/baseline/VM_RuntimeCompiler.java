2f7d946a008dbe7f86e15f8c48ec4ce5d025c505
Janice Shepherd <janshep@us.ibm.com>
2001-12-07 20:48:39 +0000

    Move command line arg processing to new VM_BaselineCompiler class

--- rvm/src/vm/compilers/harness/runtime/baseline/VM_RuntimeCompiler.java
+++ rvm/src/vm/compilers/harness/runtime/baseline/VM_RuntimeCompiler.java
@@ -20,15 +20,7 @@ class VM_RuntimeCompiler extends VM_RuntimeCompilerInfrastructure {
   }
 
   static void processCommandLineArg(String arg) {
-    if (VM_Compiler.setUpOptions != null) {
-      if (VM_Compiler.setUpOptions.processAsOption("-X:irc", arg)) {
-	return;
-      } else {
-	VM.sysWrite("VM_RuntimeCompiler(baseline): Ignoring unrecognized argument \""+ arg + "\"\n");
-      }
-    } else {
-	VM.sysWrite("VM_RuntimeCompiler(baseline): Compiler setUpOptions not enabled; Ignoring argument \""+ arg + "\"\n");
-    }
+    VM_BaselineCompiler.processCommandLineArg("-X:irc",arg);
   }
 
   static VM_CompiledMethod compile(VM_Method method) {
