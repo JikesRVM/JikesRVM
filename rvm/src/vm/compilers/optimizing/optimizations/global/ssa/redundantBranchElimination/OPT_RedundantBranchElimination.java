33c01fcf9ef6506935021f4825644f8f726b46e0
Dave Grove <groved@us.ibm.com>
2001-12-13 22:15:27 +0000

    several bug fixes; do unreachable code elimination

--- rvm/src/vm/compilers/optimizing/optimizations/global/ssa/redundantBranchElimination/OPT_RedundantBranchElimination.java
+++ rvm/src/vm/compilers/optimizing/optimizations/global/ssa/redundantBranchElimination/OPT_RedundantBranchElimination.java
@@ -9,10 +9,28 @@ import java.util.*;
 /**
  * Redundant branch elimination based on SSA form, global value numbers,
  * and dominance relationships. 
- * A conditional branch is redundant if it is dominated by either
- * the true or false successor of an equivalent conditional branch
- * (two conditional branches are equivalent if they have the same 
- *  value number).
+ * The following are sufficient conditions for a conditional branch cb1
+ * to be eliminated as redundant
+ * <ul>
+ * <li> It is equivalent (has the same value number) as another 
+ *      conditional branch cb2
+ * <li> Either (a) the target of the taken branch of cb2 dominates cb1 
+ *      and said target block has exactly one in edge or (b)
+ *      the not-taken continuation of cb2 dominates cb1 and 
+ *      said continuation block has exactly one in edge.
+ * </ul>
+ * NOTE: the check for exactly one in edge is used to rule out
+ *       situations like the following:
+ * <pre>
+ *      if (C) goto L2              // cb2
+ *      x = x + 1;
+ *  L2: x = x + 1;
+ *      if (C) goto L3.            // cb1
+ * </pre>
+ * Here L2 (the target of cb2) dominates cb1, but it 
+ * is not correct to eliminate cb1 because it is also 
+ * reachable (but not dominated) from the continutation
+ * block of cb2!
  *
  * @author Steve Fink
  * @author Dave Grove
@@ -50,10 +68,10 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
 
   private static final class RBE extends OPT_CompilerPhase implements OPT_Operators {
     private static final boolean DEBUG = false;
-    final String getName() { return "Transform"; }
+    final String getName() { return "RBE Transform"; }
     final boolean shouldPerform(OPT_Options options) { return true; }
     final boolean printingEnabled (OPT_Options options, boolean before) {
-      return DEBUG;
+      return false && DEBUG;
     }
 
     /**
@@ -63,6 +81,7 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
      * @param OPT_IR the IR on which to apply the phase
      */
     final void perform (OPT_IR ir) {
+      // (1) Remove redundant conditional branches and locally fix the PHIs
       OPT_GlobalValueNumberState gvns = ir.HIRInfo.valueNumbers;
       OPT_DominatorTree dt = ir.HIRInfo.dominatorTree;
       for (OPT_BasicBlockEnumeration bbs = ir.getBasicBlocks();
@@ -76,12 +95,16 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
 	  for (Iterator e = cc.iterator(); e.hasNext();) {
 	    OPT_Instruction poss = (OPT_Instruction)((OPT_ValueGraphVertex)e.next()).name;
 	    if (poss != candTest) {
-	      if (dt.dominates(getNotTakenBlock(poss), candBB)) {
+	      OPT_BasicBlock notTaken = getNotTakenBlock(poss);
+	      OPT_BasicBlock taken = poss.getBranchTarget();
+	      if (taken == notTaken) continue; // both go to same block, so we don't know anything!
+	      if (notTaken.hasOneIn() && dt.dominates(notTaken, candBB)) {
 		if (DEBUG) VM.sysWrite(candTest + " is dominated by not-taken branch of "+poss+"\n");
 		removeCondBranch(candBB, candTest, ir);
 		cc.removeVertex(gvns.valueGraph.getVertex(candTest));
 		break;
-	      } else if (dt.dominates(poss.getBranchTarget(), candBB)) {
+	      }
+	      if (taken.hasOneIn() && dt.dominates(taken, candBB)) {
 		if (DEBUG) VM.sysWrite(candTest + " is dominated by taken branch of "+poss+"\n");
 		takeCondBranch(candBB, candTest, ir);
 		cc.removeVertex(gvns.valueGraph.getVertex(candTest));
@@ -89,6 +112,27 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
 	      }
 	    }
 	  }
+	}
+      }
+      // (2) We may have created some dead code; remove it
+      while (haveBlock()) {
+	OPT_BasicBlock curr = popBlock();
+	if (curr.scratch != 0) continue; // set in push block; avoid processing block twice!
+	if (DEBUG) VM.sysWrite("Checking to see if block "+curr+" is dead\n");
+	if (curr.hasZeroIn() ||
+	    (curr.hasOneIn() && curr.pointsIn(curr))) {
+	  // block is dead.
+	  curr.scratch = 0xdeadbeef;
+	  if (DEBUG) VM.sysWrite("\tremoving dead block "+curr+"\n");
+	  for (OPT_BasicBlockEnumeration e = curr.getOut();
+	       e.hasMoreElements(); ) {
+	    OPT_BasicBlock target = e.next();
+	    if (target != curr && !target.isExit()) {
+	      OPT_SSA.purgeBlockFromPHIs(curr, target);
+	      pushBlock(target);
+	    }
+	  }
+	  ir.cfg.removeFromCFGAndCodeOrder(curr);
 	}
       }
     }
@@ -117,6 +161,7 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
 	// there is no longer an edge from source to target;
 	// update any PHIs in target to reflect this.
 	OPT_SSA.purgeBlockFromPHIs(source, deadBB);
+	pushBlock(deadBB);
       }
     }
 
@@ -139,7 +184,35 @@ final class OPT_RedundantBranchElimination extends OPT_OptimizationPlanComposite
 	// there is no longer an edge from source to target;
 	// update any PHIs in target to reflect this.
 	OPT_SSA.purgeBlockFromPHIs(source, deadBB);
+	pushBlock(deadBB);
       }
     }
+
+
+    /*
+     * Support for maintaining a work list of possibly unreachable blocks
+     */
+    private static class BL {
+      OPT_BasicBlock block;
+      BL next;
+      BL(OPT_BasicBlock b, BL n) {
+	block = b;
+	next = n;
+      }
+    }
+    private BL modBlocks;
+    private void pushBlock(OPT_BasicBlock bb) {
+      bb.scratch = 0;
+      modBlocks = new BL(bb, modBlocks);
+    }
+    private boolean haveBlock() {
+      return modBlocks != null;
+    }
+    private OPT_BasicBlock popBlock() {
+      OPT_BasicBlock tmp = modBlocks.block;
+      modBlocks = modBlocks.next;
+      return tmp;
+    }
+
   }
 }
