d5dc9bb0e67cc84b034cf41080453f9122b8620e
Perry Cheng <perryche@us.ibm.com>
2002-01-22 19:45:19 +0000

    Added functions to allocate necessarily contiguous array

--- rvm/src/vm/runtime/VM_RuntimeStructures.java
+++ rvm/src/vm/runtime/VM_RuntimeStructures.java
@@ -46,4 +46,67 @@ class VM_RuntimeStructures implements VM_Constants {
 	
 	return new int[n];
     }
+
+    /**
+     * Allocate a contiguous int array
+     * @param n The number of ints
+     * @return The contiguous int array
+     */ 
+    static int[] newContiguousIntArray (int n) {
+	VM_Magic.pragmaInline();
+
+
+	if (VM.BuildForRealtimeGC) {
+	    //-#if RVM_WITH_REALTIME_GC
+	    return VM_SegmentedArray.newIntArray(n);
+	    //-#endif
+	}
+	
+	return new int[n];
+    }
+
+    /**
+     * Get an object's TIB pointer.  NOTE: Should be part of common ObjectModel code.
+     *   @param object The object
+     *   @return The object's TIB pointer
+     */
+    static Object[] getTIB (Object object) {
+	VM_Magic.pragmaInline();
+	//return (Object[]) VM_Magic.getObjectAtOffset(object, OBJECT_TIB_OFFSET);
+	return (Object[]) VM_Magic.addressAsObject(VM_Magic.getMemoryWord(VM_Magic.objectAsAddress(object)+OBJECT_TIB_OFFSET));
+    }
+
+    /**
+     * Allocate a contiguous VM_CompiledMethod array
+     * @param n The number of objects
+     * @return The contiguous object array
+     */ 
+    static VM_CompiledMethod[] newContiguousCompiledMethodArray (int n) {
+	VM_Magic.pragmaInline();
+
+	if (VM.BuildForRealtimeGC) {
+	    //-#if RVM_WITH_REALTIME_GC
+	    return VM_SegmentedArray.newContiguousCompiledMethodArray(n);
+	    //-#endif
+	}
+	
+	return new VM_CompiledMethod[n];
+    }
+
+    /**
+     * Allocate a contiguous VM_DynamicLibrary array
+     * @param n The number of objects
+     * @return The contiguous object array
+     */ 
+    static VM_DynamicLibrary[] newContiguousDynamicLibraryArray (int n) {
+	VM_Magic.pragmaInline();
+
+	if (VM.BuildForRealtimeGC) {
+	    //-#if RVM_WITH_REALTIME_GC
+	    return VM_SegmentedArray.newContiguousDynamicLibraryArray(n);
+	    //-#endif
+	}
+	
+	return new VM_DynamicLibrary[n];
+    }
 }
