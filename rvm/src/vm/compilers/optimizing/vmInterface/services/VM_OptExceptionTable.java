9d04eacacef7f201e16e18e63ae7e8507162ebd1
Janice Shepherd <janshep@us.ibm.com>
2002-02-22 18:30:53 +0000

    improve merging to eTable entries

--- rvm/src/vm/compilers/optimizing/vmInterface/services/VM_OptExceptionTable.java
+++ rvm/src/vm/compilers/optimizing/vmInterface/services/VM_OptExceptionTable.java
@@ -21,6 +21,7 @@ final class VM_OptExceptionTable implements VM_Constants {
   private static final int CATCH_START = 2;
   private static final int EX_TYPE = 3;
 
+  private static final boolean DEBUG = false;
   /**
    * Return the machine code offset for the catch block that will handle
    * the argument exceptionType,or -1 if no such catch block exists.
@@ -64,63 +65,115 @@ final class VM_OptExceptionTable implements VM_Constants {
     return  -1;
   }
 
-
   /**
    * Construct the exception table for an IR.
    */
   VM_OptExceptionTable(OPT_IR ir) {
     int index = 0;
+    int currStartOff, currEndOff;
     int tableSize = countExceptionTableSize(ir);
     eTable = new int[tableSize*4];
+      
+
+    // For each basic block
+    //   See if it has code associated with it and if it has
+    //   any reachable exception handlers.
+    //   When such a block is found, check the blocks that follow
+    //   it in code order to see if this block has the same
+    //   Bag of exceptionHandlers as any of its immediate successors.
+    //   If so the try region can be expanded to include those
+    //   successors. Stop checking successors as soon as a non-match
+    //   is found, or a block that doesn't have handlers is found.
+    //   Successors that don't have any code associated with them can
+    //   be ignored. 
+    //   If blocks were joined together then when adding the
+    //   entries to the eTable it is important to not restrict the
+    //   entries to reachable handlers; as the first block may only
+    //   throw a subset of the exception types represented by the Bag
     for (OPT_BasicBlock bblock = ir.firstBasicBlockInCodeOrder(); 
-	 bblock != null; 
-	 bblock = bblock.nextBasicBlockInCodeOrder()) {
+	 bblock != null; ) {
+      // Iteration is explicit in loop
+
       int startOff = bblock.firstInstruction().getmcOffset();
       int endOff = bblock.lastInstruction().getmcOffset();
       if (endOff > startOff) {
-        if (bblock.hasExceptionHandlers()) {
-          for (OPT_BasicBlockEnumeration e = bblock.getReachableExceptionHandlers(); 
-	       e.hasMoreElements();) {
-            OPT_ExceptionHandlerBasicBlock eBlock = 
-	      (OPT_ExceptionHandlerBasicBlock)e.nextElement();
-	  loop: for (java.util.Enumeration ets = eBlock.getExceptionTypes(); 
-		     ets.hasMoreElements();) {
-	      OPT_TypeOperand type = (OPT_TypeOperand)ets.nextElement();
-	      int catchOffset = eBlock.firstInstruction().getmcOffset();
-	      int eId = type.type.getDictionaryId();
-	      loop2: for (int i = index - 4; i >= 0; i -= 4) {
-		int prevStart = eTable[i + TRY_START];
-		int prevEnd = eTable[i + TRY_END];
-		int prevCatch = eTable[i + CATCH_START];
-		int prevEId = eTable[i + EX_TYPE];
-		if ((prevStart == startOff) && (prevEnd == endOff) && 
-		    (prevEId == eId)) {
-		  continue loop;    // skip this entry because it's useless
-		}
-		if ((prevEnd == startOff) && (prevCatch == catchOffset) && 
-		    (prevEId == eId)) {
-		  eTable[i + TRY_END] = endOff;
-		  continue loop;   // skip this entry, merging with previous
-		}
-		if (prevStart <= endOff && prevEnd >= startOff) {
-		  break loop2; // stop trying to combine; it's getting tricky
-		}
-	      }
-	      eTable[index + TRY_START] = startOff;
-	      eTable[index + TRY_END] = endOff;
-              eTable[index + CATCH_START] = catchOffset;
-	      eTable[index + EX_TYPE] = eId;
-              index += 4;
-            }
-          }
-        }
-      }
+	if (!bblock.hasExceptionHandlers()) {
+	  bblock = bblock.nextBasicBlockInCodeOrder();
+	  continue;
+	}
+
+	OPT_BasicBlock followonBB;
+	OPT_BasicBlockEnumeration reachBBe, e;
+	boolean joinedBlocks;
+
+	// First make sure at least one of the exception handlers
+	// is reachable from this block
+	reachBBe = bblock.getReachableExceptionHandlers(); 
+	if (!reachBBe.hasMoreElements()) {
+	  bblock = bblock.nextBasicBlockInCodeOrder();
+	  continue;
+	}
+	  
+	currStartOff = startOff;
+	currEndOff = endOff;
+	joinedBlocks = false;
+
+	for (followonBB = bblock.nextBasicBlockInCodeOrder();
+	     followonBB != null;
+	     followonBB = followonBB.nextBasicBlockInCodeOrder()) {
+	  int fStartOff = followonBB.firstInstruction().getmcOffset();
+	  int fEndOff = followonBB.lastInstruction().getmcOffset();
+	  // See if followon Block has any code
+	  if (fEndOff > fStartOff) {
+	    // See if followon Block has matching handler block bag
+	    if (followonBB.hasExceptionHandlers() &&
+		bblock.isExceptionHandlerEquivalent(followonBB)) {
+	      currEndOff = fEndOff;
+	      joinedBlocks = true;
+	    } else {
+	      // Can't join any more blocks together
+	      break;
+	    }
+	  }
+	}
+	// found all the matching followon blocks 
+	// Now fill in the eTable with the handlers
+	if (joinedBlocks)
+	  e = bblock.getExceptionHandlers();
+	else
+	  e = reachBBe;
+	
+	for ( ; e.hasMoreElements();) {
+	  OPT_ExceptionHandlerBasicBlock eBlock = 
+	    (OPT_ExceptionHandlerBasicBlock)e.nextElement();
+	  for (java.util.Enumeration ets = eBlock.getExceptionTypes(); 
+	       ets.hasMoreElements();) {
+	    OPT_TypeOperand type = (OPT_TypeOperand)ets.nextElement();
+	    int catchOffset = eBlock.firstInstruction().getmcOffset();
+	    int eId = type.type.getDictionaryId();
+	    eTable[index + TRY_START] = currStartOff;
+	    eTable[index + TRY_END] = currEndOff;
+	    eTable[index + CATCH_START] = catchOffset;
+	    eTable[index + EX_TYPE] = eId;
+	    index += 4;
+	  }
+	}
+	
+	bblock = followonBB;
+      
+      } else // No code in bblock
+	bblock = bblock.nextBasicBlockInCodeOrder();
     }
-    if (index != eTable.length) {              // resize arrays
+
+    if (index != eTable.length) {              // resize array
       int[] newETable = new int[index];
       for (int i = 0; i <index; i++)
         newETable[i] = eTable[i];
       eTable = newETable;
+    }
+    if (DEBUG) {
+      VM.sysWrite("eTable length: " + eTable.length + "\n");
+      printExceptionTable();
     }
   }
 
