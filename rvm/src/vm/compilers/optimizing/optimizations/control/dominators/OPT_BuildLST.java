f2805681674dfe7951f8170ec7259eab07cf6562
Dave Grove <groved@us.ibm.com>
2002-08-13 16:20:28 +0000

    Initial Revision

---
+++ rvm/src/vm/compilers/optimizing/optimizations/control/dominators/OPT_BuildLST.java
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$
+
+/**
+ * A compiler phase to construct the loop structure tree (LST).
+ * The steps are (1) construct approximate dominators (ie blocks are 
+ * not unfactored) and (2) build the LST.
+ * 
+ * @see OPT_LSTDominators
+ * @see OPT_LSTGraph
+ *
+ * @author Dave Grove
+ */
+public class OPT_BuildLST extends OPT_CompilerPhase {
+  public String getName () { return  "Build LST"; }
+
+  /**
+   * This phase contains no per-compilation instance fields.
+   */
+  final OPT_CompilerPhase newExecution(OPT_IR ir) {
+    return this;
+  }
+
+  /**
+   * Build the Loop Structure Tree (LST) for the given IR.
+   * NOTE: CFG must be reducible.  
+   * TODO: Detect irreducible CFG, apply node splitting and then construct LST.
+   * 
+   * @param ir the IR on which to apply the phase
+   */
+  public void perform (OPT_IR ir) {
+    ir.cfg.compactNodeNumbering();
+    OPT_LTDominators.approximate(ir, true);
+    OPT_DominatorTree.perform(ir, true);
+    OPT_LSTGraph.perform(ir);
+  }
+}
