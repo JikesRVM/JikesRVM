1a7f8e7ad522fad731974f22af3e7750c076caf1
Stephen Fink <sjfink@us.ibm.com>
2002-08-22 15:01:29 +0000

    redefine hashCode()

--- rvm/src/vm/compilers/optimizing/optimizations/global/ssa/gvn/OPT_ValueNumberPair.java
+++ rvm/src/vm/compilers/optimizing/optimizations/global/ssa/gvn/OPT_ValueNumberPair.java
@@ -13,32 +13,36 @@ class OPT_ValueNumberPair {
   int v1;       // the value number of an array pointer
   int v2;       // the value number of an array index
 
-  OPT_ValueNumberPair () {
+  OPT_ValueNumberPair() {
   }
 
-  OPT_ValueNumberPair (int v1, int v2) {
+  OPT_ValueNumberPair(int v1, int v2) {
     this.v1 = v1;
     this.v2 = v2;
   }
 
-  OPT_ValueNumberPair (OPT_ValueNumberPair p) {
+  OPT_ValueNumberPair(OPT_ValueNumberPair p) {
     this.v1 = p.v1;
     this.v2 = p.v2;
   }
 
-  public boolean equals (Object o) {
+  public boolean equals(Object o) {
     if (!(o instanceof OPT_ValueNumberPair))
       return  false;
     OPT_ValueNumberPair p = (OPT_ValueNumberPair)o;
     return  (v1 == p.v1) && (v2 == p.v2);
   }
 
-  public String toString () {
+  public int hashCode() {
+    return v1 << 16 | v2;
+  }
+
+  public String toString() {
     return  "<" + v1 + "," + v2 + ">";
   }
 
   // total order over OPT_ValueNumberPairs
-  public boolean greaterThan (OPT_ValueNumberPair p) {
+  public boolean greaterThan(OPT_ValueNumberPair p) {
     if (v1 > p.v1)
       return  true;
     if (v1 < p.v1)
