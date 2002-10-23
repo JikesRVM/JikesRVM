131a0e7f07f044f79601444f3588961c43945cfd
Perry Cheng <perryche@us.ibm.com>
2002-10-23 01:30:39 +0000

    measure allocation rate too

--- rvm/src/examples/gctest/FixedLive.java
+++ rvm/src/examples/gctest/FixedLive.java
@@ -14,6 +14,7 @@ class FixedLive {
   FixedLive cdr;
 
   static int liveSize = 0;  // in megabytes
+  static int objectSize = 0; // in bytes
   static FixedLive root;
   static FixedLive junk;
 
@@ -28,14 +29,20 @@ class FixedLive {
 
   public static double computeObjectSize() {
     int estimateSize = 200000;
+    // System.out.println("totalMemory = " + Runtime.getRuntime().totalMemory());
+    // System.out.println("freeMemory = " + Runtime.getRuntime().freeMemory());
     long start = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+    System.out.println("start = " + start);
     FixedLive head = new FixedLive();
     FixedLive cur = head;
     for (int i=0; i<estimateSize; i++) {
       cur.cdr = new FixedLive();
       cur = cur.cdr;
     }
+    // System.out.println("totalMemory = " + Runtime.getRuntime().totalMemory());
+    // System.out.println("freeMemory = " + Runtime.getRuntime().freeMemory());
     long end = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+    System.out.println("end = " + end);
     return (end - start) / ((double) estimateSize);
   }
 
@@ -51,18 +58,40 @@ class FixedLive {
   }
 
   public static void allocateDie(int count) {
-    int checkFreq = 1000;
-    for (int i=0; i<count / checkFreq; i++) {
+
+    double cumulativeTraceRate = 0.0;
+    double cumulativeAllocRate = 0.0;
+    int cumulativeCount = -1;
+
+    int checkFreq = 64000 / objectSize;
+    long last = System.currentTimeMillis();
+    double allocatedSize = 0;
+    for (int i=0; i< count / checkFreq; i++) {
       long start = System.currentTimeMillis();
       for (int j=0; j<checkFreq; j++) 
 	junk = new FixedLive();
+      allocatedSize += checkFreq * objectSize;
       long end = System.currentTimeMillis();
-      double elapsed = (end - start) / 1000.0;
-      if (elapsed > 0.1) {
-	double rate = liveSize / elapsed; // Mb/s
-	System.out.println("GC (probably) occurred (" + elapsed + " s) : copying rate = " + rate + " Mb/s");
+      double traceElapsed = (end - start) / 1000.0;
+      double allocElapsed = (start - last) / 1000.0;
+      if (traceElapsed > 0.1) {
+	double traceRate = liveSize / traceElapsed; // Mb/s
+	double allocRate = (allocatedSize / 1e6) / allocElapsed; // Mb/s
+	cumulativeCount++;
+	if (cumulativeCount == 0) 
+	  System.out.println("Skipping first iteration in cumulative numbers");
+	else {
+	  cumulativeTraceRate += traceRate;
+	  cumulativeAllocRate += allocRate;
+	}
+	System.out.println("GC (probably) occurred (" + traceElapsed + " s) : copying rate = " + traceRate + " Mb/s");
+	System.out.println("                                allocation rate = " + allocRate + " Mb/s");
+	allocatedSize = 0;
+	last = end;
       }
     }
+    System.out.print("Overall Rate: tracing = " + (cumulativeTraceRate / cumulativeCount) + " Mb/s");
+    System.out.println("   allocating = " + (cumulativeAllocRate / cumulativeCount) + " Mb/s");
   }
 
   public static void runTest() throws Throwable {
@@ -70,15 +99,15 @@ class FixedLive {
     System.out.println("FixedLive running with " + liveSize + " Mb fixed live data");
     
     double objSizeEstimate = computeObjectSize();
-    int objSize = (int) (objSizeEstimate + 0.5);
+    objectSize = (int) (objSizeEstimate + 0.5);
     System.out.print("Estimated object size of a 4-field object is " + objSizeEstimate + " bytes.  ");
-    System.out.println("Rounded to " + objSize + " bytes");
+    System.out.println("Rounded to " + objectSize + " bytes");
 
-    int count = (int) (liveSize << 20) / objSize;
+    int count = (int) (liveSize << 20) / objectSize;
     System.out.println("Creating live tree with " + count + " nodes");
     root = createTree(count);
     int spinSize = 1 << 10; // in megabytes
-    int spinCount = (spinSize << 20) / objSize;
+    int spinCount = (spinSize << 20) / objectSize;
     System.out.println("Allocating " + spinSize + " Mb or " + spinCount + " nodes which immediately die");
     allocateDie(spinCount);
   }
