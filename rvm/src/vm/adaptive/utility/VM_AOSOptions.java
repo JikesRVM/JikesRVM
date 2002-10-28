3f8d50703bc364f706646e16952c7787372712f3
Dave Grove <groved@us.ibm.com>
2002-10-28 18:45:59 +0000

    Initial Revision

---
+++ rvm/src/vm/adaptive/utility/VM_AOSOptions.java
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright IBM Corp. 2002
+ */
+//$Id$
+package com.ibm.JikesRVM.adaptive;
+
+/**
+ * Additional option values that are computed internally are defined
+ * here.  Commnad line options are inherited from VM_AOSExternalOptions
+ * which is machine generated from the various .dat files.
+ */
+public final class VM_AOSOptions extends VM_AOSExternalOptions {
+  public int MAX_OPT_LEVEL;
+
+  public int FILTER_OPT_LEVEL;
+
+}
