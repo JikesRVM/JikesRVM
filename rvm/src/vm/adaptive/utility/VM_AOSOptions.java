d2c3b0eb8bcc3f79a3efd2399e4276e2b436ab40
Dave Grove <groved@us.ibm.com>
2002-10-29 22:37:38 +0000

    missing author tag

--- rvm/src/vm/adaptive/utility/VM_AOSOptions.java
+++ rvm/src/vm/adaptive/utility/VM_AOSOptions.java
@@ -8,6 +8,7 @@ package com.ibm.JikesRVM.adaptive;
  * Additional option values that are computed internally are defined
  * here.  Commnad line options are inherited from VM_AOSExternalOptions
  * which is machine generated from the various .dat files.
+ * @author Dave Grove
  */
 public final class VM_AOSOptions extends VM_AOSExternalOptions {
   public int MAX_OPT_LEVEL;
