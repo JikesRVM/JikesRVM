0d5fe0a693e030e4fa3172ddfb64437e9c16e2c2
Perry Cheng <perryche@us.ibm.com>
2002-11-02 15:22:43 +0000

    fixed ID tag

--- rvm/src/vm/memoryManagers/JMTk/policy/BasePolicy.java
+++ rvm/src/vm/memoryManagers/JMTk/policy/BasePolicy.java
@@ -10,14 +10,13 @@ import com.ibm.JikesRVM.VM;
 import com.ibm.JikesRVM.VM_Address;
 
 /**
- * @author David Bacon
- * @author Steve Fink
- * @author Dave Grove
  * @author Perry Cheng
  * @author <a href="http://cs.anu.edu.au/~Steve.Blackburn">Steve Blackburn</a>
  */
 abstract public class BasePolicy { // implements HeaderConstants {
   
+  public final static String Id = "$Id$"; 
+
   public static void prepare(VMResource vm, MemoryResource mr) { VM._assert(false); }
   public static void release(VMResource vm, MemoryResource mr) { VM._assert(false); }
   public static VM_Address traceObject(VM_Address object) { VM._assert(false); return VM_Address.zero(); }
