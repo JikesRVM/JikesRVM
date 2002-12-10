569fbabacaee991da59cdde9802669f85e12f47b
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2002-12-10 11:20:48 +0000

    Added author tag and comments

--- rvm/src/vm/memoryManagers/JMTk/vmInterface/Monitor.java
+++ rvm/src/vm/memoryManagers/JMTk/vmInterface/Monitor.java
@@ -2,7 +2,6 @@
  * (C) Copyright Department of Computer Science,
  * Australian National University. 2002
  */
-
 package com.ibm.JikesRVM.memoryManagers.vmInterface;
 
 import com.ibm.JikesRVM.VM_Callbacks;
@@ -10,11 +9,30 @@ import com.ibm.JikesRVM.VM_PragmaInterruptible;
 import com.ibm.JikesRVM.VM_Uninterruptible;
 import com.ibm.JikesRVM.memoryManagers.JMTk.Plan;
 
-public class Monitor implements Constants, VM_Uninterruptible, VM_Callbacks.ExitMonitor {
+/**
+ * This class allows JMTk to register call backs with VM_Callbacks.
+ * 
+ * @author <a href="http://cs.anu.edu.au/~Steve.Blackburn">Steve Blackburn</a>
+ *
+ * @version $Revision$
+ * @date $Date$
+ */
+public class Monitor 
+  implements Constants, VM_Uninterruptible, VM_Callbacks.ExitMonitor {
   public final static String Id = "$Id$"; 
+
+  /**
+   * Register the exit monitor at boot time.
+   */
   public static void boot() throws VM_PragmaInterruptible {
     VM_Callbacks.addExitMonitor(new Monitor());
   }
+
+  /**
+   * The VM is about to exit.  Notify the plan.
+   *
+   * @param value The exit value
+   */
   public void notifyExit(int value) {
     Plan.notifyExit(value);
   }
