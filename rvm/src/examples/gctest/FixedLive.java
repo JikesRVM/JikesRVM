b6824485a13c42bec7fe3a7e8ace22219dd5e5a9
Perry Cheng <perryche@us.ibm.com>
2002-12-10 22:38:59 +0000

    will now exit GC portion if in danger of exceeding 350 seconds in that portion

--- rvm/src/examples/gctest/FixedLive.java
+++ rvm/src/examples/gctest/FixedLive.java
@@ -11,6 +11,7 @@ import com.ibm.JikesRVM.VM_PragmaNoInline;
 class FixedLive {
 
   static int liveSize = 0;  // in megabytes
+  static int checkFreq = 128 * 1024; // in bytes
   static Node2I2A root;
   static Node2I2A junk;
 
@@ -51,24 +52,48 @@ class FixedLive {
     }
   }
 
-  public static void showResults() {
-    double avgTraceRate = sumTraceRate / sampleCount;
-    double avgAllocRate = sumAllocRate / sampleCount;
-    double diffSquaredSumTraceRate = squaredSumTraceRate + sampleCount * (avgTraceRate * avgTraceRate) 
+    static double avgTraceRate;
+    static double avgAllocRate;
+    static double diffSquaredSumTraceRate;
+    static double diffSquaredSumAllocRate;
+    static double rmsTraceRate;
+    static double rmsAllocRate;
+    static double zTraceRate;
+    static double zAllocRate;
+
+
+  public static double chop(double x) {
+    return ((int) (1000 * x) + 0.5) / 1000;
+  }
+
+  public static void updateStats() {
+    avgTraceRate = sumTraceRate / sampleCount;
+    avgAllocRate = sumAllocRate / sampleCount;
+    diffSquaredSumTraceRate = squaredSumTraceRate + sampleCount * (avgTraceRate * avgTraceRate) 
       - 2 * avgTraceRate * sumTraceRate;
-    double rmsTraceRate = Math.sqrt(diffSquaredSumTraceRate / sampleCount);
-    double diffSquaredSumAllocRate = squaredSumAllocRate + sampleCount * (avgAllocRate * avgAllocRate) 
+    diffSquaredSumAllocRate = squaredSumAllocRate + sampleCount * (avgAllocRate * avgAllocRate) 
       - 2 * avgAllocRate * sumAllocRate;
-    double rmsAllocRate = Math.sqrt(diffSquaredSumAllocRate / sampleCount);
-    avgTraceRate = ((int) (10000 * avgTraceRate) + 0.5) / 10000;
-    avgAllocRate = ((int) (10000 * avgAllocRate) + 0.5) / 10000;
-    rmsTraceRate = ((int) (10000 * rmsTraceRate) + 0.5) / 10000;
-    rmsAllocRate = ((int) (10000 * rmsAllocRate) + 0.5) / 10000;
+    rmsTraceRate = Math.sqrt(diffSquaredSumTraceRate / sampleCount);
+    rmsAllocRate = Math.sqrt(diffSquaredSumAllocRate / sampleCount);
+    zTraceRate = rmsTraceRate / avgTraceRate;
+    zAllocRate = rmsAllocRate / avgAllocRate;
+    avgTraceRate = chop(avgTraceRate);
+    avgAllocRate = chop(avgAllocRate);
+    rmsTraceRate = chop(rmsTraceRate);
+    rmsAllocRate = chop(rmsAllocRate);
+    zTraceRate = chop(rmsTraceRate);
+    zAllocRate = chop(rmsAllocRate);
+  }
+
+  public static void showResults() {
+    updateStats();
     System.out.println();
-    System.out.print("Overall Rate:           tracing  rate = " + avgTraceRate + " Mb/s");
-    System.out.println("   allocation rate = " + avgAllocRate + " Mb/s");
-    System.out.print("Standard Deviation:     tracing sigma = " + rmsTraceRate + " Mb/s");
-    System.out.println("   allocation sigma = " + rmsAllocRate + " Mb/s");
+    System.out.print("Overall Rate:           tracing    rate = " + avgTraceRate + " Mb/s");
+    System.out.println("   allocation     rate = " + avgAllocRate + " Mb/s");
+    System.out.print("Standard Deviation:     tracing   sigma = " + rmsTraceRate + " Mb/s");
+    System.out.println("   allocation    sigma = " + rmsAllocRate + " Mb/s");
+    System.out.print("Z Score:                tracing z-score = " + zTraceRate);
+    System.out.println("           allocation z-score = " + zAllocRate);
     System.out.println("Total Allocation Time = " + sumAllocTime + " s");
     System.out.println("Total Tracing    Time = " + sumTraceTime + " s");
   }
@@ -80,21 +105,24 @@ class FixedLive {
       junk = new Node2I2A();
   }
 
-  public static void allocateDie(int maxGC, int maxMb) {
+  public static void allocateDie(int maxGC, int maxMb, double maxTime) {
 
     int count = maxMb * ((1 << 20) / Node2I2A.objectSize);
-    System.out.println("Allocating " + maxMb + " Mb or until " + maxGC + " GCs have occurred.  First " + exclude + "GCs are excluded");
+    long firstStart = System.currentTimeMillis();
+    System.out.println("Allocating " + maxMb + " Mb or until " + maxGC + " GCs have occurred or until we would exceed " + maxTime + " secs.");
+    System.out.println("First " + exclude + "GC's are excluded from overall statistics\n");
 
-    int checkFreq = 64000 / Node2I2A.objectSize;
+    int checkFreqObj = checkFreq / Node2I2A.objectSize;
     long last = System.currentTimeMillis();
     double allocatedSize = 0;
-    for (int i=0; i< count / checkFreq && sampleCount < maxGC; i++) {
+    for (int i=0; i< count / checkFreqObj && sampleCount < maxGC; i++) {
       long start = System.currentTimeMillis();
-      allocateLoop(checkFreq);
-      allocatedSize += checkFreq * Node2I2A.objectSize;
+      allocateLoop(checkFreqObj);
+      allocatedSize += checkFreq;
       long end = System.currentTimeMillis();
       double traceElapsed = (end - start) / 1000.0;
       double allocElapsed = (start - last) / 1000.0;
+      double totalElapsed = traceElapsed + allocElapsed;
       if (traceElapsed > 0.1) {
 	double traceRate = liveSize / traceElapsed; // Mb/s
 	double allocRate = (allocatedSize / 1e6) / allocElapsed; // Mb/s
@@ -102,22 +130,24 @@ class FixedLive {
 	allocatedSize = 0;
 	last = end;
       }
+      if (0.001 * (end - firstStart) + totalElapsed > maxTime)
+	break;
     }
     showResults();
   }
 
   public static void runTest() throws Throwable {
 
-    System.out.println("FixedLive running with " + liveSize + " Mb fixed live data");
+    System.out.println("FixedLive running with " + liveSize + " Mb fixed live data\n");
     
     Node2I2A.computeObjectSize();
     System.out.println("Estimated object size of a 4-field object (2 int, 2 ref) is " + Node2I2A.objectSize + " bytes");
 
     int count = (int) (liveSize << 20) / Node2I2A.objectSize;
-    System.out.println("Creating tree with " + count + " nodes");
+    System.out.println("Creating live data: tree with " + count + " nodes");
     root = Node2I2A.createTree(count);
 
-    allocateDie(5, 2048);
+    allocateDie(5, 2048, 350.0);
   }
 
 
