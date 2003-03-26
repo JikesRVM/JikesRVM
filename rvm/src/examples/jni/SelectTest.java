07a68bb85a41b985c23c6be64e47aa2ea758bce8
Dave Grove <groved@us.ibm.com>
2003-03-26 14:45:41 +0000

    simple test for intercepting selects

---
+++ rvm/src/examples/jni/SelectTest.java
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$ 
+/**
+ * Simple test to see if hijacking of select works.
+ * 
+ * @author Dave Grove
+ */
+class SelectTest {
+
+  public static native void doit();
+
+  public static void main(String args[]) {
+    System.loadLibrary("SelectTest");
+    doit();
+  }
+}
