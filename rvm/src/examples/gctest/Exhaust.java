00aa4c649fe0ef57ca7e66803425b9691351363b
Perry Cheng <perryche@us.ibm.com>
2003-04-16 19:56:59 +0000

    test OutOfMemory when we implement feature

---
+++ rvm/src/examples/gctest/Exhaust.java
@@ -0,0 +1,44 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$
+/*
+ * @author Perry Cheng
+ */
+
+import com.ibm.JikesRVM.VM_PragmaNoInline;
+import java.lang.*;
+
+class Exhaust {
+
+  static int itemSize = 4 * 1024; 
+  static int metaSize = 1024 * 1024;
+  static Object [] junk;
+  static int cursor = 0;
+
+  public static void main(String args[])  throws Throwable {
+
+    runTest();
+
+    System.exit(0);
+  }
+
+  public static void runTest() throws Throwable {
+
+    for (int i=0; i<10; i++) {
+	junk = new Object[metaSize];
+	while (true) {
+	    try {
+		junk[cursor++] = new byte[itemSize];
+	    }
+	    catch (OutOfMemoryError e) {
+		System.out.println("Caught OutOfMemory - freeing now");
+		junk = null;  // kills everything
+		cursor = 0;
+		break;
+	    }
+	}
+    }
+  }
+
+}
