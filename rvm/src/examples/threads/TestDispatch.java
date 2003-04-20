a35808f5ef4af136445fce5397f0d99db709a8bc
Perry Cheng <perryche@us.ibm.com>
2003-04-20 01:30:50 +0000

    parameterizable; better output

--- rvm/src/examples/threads/TestDispatch.java
+++ rvm/src/examples/threads/TestDispatch.java
@@ -5,19 +5,33 @@
 /**
  * @author unascribed
  */
-class TestDispatch
-   {
-   public static void 
-   main(String args[])
-      throws Exception
-      {
-      System.out.println("TestDispatch");
-      TestDispatchWorker a = new TestDispatchWorker("ping"); a.start();
-      TestDispatchWorker b = new TestDispatchWorker("                   pong"); b.start();
-      while (!a.isFinished && !b.isFinished)
-         Thread.currentThread().yield();
-      TestDispatchWorker.say("main", "bye");
-      }
+class TestDispatch {
+
+   static public int threadCount = 2;
+   static public TestDispatchWorker [] workers = new TestDispatchWorker[threadCount];
+
+   public static void main(String args[]) throws Exception {
+       
+     System.out.println("TestDispatch");
+     for (int i=0; i<threadCount; i++) {
+	 String name = "    worker " + i;
+	 for (int j=0; j<i; j++)
+	     name = "                    " + name;   
+	 workers[i] = new TestDispatchWorker(name);
+     }
+     for (int i=0; i<threadCount; i++) 
+	 workers[i].start();
+     boolean done = false;
+     while (!done) {
+	 Thread.currentThread().yield();
+	 done = true;
+	 for (int i=0; i<threadCount; i++) 
+	     if (!workers[i].isFinished) 
+		 done = false;
+     }
+     TestDispatchWorker.say("main", "bye");
    }
+
+}
 
 
