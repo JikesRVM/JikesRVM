1f8cdb75034900cd74659c8a25de46f476fc9a8a
Dave Grove <groved@us.ibm.com>
2003-04-23 13:22:11 +0000

    format

--- rvm/src/vm/VM_SizeConstants.java
+++ rvm/src/vm/VM_SizeConstants.java
@@ -14,17 +14,16 @@ package com.ibm.JikesRVM;
  */
 public interface VM_SizeConstants {
 
-    static final int LOG_BITS_IN_BYTE = 3;
-    static final int BITS_IN_BYTE = 1<<LOG_BITS_IN_BYTE;
+  public static final int LOG_BITS_IN_BYTE = 3;
+  public static final int BITS_IN_BYTE = 1<<LOG_BITS_IN_BYTE;
 
-    static final int LOG_BYTES_IN_INT = 2;
-    static final int BYTES_IN_INT = 1<<LOG_BYTES_IN_INT;
-    static final int LOG_BITS_IN_INT = LOG_BITS_IN_BYTE + LOG_BYTES_IN_INT;
-    static final int BITS_IN_INT = 1<<LOG_BITS_IN_INT;
+  public static final int LOG_BYTES_IN_INT = 2;
+  public static final int BYTES_IN_INT = 1<<LOG_BYTES_IN_INT;
+  public static final int LOG_BITS_IN_INT = LOG_BITS_IN_BYTE + LOG_BYTES_IN_INT;
+  public static final int BITS_IN_INT = 1<<LOG_BITS_IN_INT;
 
-    static final int LOG_BYTES_IN_ADDRESS = VM.BuildFor64Bit ? 3 : 2;
-    static final int BYTES_IN_ADDRESS = 1<<LOG_BYTES_IN_ADDRESS;
-    static final int LOG_BITS_IN_ADDRESS = LOG_BITS_IN_BYTE + LOG_BYTES_IN_ADDRESS;
-    static final int BITS_IN_ADDRESS = 1<<LOG_BITS_IN_ADDRESS;
-
+  public static final int LOG_BYTES_IN_ADDRESS = VM.BuildFor64Bit ? 3 : 2;
+  public static final int BYTES_IN_ADDRESS = 1<<LOG_BYTES_IN_ADDRESS;
+  public static final int LOG_BITS_IN_ADDRESS = LOG_BITS_IN_BYTE + LOG_BYTES_IN_ADDRESS;
+  public static final int BITS_IN_ADDRESS = 1<<LOG_BITS_IN_ADDRESS;
 }
