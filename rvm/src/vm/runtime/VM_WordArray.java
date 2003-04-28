a84afdae7dd147c62fe3be36e7bb0ab9dcc0d205
Dave Grove <groved@us.ibm.com>
2003-04-28 13:25:03 +0000

    initialize backing [] during bootimage writing; allow set operations with any WordType

--- rvm/src/vm/runtime/VM_WordArray.java
+++ rvm/src/vm/runtime/VM_WordArray.java
@@ -21,6 +21,10 @@ final public class VM_WordArray implements VM_Uninterruptible {
 
   private VM_WordArray (int size) throws VM_PragmaInterruptible {
     data = new VM_Word[size];
+    VM_Word zero = VM_Word.zero();
+    for (int i=0; i<size; i++) {
+      data[i] = zero;
+    }
   }
 
   public VM_Word get (int index) throws VM_PragmaInline {
@@ -31,6 +35,21 @@ final public class VM_WordArray implements VM_Uninterruptible {
   public void set (int index, VM_Word v) throws VM_PragmaInline {
     if (VM.runningVM || VM.writingImage) VM._assert(false);  // should be hijacked
     data[index] = v;
+  }
+
+  public void set (int index, VM_Address v) throws VM_PragmaInline {
+    if (VM.runningVM || VM.writingImage) VM._assert(false);  // should be hijacked
+    data[index] = v.toWord();
+  }
+
+  public void set (int index, VM_Offset v) throws VM_PragmaInline {
+    if (VM.runningVM || VM.writingImage) VM._assert(false);  // should be hijacked
+    data[index] = v.toWord();
+  }
+
+  public void set (int index, VM_Extent v) throws VM_PragmaInline {
+    if (VM.runningVM || VM.writingImage) VM._assert(false);  // should be hijacked
+    data[index] = v.toWord();
   }
 
   public int length() throws VM_PragmaInline {
