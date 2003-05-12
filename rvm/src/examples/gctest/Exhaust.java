890c6a58e464740c3781c97eac37d2286032038e
Perry Cheng <perryche@us.ibm.com>
2003-05-12 23:43:06 +0000

    rewrote to not tickle opt-compiler bug

--- rvm/src/examples/gctest/Exhaust.java
+++ rvm/src/examples/gctest/Exhaust.java
@@ -26,7 +26,7 @@ class Exhaust {
     System.exit(0);
   }
 
-  public static void runTest() throws Throwable {
+  public static void runTestOrig() throws Throwable {
 
     double growthFactor = 1.0;
     for (int i=1; i<=10; i++) {
@@ -51,6 +51,38 @@ class Exhaust {
     }
     System.out.println("Overall: SUCCESS");
   }
+
+  public static int doInner (int size) throws Throwable, VM_PragmaNoInline {
+      for (int j=0; j<metaSize; j++) {
+	  junk[cursor++] = new byte[(int) size];
+	  size *= growthFactor;
+      }
+      return size;
+  }
+
+  public static void runTest() throws Throwable, VM_PragmaNoInline {
+
+    double growthFactor = 1.0;
+    for (int i=1; i<=10; i++) {
+	growthFactor *= metaGrowthFactor;
+	growthFactor = ((int) (100 * growthFactor)) / 100.0;
+	System.out.println("Starting round " + i + " with growthFactor " + growthFactor);
+	junk = new Object[metaSize];
+	System.out.println("  Allocating until exception thrown");
+      int size = itemSize;
+      try {
+	  size = doInner(size);
+      }
+      catch (OutOfMemoryError e) {
+	  junk = null;  // kills everything
+	  cursor = 0;
+	  System.out.println("  Caught OutOfMemory - freeing now");  // this allocates; must follow nulling
+	  System.out.println("  Maximum size reached is " + size);
+      }
+    }
+    System.out.println("Overall: SUCCESS");
+  }
+
 
 }
 
