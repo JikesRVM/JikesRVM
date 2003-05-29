cc55647da9d37810f1a9d7e3d7cef45db040803f
Perry Cheng <perryche@us.ibm.com>
2003-05-29 01:22:48 +0000

    remove unneeded Buf class

--- rvm/src/examples/jni/t3GT3Worker.java
+++ rvm/src/examples/jni/t3GT3Worker.java
@@ -13,39 +13,49 @@
 
 class t3GT3Worker extends Thread {
 
-  int        reps, length, arg3;
-  boolean    isReady;
-  boolean    isFinished;
-  
-  t3GT3Worker(int reps, int length)
-  {
+  int        reps, length;
+  boolean    isReady = false;
+  boolean    isFinished = false;
+  Object     syncher;
+
+  t3GT3Worker(int reps, int length, Object o) {
     this.reps = reps;
     this.length = length;
-    this.isFinished = false;
-    this.isReady    = false;
+    syncher = o;
   }
   
-  public void start() //- overrides Thread
-  {
+  public void start() { //- overrides Thread
     super.start();
   }
   
-  public void run()  //- overrides Thread
-  {
-		isReady = true;
-		synchronized (t3GT3.syncher) {
-		try {
-		t3GT3.syncher.wait();
-		}
-		catch (InterruptedException e)
-		{
-		}
-		}
-		t3GTGC.runit(reps,length);
+  public void run() { //- overrides Thread
+      isReady = true;
+      synchronized (syncher) {
+	  try {
+	      syncher.wait();
+	  }
+	  catch (InterruptedException e) {
+	  }
+      }
+      allocate(reps,length);
       isFinished = true;
-		synchronized (t3GT3.syncher2) {
-		t3GT3.syncher2.notify();
-  	}
+      synchronized (t3GT3.syncher2) {
+	  t3GT3.syncher2.notify();
+      }
   }
+
+
+   public static void allocate (int reps, int length) {
+
+       for (int i = 0; i < reps; i++) {
+	   char buf [] = new char[0];
+	   for (int j = 0; j < length; j++) {
+               char newbuf [] = new char[buf.length + 1];
+	       System.arraycopy(buf, 0, newbuf, 0, buf.length);
+	       newbuf[buf.length] = 'x';
+	       buf = newbuf;
+	   }
+       }
+   }
 }
 
