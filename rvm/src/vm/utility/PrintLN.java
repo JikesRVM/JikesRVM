e923cfc1fe608c3418122d3c6b5e131bd9f4fd9d
Steven Augart <augart@watson.ibm.com>
2003-05-29 20:42:19 +0000

    A PrintLN interface.  The interface implemented by PrintContainer

---
+++ rvm/src/vm/utility/PrintLN.java
@@ -0,0 +1,22 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$
+package com.ibm.JikesRVM;
+// import com.ibm.JikesRVM.PrintLN;
+import java.io.PrintWriter;
+import java.io.PrintStream;
+
+/**
+ * This interface is implemented by com.ibm.JikesRVM.PrintContainer.  The
+ * interfaces is used by our java.lang.Throwable to print stack traces.
+ *
+ * @author Steven Augart (w/ brainstorming by David Grove)
+ */
+public interface PrintLN {
+  //  PrintLN(PrintWriter out);
+  //  PrintLN(PrintStream out);
+  void print(String s);
+  void println(String s);
+}
+
