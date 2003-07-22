be7795b1cff602cca22bd5fc870bc073c647a82b
Steven Augart <augart@watson.ibm.com>
2003-07-22 00:19:39 +0000

    Fixed multiple bugs.  This now does what it was probably intended to do.  It also runs in a fraction of the time; it no longer perpetually allocates 32-byte blocks.

--- rvm/src/examples/gctest/Exhaust.java
+++ rvm/src/examples/gctest/Exhaust.java
@@ -28,10 +28,19 @@ class Exhaust {
   static double growthFactor = 1.0;
   static double metaGrowthFactor = 1.1;
   static int rounds = 10;
+  static long tot = 0;		// total # allocated
+  static long wasAllocating;		// How much were we allocating?
+  
 
   public static void main(String args[])  //throws Throwable 
   {
-
+    long mhs = Options.getMaxHeapSize();
+    
+    o.println("Max heap size: " + mhs + " bytes");
+    if (mhs > 1024 * 1024)
+      o.println("  that's " + mhs / ( 1024.0 * 1024.0 ) + " megabytes");
+    if (mhs > 1024 * 1024 * 1024)
+      o.println("  that's " + mhs / (1024.0 * 1024 * 1024) + " gigabytes");
     runTest();
 
     System.exit(0);
@@ -45,38 +54,52 @@ class Exhaust {
       if (cursor >= metaSize) {
 	o.println("cursor >= metaSize; we won't allocate more memory.  This should never happen.");
       }
+      wasAllocating = size;
       junk[cursor++] = new byte[(int) size];
+      tot += size;
+      wasAllocating = 0;
+      o.println("growthFactor = " + growthFactor);
       size *= growthFactor;
+      o.println("Size = " + size);
     }
     return size;
   }
 
   public static void runTest() 
     throws // Throwable, 
-	   VM_PragmaNoInline 
+      VM_PragmaNoInline 
   {
-
-    double growthFactor = 1.0;
+    // Whoops!   This does not do what we thought it would; hides the global.
+    // double growthFactor = 1.0;
     for (int i=1; i<=10; i++) {
-	growthFactor *= metaGrowthFactor;
-	growthFactor = ((int) (100 * growthFactor)) / 100.0;
-	o.println("Starting round " + i + " with growthFactor " + growthFactor);
-	junk = new Object[metaSize];
-	o.println("  Allocating until exception thrown");
+      growthFactor *= metaGrowthFactor;
+      growthFactor = ((int) (100 * growthFactor)) / 100.0;
+      o.println("Starting round " + i + " with growthFactor " + growthFactor);
+      o.println("Allocating an array with room for " + metaSize + " objects");
+	
+      junk = new Object[metaSize];
+      tot = 0;
+	
+      o.println("  Allocating until exception thrown");
       int size = itemSize;
       try {
-	  size = doInner(size);
+	size = doInner(size);
       }
       catch (OutOfMemoryError e) {
-	  junk = null;  // kills everything
-	  cursor = 0;
-	  System.out.println("  Caught OutOfMemory - freeing now");  // this allocates; must follow nulling
-	  System.out.println("  Maximum size reached is " + size);
+	junk = null;  // kills everything
+	cursor = 0;
+	o.println("  Caught OutOfMemory - freeing now");  // this allocates; must follow nulling
+
+	//	  o.println("  Maximum size reached is " + size);
+
+	o.println("  Had " + tot + " bytes allocated; failed trying to allocate " + wasAllocating + " bytes");
+      }
+      if (junk != null) {
+	junk = null;
+	o.println("  Had " + tot + " bytes allocated; kept on allocating until we couldn't go any further; this should never happen.");
       }
     }
     System.out.println("Overall: SUCCESS");
   }
-
-
 }
 
