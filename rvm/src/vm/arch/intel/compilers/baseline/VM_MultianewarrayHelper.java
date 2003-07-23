eed3deb8acc7755f57ce2aff34d8b66492abac3d
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2003-07-23 02:42:51 +0000

    Fixed minor typo

--- rvm/src/vm/arch/intel/compilers/baseline/VM_MultianewarrayHelper.java
+++ rvm/src/vm/arch/intel/compilers/baseline/VM_MultianewarrayHelper.java
@@ -23,12 +23,13 @@ public class VM_MultianewarrayHelper {
   /**
    * Allocate something like "new Foo[cnt0][cnt1]...[cntN-1]",
    *                      or "new int[cnt0][cnt1]...[cntN-1]".
-   * @param methodId      id of caller
+   * @param methodId method id of caller
    * @param numDimensions number of array dimensions
-   * @param typeId        id of type reference for array
-   * @param argOffset     position of word *above* `cnt0' argument within caller's frame
-   *                      This is used to access the number of elements to 
-   *                      be allocated for each dimension.
+   * @param typeId type id of type reference for array
+   * @param argOffset position of word *above* `cnt0' argument within
+   * caller's frame This is used to access the number of elements to
+   * be allocated for each dimension.
+   *
    * See also: bytecode 0xc5 ("multianewarray") in VM_Compiler
    */
   static Object newArrayArray (int methodId, int numDimensions, int typeId, int argOffset)
@@ -53,7 +54,7 @@ public class VM_MultianewarrayHelper {
     
     // create array
     //
-    VM_TypeReference tRef = VM_TypeReference.getTypeRef(methodId);
+    VM_TypeReference tRef = VM_TypeReference.getTypeRef(typeId);
     VM_Array array = tRef.resolve().asArray();
     return VM_Runtime.buildMultiDimensionalArray(methodId, numElements, array);
   }
