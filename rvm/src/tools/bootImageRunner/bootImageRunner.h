679c6433cd5e864272857591921216de05112d5f
Steven Augart <augart@watson.ibm.com>
2003-08-18 19:03:42 +0000

    Added VmProcessorIdKey, IsVmProcessorKey to bootImageRunner.h; got rid of (slightly dangerous) extern declarations in syswrap.C.

--- rvm/src/tools/bootImageRunner/bootImageRunner.h
+++ rvm/src/tools/bootImageRunner/bootImageRunner.h
@@ -13,6 +13,10 @@
 //
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif    
+
 extern FILE *SysErrorFile;    // sink for serious error messages
 extern FILE *SysErrorFile;	// libvm.C
 // extern int SysErrorFd;	// in IA32 libvm.C, not in powerpc.
@@ -42,30 +46,39 @@ extern unsigned maximumHeapSize;
 extern int verboseBoot;
 
 /* Defined in libvm.C; used in RunBootImage.C */
-extern "C" int createJVM(int);
+extern int createJVM(int);
 /* Used in libvm.C; Defined in sys.C */
-extern "C" int getArrayLength(void* ptr);
+extern int getArrayLength(void* ptr);
 
 /* sys.C and RunBootImage.C */
-extern "C" void findMappable();
+extern void findMappable(void);
 
 #if defined(RVM_FOR_POWERPC)
-/* Used in libvm.C, sys.C.  Declared in assembly code.; */
-extern "C" void bootThread(int jtoc, int pr, int ti_or_ip, int fp); // assembler routine
+/* Used in libvm.C, sys.C.  Defined in assembly code: */
+extern void bootThread(int jtoc, int pr, int ti_or_ip, int fp); // assembler routine
 #elif defined(RVM_FOR_IA32)
-extern "C" int bootThread(int ti_or_ip, int jtoc, int pr, int sp); // assembler routine
+extern int bootThread(int ti_or_ip, int jtoc, int pr, int sp); // assembler routine
 #else
-#error "Warning: Undefined configuration: neither IA32 nor POWERPC: won't declare bootThread()"
+// Some files (e.g., syswrap.C) don't need bootThread's prototype declared,
+// but do need a few other prototypes here.  So we don't make it an error:
+// #error "Warning: Undefined configuration: neither IA32 nor POWERPC: won't declare bootThread()"
 #endif
 
 // These are defined in libvm.C.
 #if !defined(RVM_WITHOUT_INTERCEPT_BLOCKING_SYSTEM_CALLS)
-extern "C" void *getJTOC();
-extern "C" int getProcessorsOffset();
+extern void *getJTOC(void);
+extern int getProcessorsOffset(void);
 #endif // RVM_WITHOUT_INTERCEPT_BLOCKING_SYSTEM_CALLS
 
-// Defined in sys.C.; used in libvm.C
-extern "C" void sysSyncCache(caddr_t, int size);
-// Defined in libvm.C.  Used in sys.C.
-extern "C" void processTimerTick();
+/* These are defined in sys.C; used in syswrap.C */
+extern pthread_key_t VmProcessorIdKey;
+extern pthread_key_t IsVmProcessorKey;
 
+// Defined in sys.C.; used in libvm.C
+extern void sysSyncCache(caddr_t, int size);
+// Defined in libvm.C.  Used in sys.C.
+extern void processTimerTick(void);
+
+#ifdef __cplusplus
+}
+#endif    
