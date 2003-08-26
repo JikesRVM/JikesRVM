647c522940799493d30ca5bbe5f706013ce39cdb
Steven Augart <augart@watson.ibm.com>
2003-08-26 23:48:13 +0000

    Added implementations of several functions, so that our inheritors do not have to override them.

--- rvm/src/vm/utility/PrintLN.java
+++ rvm/src/vm/utility/PrintLN.java
@@ -22,15 +22,64 @@ import java.io.PrintStream;
 public abstract class PrintLN {
   //  PrintLN(PrintWriter out);
   //  PrintLN(PrintStream out);
-  public abstract boolean isVMSysWriteln();
-  public abstract boolean isSystemErr();
+  public boolean isVMSysWriteln() { return false; };
+  public boolean isSystemErr() { return false; };
   public abstract void flush();
   
   public abstract void println();
+  public void println(String s) {
+    print(s);
+    println();
+  }
   public abstract void print(String s);
-  public abstract void println(String s);
-  public abstract void print(int i);
-  public abstract void printHex(int i);
+
+  /* Here, we are writing code to make sure that we do not rely upon any
+   * external memory accesses. */
+  // largest power of 10 representable as a Java integer.
+  // (max int is            2147483647)
+  final int max_int_pow10 = 1000000000;
+
+  public void print(int n) {
+    boolean suppress_leading_zero = true;
+    if (n == 0x80000000) {
+      print("-2147483648");
+      return;
+    } else if (n == 0) {
+      print('0');
+      return;
+    } else if (n < 0) {
+      print('-');
+      n = -n;
+    }     
+    /* We now have a positive # of the proper range.  Will need to exit from
+       the bottom of the loop. */
+    for (int p = max_int_pow10; p >= 1; p /= 10) {
+      int digit = n / p;
+      n -= digit * p;
+      if (digit == 0 && suppress_leading_zero)
+	continue;
+      suppress_leading_zero = false;
+      char c = (char) ('0' + digit);
+      print(c);
+    }
+  }
+
+  public void printHex(int n) {
+    print("0x");
+    // print exactly 8 hexadec. digits.
+    for (int i = 32 - 4; i >= 0; i -= 4) {
+      int digit = (n >>> i) & 0x0000000F;		// fill with 0 bits.
+      char c;
+
+      if (digit <= 9) {
+	c = (char) ('0' + digit);
+      } else {
+	c = (char) ('A' + (digit - 10));
+      }
+      print(c);
+    }
+  }
+
   public abstract void print(char c);
   /* Code related to VM_Atom.classNameFromDescriptor() */
   public void print(VM_Class class_) {
@@ -68,7 +117,21 @@ public abstract class PrintLN {
     //    }
 
 
-  public abstract void print(VM_Member m);
-  public abstract void print(VM_Atom a);
+  /* Here we need to imitate the work that would normally be done by
+   * VM_Member.toString() (which VM_Method.toString() inherits) */
+  public void print(VM_Member m) {
+    print(m.getDeclaringClass()); // VM_Class
+    print('.');
+    print(m.getName());
+    print(' ');
+    print(m.getDescriptor());
+  }
+
+  public void print(VM_Atom a) {
+    byte[] val = a.toByteArray();
+    for (int i = 0; i < val.length; ++i) {
+      print((char) val[i]);
+    }
+  }
 }
 
