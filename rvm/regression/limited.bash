b36e15df4fdea78a89140b57d6499ec078c47cc3
Steven Augart <augart@watson.ibm.com>
2003-10-10 17:56:59 +0000

    Now conforms more to our other shell scripts in style (ME for the name of this command, etc.)
    Added more flags: -v (verbose), which can be repeated, and:
          $ME showkids.  Nice tracing of how it works.
    
    New function, list_recursive_kids, that no longer runs ps repeatedly
    (just calls ps or pstree once).

--- rvm/regression/limited.bash
+++ rvm/regression/limited.bash
@@ -1,60 +1,91 @@
-#! /bin/bash
+#! /usr/bin/env bash
 #
 # (C) Copyright IBM Corp. 2003
 #
 # $Id$
 #
 # @author Julian Dolby
-# @modiifed Steven Augart, to make it run on Bash and be fully compatible
-# with the existing limited script.
+# @modiifed Steven Augart
 
-basename="${0##*/}"
+ME="${0##*/}"
+allkids_awk=$RVM_ROOT/rvm/regression/allkids.awk
+mypath=$RVM_ROOT/rvm/regression/$ME
 
+# This does not seem to change whether the "Killed" message is output:
+# set +o notify
 usage () {
     exit_status=${1-1};
     shift;
     ## Go to stderr unless exiting with status zero.
-    (( $1 == 0 )) || exec >&2
-    [ "$*" ] && echo "$basename: $*"
+    (( $exit_status == 0 )) || exec >&2
+    [ "$*" ] && echo "$ME: $*"
     cat <<- EOF
-	usage: $basename [ -- ] <seconds> <command>
-	   or: $basename kill <PID> ...
-	   or: $basename [ -h | --help ]
+	usage: $ME [-v | --verbose] [ -- ] <seconds> <command>
+	   or: $ME [-v | --verbose] kill <PID> ...
+	   or: $ME [ -h | --help ]
 	       The first form runs the given command for at most 'seconds' seconds.
 	       The second form kills the children of the given PID(s)
 	       recursively, i.e. it also kills all their sub-processes.
 	       It does not kill the <PID> itself (for historical compat.)
 
 	  The -- flag has the usual meaning of signaling the end of the flags..
+	  The -v flag means to be verbose.
 
+	For debugging, you can also use:
+	    $ME showkids <PID>...
+			List children of <PID>...
 EOF
     exit $exit_status
 }
     
+## List all of the kids recursively, if you can.  If not, return unsuccessful
+## exit status.
+function list_recursive_kids () {
+    if type pstree &> /dev/null; then
+	# Extract just the numbers and make them into a newline-separated list.
+	pstree -p "$@" | sed -e 's/[^0-9]\+/ /g' -e 's/  //g' -e 's/  //g' -e 's/^ //' -e 's/ $//' -e '/^$/d' | tr ' ' '\n'
+    else
+	show_process_table | awk -f $allkids_awk "$@"
+    fi
+}
+
+function show_process_table () {
+    if [[ $(uname) == Darwin ]]; then
+	ps -wwajx
+    else
+	ps -ef
+    fi
+}
+
+
 ## Auxiliary functions
-function kill_children_of () {
-    [[ "$*" ]] || usage 1 "No pids specified to kill the kids of"
-    for pid; do
-	kill -STOP $pid
-	if [[ `uname` == Darwin ]]; then
-	    local children="$(ps -wwajx | awk '$3~/'$pid'/{print $2}')"
-	else
-	    local children="$(ps -ef | awk '$3~/'$pid'/{print $2}')"
-	fi
-	if [ "$children" ]; then
-	    kill_pids $children
-	    kill -9 $children
-	fi
-    done
+function kill_recursively () {
+    [[ "$*" ]] || usage 1 "No pids specified to kill recursively"
+
+    kill -STOP $*		# STOP them all from spawning
+    all_kids="$(list_recursive_kids $*)" || echo >&2 "$ME: Trouble getting a list of the spawn of $*.  We'll try with what we have."
+    if [[ "$all_kids" != *[0-9]* ]] ; then
+	echo >&2 "$ME: Can't find any of $* and her/their spawn to kill (maybe they died of natural causes while they were on death row)?"
+	return 1;
+    fi
+    (( verbose )) && echo "$ME: Killing" $all_kids
+    (( verbose > 1 )) && ps l $all_kids
+    kill -9 $all_kids
 }
 
 
 ## Arg parsing
 
 
+declare -i verbose=0
+
 while :; do
     if [[ $1 = "-h" || $1 = "--help" ]]; then
+	shift
 	usage 0;
+    elif [[ $1 = "-v" || $1 = "--verbose" ]]; then
+	shift
+	let ++verbose
     elif [[ $1 = -- ]]; then
 	shift
 	break
@@ -67,13 +98,46 @@ done
 
 (( $# >= 2 )) || usage 1 "too few arguments"
 
+(( verbose > 1 )) && echo "$ME: My PID is $$"
+
+declare -i executioner_started=0
+
+function executioner_alarm() {
+    echo >&2 "$ME: OVERTIME ($timeout seconds have passed) executing ${worker} and spawn (command: $*)"
+    let ++executioner_started
+    kill_recursively $worker
+}
+
+function executioner_interrupt() {
+    (( $verbose )) && echo "$ME: INTERRUPTED!  Executing ${worker} and spawn (command: $*)..."
+    let ++executioner_started
+    kill_recursively $worker
+    (( $verbose )) && echo 'executed.'
+}
+
 if [[ $1 = "kill" ]]; then
     shift;
-    kill_children_of "$@";
+    kill_recursively "$@";
+elif [[ $1 = "showkids" ]]; then
+    shift;
+    list_recursive_kids "$@"
 else
     # Run with a timeout.
     timeout="$1"
     shift
-    ulimit -t $timeout
-    exec "$@"
+    "$@" &
+    worker=$!
+    (( verbose )) && echo "$ME: Worker pid is $worker"
+    trap "executioner_alarm '$*'" ALRM
+    trap "executioner_interrupt '$*'" INT QUIT HUP TERM
+    ( sleep $timeout ; kill -ALRM $$) &
+    executioner=$!
+    (( verbose )) && echo "$ME: Executioner pid is $executioner"
+    wait $worker
+    worker_xitstatus=$?
+#    if (( worker_xitstatus != 128 + 9 )); then
+    if (( ! executioner_started )); then
+	(( verbose )) && echo "$ME: worker $worker (running $*) exited with status $worker_xitstatus; let's kill the executioner $executioner"
+	kill_recursively $executioner
+    fi
 fi
