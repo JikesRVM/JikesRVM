cad691a41d069a75be29a607fc21824c56dd4136
Steven Augart <augart@watson.ibm.com>
2003-10-11 03:49:20 +0000

    Skeleton file to use to begin a new Java source file.  Contains an IBM copyright notice, an @author header, and a CVS Id tag.

---
+++ rvm/aux_/Skeleton.java
@@ -0,0 +1,15 @@
+/* -*-coding: iso-8859-1 -*-
+ *
+ * (C) Copyright © IBM Corp 2003
+ *
+ * $Id$
+ */
+package com.ibm.JikesRVM;       // FILL ME IN with the appropriate package.
+
+import com.ibm.JikesRVM.classloader.VM_ClassLoader; // FILL ME IN 
+
+/** TODO Substitute a brief description of what this program or library does.
+ *
+ * @author Your Name Here
+ * @date 11 October 2003
+ */
