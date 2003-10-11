f4ac8c3bf6a30eb22c708f13ee06407f08b08620
Steven Augart <augart@watson.ibm.com>
2003-10-11 03:42:06 +0000

    Skeleton file to use to begin a new Bash shell script.  Contains an IBM copyright notice and an @author header.

---
+++ rvm/aux_/skeleton.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+#
+# (C) Copyright IBM Corp. 2003
+#
+# $Id$
+#
+# Substitute a brief description of what this program or library does.
+#
+# @author Your Name Here
+# @date 11 October 2003
