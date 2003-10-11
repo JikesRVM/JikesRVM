ddc28b60d6245a7166896488988abe474a90dc21
Steven Augart <augart@watson.ibm.com>
2003-10-11 03:54:01 +0000

    Skeleton file to use to begin a new C++ source file.  Contains an IBM copyright notice, an @author header, and a CVS Id tag.

---
+++ rvm/aux_/skeleton.C
@@ -0,0 +1,12 @@
+/* -*-coding: iso-8859-1 -*-
+ *
+ * (C) Copyright © IBM Corp 2003
+ *
+ * $Id$
+ */
+
+/** FILL ME IN with a brief description of what this file does.
+ *
+ * @author Your Name Here
+ * @date 11 October 2003
+ */
