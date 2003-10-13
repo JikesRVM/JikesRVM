fb8c2df9d533c9fb20319e096f08a1da835d8069
Dave Grove <groved@us.ibm.com>
2003-10-13 19:57:24 +0000

    get makefile working again

--- rvm/src/vm/arch/intel/disassembler/IntelDisassembler.java
+++ rvm/src/vm/arch/intel/disassembler/IntelDisassembler.java
@@ -13,20 +13,10 @@
  * @author Ton Ngo 
  * @date 2/12/2001 
  */
-
 public class IntelDisassembler {
 
-  public static byte[] intToByteArray(int[] intArray) {
-    byte byteArray[] = new byte[intArray.length*4];
-    for (int i=0; i<intArray.length; i++) {
-      int word = intArray[i];
-      int offset = i*4;
-      byteArray[offset]   = (byte) ( word & 0x000000FF);
-      byteArray[offset+1] = (byte) ((word & 0x0000FF00) >> 8);
-      byteArray[offset+2] = (byte) ((word & 0x00FF0000) >> 16);
-      byteArray[offset+3] = (byte) ((word & 0xFF000000) >> 24);
-    }
-    return byteArray;
+  static {
+    System.loadLibrary("IntelDisassembler");
   }
 
   /**
@@ -34,7 +24,6 @@ public class IntelDisassembler {
    * -if count is nonzero, disassemble up to count
    * -if count is zero, disassemble until there is no more valid 
    *  instructions in the buffer
-   * 
    */
   public static native String disasm(byte[] instr, int count, int address);
 
@@ -43,32 +32,14 @@ public class IntelDisassembler {
   }
 
   /**
-   * Convenience methods:  convert from int to byte
-   */
-  public static String disasm(int instrArray[], int address) {
-    return disasm(intToByteArray(instrArray), 0, address);
-  }
-
-  public static String disasm(int instrArray[], int count, int address) {
-    return disasm(intToByteArray(instrArray), count, address);
-  }
-
-
-  /**
    * Compute the instruction length for a block of instruction
-   * 
-   *
    */
   public static native byte[] instructionLength(byte[] instr);
-  public static byte[] instructionLength(int instrArray[]) {
-    return instructionLength(intToByteArray(instrArray));
-  }
-
 
   /**
    * Test if this is a CALL instruction
    */
-  public static boolean isCallInstruction(int instrArray[]) {
+  public static boolean isCallInstruction(byte[] instrArray) {
     String instructionString = disasm(instrArray, 1, 0);
     if (instructionString.indexOf("CALL") == -1)
       return false;
@@ -82,11 +53,6 @@ public class IntelDisassembler {
   public static native int getBranchTarget(byte instrArray[], int[] regs);
   // just a way to return more info from decoding the branch target
   public static native boolean isLastAddressIndirect();   
-
-  public static int getBranchTarget(int instrArray[], int[] regs) {
-    byte bArray[] = intToByteArray(instrArray);
-    return getBranchTarget(bArray, regs);
-  }
 
   /**
    * for stand alone testing 
@@ -111,18 +77,10 @@ public class IntelDisassembler {
     (byte) 0x08, (byte) 0xc9, (byte) 0xc3, (byte) 0x90, 
     (byte) 0x90};
 
-  // static byte testInstructions[] = {(byte) 0x55  };
-
-
   // for testing 
   public static void main(String args[]) {
-
-    System.loadLibrary("IntelDisassembler");
     System.out.println("Disassemble Intel: " + testInstructions.length +
 		       " bytes");
     System.out.println("return mnemonic: " + disasm(testInstructions));
-    
   }
-
-
 }
