76ac1548e1e91faf8367675ceb47b32df3854b7e
Steven Augart <augart@watson.ibm.com>
2003-10-19 22:50:25 +0000

    Accidentally checked it in; whoops!

--- rvm/src/tools/configuring/config.h
+++
@@ -1,5 +0,0 @@
-/* HAVE_STRTOLD: Do we have a working strtold() function?  AIX 5.1 
-   does not declare strtold() in <stdlib.h> unless 
-   sizeof (long double) > sizeof (double).   Note that C '99 requires 
-   that function to be present. */
-#define HAVE_STRTOLD 1
