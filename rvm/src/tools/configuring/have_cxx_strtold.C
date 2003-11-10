59a41b6b71661cb25f4d7336fd522d3ec27e8ceb
Steven Augart <augart@watson.ibm.com>
2003-11-10 23:33:01 +0000

    We do not define #define _ISOC99_SOURCE, nor _ISOC9X_SOURCE any more; they were breaking the build on Red Hat Linux 8 by making caddr_t unavailable (!).

--- rvm/src/tools/configuring/have_cxx_strtold.C
+++ rvm/src/tools/configuring/have_cxx_strtold.C
@@ -1,9 +1,3 @@
-#define _ISOC99_SOURCE          /* make it clear to slightly old versions of
-                                   the GNU C library that we want C99
-                                   features, specifically strtold() */
-#define _ISOC9X_SOURCE          // For even older versions of GNU C library
-
-
 extern long double dummy(void);
 #include <stdlib.h>
 
