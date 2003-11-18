47464e9dadf341e208798379da5c832deb283f99
Dave Grove <groved@us.ibm.com>
2003-11-18 20:14:13 +0000

    64bit bug fix

--- rvm/src/vm/arch/powerPC/runtime/VM_DynamicLinkerHelper.java
+++ rvm/src/vm/arch/powerPC/runtime/VM_DynamicLinkerHelper.java
@@ -24,8 +24,8 @@ class VM_DynamicLinkerHelper implements VM_Constants, VM_Uninterruptible {
     VM_Address callingFrame = VM_Magic.getCallerFramePointer(VM_Magic.getFramePointer());
     callingFrame = VM_Magic.getCallerFramePointer(callingFrame);
     callingFrame = VM_Magic.getCallerFramePointer(callingFrame);
-    VM_Address location = callingFrame.sub((LAST_NONVOLATILE_FPR - FIRST_VOLATILE_FPR + 1) * 8 + 
-					   (LAST_NONVOLATILE_GPR - FIRST_VOLATILE_GPR + 1) * 4); 
+    VM_Address location = callingFrame.sub((LAST_NONVOLATILE_FPR - FIRST_VOLATILE_FPR + 1) * BYTES_IN_DOUBLE + 
+					   (LAST_NONVOLATILE_GPR - FIRST_VOLATILE_GPR + 1) * BYTES_IN_ADDRESS); 
     
     return VM_Magic.addressAsObject(VM_Magic.getMemoryAddress(location));
   }
