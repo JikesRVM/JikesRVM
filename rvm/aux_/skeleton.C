4b20f8e67bc3afcd93ef278d895414adac03df82
Steven Augart <augart@watson.ibm.com>
2003-11-21 16:13:44 +0000

    Added comment.

--- rvm/aux_/skeleton.C
+++ rvm/aux_/skeleton.C
@@ -5,7 +5,8 @@
  * $Id$
  */
 
-/** FILL ME IN with a brief description of what this file does.
+/** Skeleton file for new Jikes RVM C and C++ code.
+ * FILL ME IN with a brief description of what this file does.
  *
  * @author Your Name Here
  * @date 11 October 2003
