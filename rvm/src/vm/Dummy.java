a1c05b683965deb45a077ce7716e269d761e355c
Dave Grove <groved@us.ibm.com>
2004-01-16 21:54:16 +0000

    don't need explicit reference to VM_Runtime

--- rvm/src/vm/Dummy.java
+++ rvm/src/vm/Dummy.java
@@ -19,7 +19,6 @@ class Dummy {
   static VM                         a;
   static com.ibm.JikesRVM.classloader.VM_TableBasedDynamicLinker b;
   static VM_DynamicLinker           c;
-  static VM_Runtime                 d;
   static VM_Reflection              e;
   static VM_Process                 f;
   static com.ibm.JikesRVM.jni.BuildJNIFunctionTable g;
