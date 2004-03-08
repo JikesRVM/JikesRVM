55c65ec76a593f1e7483da63afa30e4e68010cc4
Steven Augart <augart@watson.ibm.com>
2004-03-08 16:04:31 +0000

    1) Corrected typo for "Separate". 2) VM_SystemClassLoader => VM_BootstrapClassLoader

--- rvm/src/vm/compilers/baseline/GCMap/VM_ReferenceMaps.java
+++ rvm/src/vm/compilers/baseline/GCMap/VM_ReferenceMaps.java
@@ -286,7 +286,7 @@ public final class VM_ReferenceMaps implements VM_BaselineConstants, VM_Uninterr
    *       when a jsr map is being scanned.
    *       This shoulkd be a low probability event.
    *
-   * NOTE: return addresses are handled seperately from references because they point
+   * NOTE: return addresses are handled separately from references because they point
    *       inside an object ( internal pointers)
    *
    * Return NOMORE when no
@@ -352,7 +352,7 @@ public final class VM_ReferenceMaps implements VM_BaselineConstants, VM_Uninterr
     return bytesPerMap();
   }
 
-  private static final VM_TypeReference TYPE = VM_TypeReference.findOrCreate(VM_SystemClassLoader.getVMClassLoader(),
+  private static final VM_TypeReference TYPE = VM_TypeReference.findOrCreate(VM_BootstrapClassLoader.getVMClassLoader(),
                                                                              VM_Atom.findOrCreateAsciiAtom("Lcom/ibm/JikesRVM/VM_ReferenceMaps;"));
   int size() throws VM_PragmaInterruptible {
     int size = TYPE.peekResolvedType().asClass().getInstanceSize();
