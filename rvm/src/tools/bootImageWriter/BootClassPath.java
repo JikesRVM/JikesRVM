7b6468b6c94491a808561f4ac4cc32f32d1c21cf
Steven Augart <augart@watson.ibm.com>
2004-03-10 18:06:29 +0000

    Decided to keep this in my private tools instead; not generally useful enough.

--- rvm/src/tools/bootImageWriter/BootClassPath.java
+++
@@ -1,8 +0,0 @@
-/** This is a public domain routine to print out the Boot Class Path. */
-public class BootClassPath {
-  private BootClassPath() {}
-
-  public static void main(String[] args) {
-    System.out.println(System.getProperty("sun.boot.class.path"));
-  }
-}
