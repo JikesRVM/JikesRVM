5d2ba3c245ecdeceb6d2f16dc20e5e07ad94462e
Steven Augart <augart@watson.ibm.com>
2004-04-24 03:28:10 +0000

    Fixed the @USER_MANUAL@ expansion. <p> Added a @VERSION@ expansion.

--- rvm/src/tools/install/macro-expand.sh
+++ rvm/src/tools/install/macro-expand.sh
@@ -3,7 +3,12 @@
 [ $# = 2 ] || { echo >&2 "Usage: $0 SRC DEST" ; exit 2 ; }
 src="$1"
 dest="$2"
-sed	-e 's,@USER_MANUAL@,@DOC_DIR,g'				\
+
+#	-e 's,,,'			\
+#
+
+sed	-e 's,@USER_MANUAL@,@DOC_DIR@,g'			\
+        -e 's,@VERSION@,2.3.2+CVS,g'				\
 	-e 's,@DOC_DIR@,/usr/share/doc/jikesrvm,g'		\
 	-e 's,@MAN_DIR@,/usr/share/man,g'			\
 	-e 's,@BIN_DIR@,/usr/bin,g'				\
