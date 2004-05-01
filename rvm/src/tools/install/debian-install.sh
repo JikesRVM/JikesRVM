e5ddf12aec8e69d21f1d548dfb59a729b5f41c20
Steven Augart <augart@watson.ibm.com>
2004-05-01 13:40:09 +0000

    Fixed warning about no @author tag.

--- rvm/src/tools/install/debian-install.sh
+++ rvm/src/tools/install/debian-install.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
+# -*- coding: iso-8859-1 ; mode: shell-script ;-*-
+# (C) Copyright © IBM Corp. 2004
+#
 # $Id$
-# Install Jikes RVM on a Debian system.
+#
+# Install Jikes RVM in /usr (/usr/bin, /usr/lib/jikesrvm,
+# /usr/share/jikesrvm, /usr/share/doc/jikesrvm, /usr/man/man1) on a
+# system.
+#
+# @author Steven Augart
+# @date 28 April 2004
+# 
 unset makeflag
 unset verbose
 [[ $1 = -v ]] && verbose=1
