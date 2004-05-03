1ca25db7652db8779db3be469de8160cc377badc
Dave Grove <groved@us.ibm.com>
2004-05-03 11:42:11 +0000

    reformatted; updated to classpath 0.09 (they removed the constructor for InetAddress that this code was using

--- rvm/src/vm/libSupport/java/net/JikesRVMSupport.java
+++ rvm/src/vm/libSupport/java/net/JikesRVMSupport.java
@@ -4,36 +4,36 @@
 //$Id$
 package java.net;
 import com.ibm.JikesRVM.VM_SizeConstants;
+
 /**
  * Library support interface of Jikes RVM
  *
  * @author Julian Dolby
- *
  */
-public class JikesRVMSupport implements VM_SizeConstants{
+public class JikesRVMSupport implements VM_SizeConstants {
 
-    private static byte[] toArrayForm(int address) {
-        byte[] addr = new byte[4];
-        addr[0] = (byte)((address>>(3*BITS_IN_BYTE)) & 0xff);
-        addr[1] = (byte)((address>>(2*BITS_IN_BYTE)) & 0xff);
-        addr[2] = (byte)((address>>BITS_IN_BYTE) & 0xff);
-        addr[3] = (byte)(address & 0xff);
-        return addr;
-    }
+  private static byte[] toArrayForm(int address) {
+    byte[] addr = new byte[4];
+    addr[0] = (byte)((address>>(3*BITS_IN_BYTE)) & 0xff);
+    addr[1] = (byte)((address>>(2*BITS_IN_BYTE)) & 0xff);
+    addr[2] = (byte)((address>>BITS_IN_BYTE) & 0xff);
+    addr[3] = (byte)(address & 0xff);
+    return addr;
+  }
 
-    public static InetAddress createInetAddress(int address) {
-        return new InetAddress( toArrayForm(address) );
-    }
+  public static InetAddress createInetAddress(int address) {
+    return new InetAddress(toArrayForm(address), null, null);
+  }
     
-    public static InetAddress createInetAddress(int address, String hostname) {
-        return new InetAddress(toArrayForm(address), hostname);
-    }
+  public static InetAddress createInetAddress(int address, String hostname) {
+    return new InetAddress(toArrayForm(address), hostname, null);
+  }
     
-    public static int getFamily(InetAddress inetaddress) {
-        return inetaddress.family;
-    }
+  public static int getFamily(InetAddress inetaddress) {
+    return inetaddress.family;
+  }
     
-    public static void setHostName(InetAddress inetaddress, String hostname) {
-        inetaddress.hostName = hostname;
-    }
+  public static void setHostName(InetAddress inetaddress, String hostname) {
+    inetaddress.hostName = hostname;
+  }
 }
