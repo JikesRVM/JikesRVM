da7e28e33f8e207a06220e348d02b36099c257bf
Steven Augart <augart@watson.ibm.com>
2004-05-07 20:08:53 +0000

    Added EXIT_STATUS_BOGUS_COMMAND_LINE_ARG, so that the option-parsing code in sys.C can exit with the proper status in case of trouble.

--- rvm/src/tools/bootImageRunner/sys.h
+++ rvm/src/tools/bootImageRunner/sys.h
@@ -10,17 +10,24 @@
  * @date December 29, 2003
  */
 
-// These exit status codes are defined here and in VM.java.
-// If you change one of them here, or add any, add it there too.
-// See VM.java; vm.exitStatusSyscallTrouble
+/* These exit status codes are defined here and in VM.java.
+   If you change one of them here, or add any, add it there too. */
+
+/* See VM.java; VM.exitStatusSyscallTrouble */
 const int EXIT_STATUS_SYSCALL_TROUBLE = 121;
-// See VM.java; vm.exitStatusTimerTrouble
+
+/* See VM.java; VM.exitStatusTimerTrouble */
 const int EXIT_STATUS_TIMER_TROUBLE = EXIT_STATUS_SYSCALL_TROUBLE;
 
+/* See VM.java: VM.exitStatusUnexpectedCallToSys and
+ * VM.exitStatusUnsupportedInternalOp */
 const int EXIT_STATUS_UNSUPPORTED_INTERNAL_OP = 120;
-
-// see VM.exitStatusUnexpectedCallToSys:
 const int EXIT_STATUS_UNEXPECTED_CALL_TO_SYS
             = EXIT_STATUS_UNSUPPORTED_INTERNAL_OP;
+
+/* See VM.exitStatusBogusCommandLineArg in VM.java.  
+ * If you change this value, change it there too.
+ * This is used in sys.C and in RunBootImage.C*/
+const int EXIT_STATUS_BOGUS_COMMAND_LINE_ARG = 98;
 
 
