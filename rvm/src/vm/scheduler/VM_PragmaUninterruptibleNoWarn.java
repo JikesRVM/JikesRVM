415f9fd162565c33e35839872232e17d60d7d723
Dave Grove <groved@us.ibm.com>
2004-08-16 14:59:17 +0000

    Initial Revision

---
+++ rvm/src/vm/scheduler/VM_PragmaUninterruptibleNoWarn.java
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright IBM Corp. 2002, 2004
+ */
+//$Id$
+package com.ibm.JikesRVM;
+
+import com.ibm.JikesRVM.classloader.*;
+
+/**
+ * A pragma that has the same direct effect as VM_PragmaUninterruptible
+ * but also suppresses checking of uninterruptibility violations for
+ * the method.  This should be used with care and is only justified when 
+ * Uninterruptibility is ensured via some other mechansism. 
+ * For example, the method explicitly disables threadswitching
+ * around the interruptible regions (VM.sysWrite on String).  
+ * Or the interruptible regions are not reachable when the VM is 
+ * running (various VM.sysWrite that check VM.runningVM).
+ *
+ * @author Dave Grove
+ */
+public class VM_PragmaUninterruptibleNoWarn extends VM_PragmaException {
+  private static final VM_TypeReference me = getTypeRef("Lcom/ibm/JikesRVM/VM_PragmaUninterruptibleNoWarn;");
+  public static boolean declaredBy(VM_Method method) {
+    return declaredBy(me, method);
+  }
+}
