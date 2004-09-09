cd915d998080b6a988b3074c8e5a41c9e3fbff21
Dave Grove <groved@us.ibm.com>
2004-09-09 13:00:32 +0000

    exapnd comment to discuss uninterruptible vs unpreemtible

--- rvm/src/vm/vmmagic/pragma/Uninterruptible.java
+++ rvm/src/vm/vmmagic/pragma/Uninterruptible.java
@@ -11,13 +11,23 @@ package org.vmmagic.pragma;
  *     emitted in the method prologue is omitted.
  * (2) the stack overflow test that would be emitted
  *     in the method prologue is omitted.
- *
- * <P>
+ * <p>
+ * Uninterruptible and {@link Unpreemptible} have the same direct effect on
+ * the generated code.  The difference is that Uninterruptible
+ * indicates a stronger invariant: It is a programming error (and will
+ * be reported as such) for Uninterruptible code to contain any
+ * bytecodes that could cause a loss of control. Furthermore,
+ * Uninterruptible code will be generated assuming no
+ * RuntimeExceptions are raised and without any GC maps (since by
+ * definition there can be noGC if control is not lost). Unpreemtible
+ * code will have GC maps for all potential GC points and may contain
+ * places where a thread explicitly yields.
+ * <p>
  * You can use {@link UninterruptiblePragma} and
  * {@link InterruptiblePragma} to control
  * this property at a per-method granularity.
  * <P>
- * There is no matching <code>VM_Interruptible</code> pseudo-interface, 
+ * There is no matching <code>Interruptible</code> pseudo-interface, 
  * since that is the default.
  * 
  * @author Bowen Alpern
