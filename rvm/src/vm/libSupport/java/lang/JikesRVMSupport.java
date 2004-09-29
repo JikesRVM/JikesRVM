77daf2c1415329f0c49facb4d835fc76d64f8e6c
Steven Augart <augart@watson.ibm.com>
2004-09-29 02:48:45 +0000

    Added support for Jikes RVM using GNU Classpath's java.lang.Class, either with Classpath 0.11 or with Classpath 0.10.

--- rvm/src/vm/libSupport/java/lang/JikesRVMSupport.java
+++ rvm/src/vm/libSupport/java/lang/JikesRVMSupport.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp 2002
+ * (C) Copyright IBM Corp 2002, 2004
  */
 //$Id$
 package java.lang;
@@ -18,16 +18,58 @@ import org.vmmagic.pragma.*;
 public class JikesRVMSupport {
 
   public static Class createClass(VM_Type type) {
+    //-#if RVM_WITH_OWN_JAVA_LANG_CLASS
     return Class.create(type);
+    //-#else
+    // return new Class((Object) VMClass.create(type));
+    return createClass(type, null);
+    //-#endif
+  }
+
+  public static Class createClass(VM_Type type, ProtectionDomain pd) {
+    //-#if RVM_WITH_OWN_JAVA_LANG_CLASS
+    Class c = Class.create(type);
+    setClassProtectionDomain(c, pd);
+    return c;
+    //-#else
+      //-#if RVM_WITH_CLASSPATH_0_11_OR_LATER
+      return new Class((Object) VMClass.create(type), pd);
+      //-#else
+      /* Classpath 0.10 doesn't seem to have any way to actually set the
+         ProtectionDomain.  Ugly. */
+        //-#if RVM_WITH_CLASSPATH_0_10_OR_LATER
+        return new Class((Object) VMClass.create(type));
+        //-#else
+        // The constructor in 0.08 takes a VMClass, not an Object.
+        return new Class(VMClass.create(type));
+        //-#endif
+      //-#endif
+    //-#endif
   }
 
   public static VM_Type getTypeForClass(Class c) {
+  //-#if RVM_WITH_OWN_JAVA_LANG_CLASS
     return c.type;
+  //-#else
+    //-#if RVM_WITH_CLASSPATH_0_10_OR_LATER
+    VMClass vc = (VMClass) c.vmdata;
+    //-#else
+    VMClass vc = (VMClass) c.vmClass;
+    //-#endif
+    return vc.type;
+  //-#endif
   }
 
+  //-#if RVM_WITH_OWN_JAVA_LANG_CLASS
+  /** XXX This isn't needed under GNU Classpath --- we properly want to
+      create this at the time that we create the Class structure.  That's
+      because the ProtectionDomain associated with a class is a final field in
+      GNU Classpath 0.11's implementation.  */
   public static void setClassProtectionDomain(Class c, ProtectionDomain pd) {
     c.pd = pd;
   }
+  //-#endif
+  
 
   public static char[] getBackingCharArray(String str) throws UninterruptiblePragma {
     return str.value;
