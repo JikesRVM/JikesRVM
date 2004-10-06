b380a362eb13cf2d8cb9570d313096a1ee7d7c24
Steven Augart <augart@watson.ibm.com>
2004-10-06 17:38:58 +0000

    Fix for ! RVM_WITH_OWN_JAVA_LANG_CLASS

--- rvm/src/vm/libSupport/java/lang/JikesRVMSupport.java
+++ rvm/src/vm/libSupport/java/lang/JikesRVMSupport.java
@@ -40,12 +40,9 @@ public class JikesRVMSupport {
       //-#else
       /* Classpath 0.10 doesn't seem to have any way to actually set the
          ProtectionDomain.  Ugly. */
-        //-#if RVM_WITH_CLASSPATH_0_10_OR_LATER
-        return new Class((Object) VMClass.create(type));
-        //-#else
-        // The constructor in 0.08 takes a VMClass, not an Object.
-        return new Class(VMClass.create(type));
-        //-#endif
+      Class c = new Class((Object) VMClass.create(type));
+      setClassProtectionDomain(c, pd);
+      return c;
       //-#endif
     //-#endif
   }
@@ -64,15 +61,21 @@ public class JikesRVMSupport {
   }
 
   //-#if RVM_WITH_OWN_JAVA_LANG_CLASS
-  /** XXX This isn't needed under GNU Classpath --- we properly want to
+  /** XXX This isn't needed under GNU Classpath 0.11 --- we properly want to
       create this at the time that we create the Class structure.  That's
       because the ProtectionDomain associated with a class is a final field in
       GNU Classpath 0.11's implementation.  */
   public static void setClassProtectionDomain(Class c, ProtectionDomain pd) {
     c.pd = pd;
   }
+  //-#else
+  //-#if RVM_WITH_CLASSPATH_0_10_OR_LATER && !RVM_WITH_CLASSPATH_0_11_OR_LATER
+  /** However, under Classpath 0.10, we don't set the ProtectionDomain, and
+   * that might be necessary.  We have to use an ugly reflection hack, though,
+   * to use Classpath's java.lang.Class, since the field is private. */
+  VM_Entrypoints.javaLangClassProtectionDomain.setObjectValueUnchecked(c, pd);
   //-#endif
-  
+  //-#endif
 
   /***
    * String stuff
