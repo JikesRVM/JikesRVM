b9b5207d07293549fdf96432076113ff6e4d6348
Steven Augart <augart@watson.ibm.com>
2004-11-02 00:33:11 +0000

    Fixed typo.

--- rvm/src/vm/scheduler/VM_Scheduler.java
+++ rvm/src/vm/scheduler/VM_Scheduler.java
@@ -190,7 +190,7 @@ public class VM_Scheduler implements VM_Constants, Uninterruptible {
   {
     VM_Thread vt = threads[PRIMORDIAL_THREAD_INDEX];
     
-    vt.setJavaThread(java.lang.JikesRVMSupport.createThread(vt, "Jikes_RVM_Boot_Thread"));
+    vt.setJavaLangThread(java.lang.JikesRVMSupport.createThread(vt, "Jikes_RVM_Boot_Thread"));
   }
 
   /**
