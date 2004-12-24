ac81f3f33cdbd3d3425c66ac5a72a1d864256dd3
Dave Grove <groved@us.ibm.com>
2004-12-24 13:21:17 +0000

    kludge from Chris Hoffman to fix build problems with CopyMS on OSX/Liunx PPC.  Hope for a better fix from Steve/Perry in the New Year

--- MMTk/src/org/mmtk/plan/CopyMS.java
+++ MMTk/src/org/mmtk/plan/CopyMS.java
@@ -69,9 +69,17 @@ public class CopyMS extends StopTheWorldGC implements Uninterruptible {
   public static final int ALLOCATORS = ALLOC_MS + 1;
 
   // spaces
-  private static MarkSweepSpace msSpace = new MarkSweepSpace("ms", DEFAULT_POLL_FREQUENCY, (float) 0.6);
+  //-#if RVM_FOR_POWERPC
+  private static final float MS_FRACTION= (float)0.37;
+  private static final float NURSERY_FRACTION= (float)0.09;
+  //-#else
+  private static final float MS_FRACTION= (float)0.6;
+  private static final float NURSERY_FRACTION= (float)0.15;
+  //-#endif
+
+  private static MarkSweepSpace msSpace = new MarkSweepSpace("ms", DEFAULT_POLL_FREQUENCY, MS_FRACTION);
   private static final int MS = msSpace.getDescriptor();
-  protected static CopySpace nurserySpace = new CopySpace("nursery", DEFAULT_POLL_FREQUENCY, (float) 0.15, true, false);
+  protected static CopySpace nurserySpace = new CopySpace("nursery", DEFAULT_POLL_FREQUENCY, NURSERY_FRACTION, true, false);
   protected static final int NS = nurserySpace.getDescriptor();
 
   /****************************************************************************
