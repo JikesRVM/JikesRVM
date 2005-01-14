b1c910633b4eb24cea82c077d8764936bfb7cd1f
Steven Augart <augart@watson.ibm.com>
2005-01-14 05:23:52 +0000

    Trivial Javadoc improvements.

--- rvm/src/vm/compilers/optimizing/optimizations/ipa/summaries/OPT_ClassSummary.java
+++ rvm/src/vm/compilers/optimizing/optimizations/ipa/summaries/OPT_ClassSummary.java
@@ -9,14 +9,14 @@ import com.ibm.JikesRVM.classloader.*;
 
 /**
  * Hold semantic information about a class that is not defined in
- * VM_Class.
+ * {@link VM_Class}.
  * 
  * @author Stephen Fink
  */
 public class OPT_ClassSummary {
 
   /**
-   * @param v lightweight class corresponding to this OPT_Class
+   * @param v lightweight class corresponding to this {@link OPT_Class}
    */
   OPT_ClassSummary (VM_Class v) {
     vmClass = v;
