96b6644b50b4b41eb67052fa7467f77c6e50bde8
Steven Augart <augart@watson.ibm.com>
2005-01-15 04:18:32 +0000

    Fixed Javadoc warning.

--- rvm/src/vm/compilers/optimizing/optimizations/ipa/summaries/OPT_ClassSummary.java
+++ rvm/src/vm/compilers/optimizing/optimizations/ipa/summaries/OPT_ClassSummary.java
@@ -4,19 +4,20 @@
 //$Id$
 package com.ibm.JikesRVM.opt;
 
-import com.ibm.JikesRVM.*;
-import com.ibm.JikesRVM.classloader.*;
+import com.ibm.JikesRVM.classloader.VM_Class;
 
 /**
  * Hold semantic information about a class that is not defined in
- * {@link VM_Class}.
+ * {@link VM_Class}.  Note: This is currently unused but is kept around
+ * in case we should have a need for it.
  * 
  * @author Stephen Fink
  */
 public class OPT_ClassSummary {
 
   /**
-   * @param v lightweight class corresponding to this {@link OPT_Class}
+   * @param v The {@link VM_Class} we want to store additional information
+   * about. 
    */
   OPT_ClassSummary (VM_Class v) {
     vmClass = v;
