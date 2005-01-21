217b939ed864e97250798bc28ff7793b152c5957
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2005-01-21 03:06:50 +0000

    Fixed 64-bit problems introduced by previous cleanup

--- MMTk/src/org/mmtk/utility/Memory.java
+++ MMTk/src/org/mmtk/utility/Memory.java
@@ -1,96 +1,211 @@
 /*
  * (C) Copyright IBM Corp. 2001
  */
-//$Id$
-
 package org.mmtk.utility;
 
 import org.mmtk.vm.Assert;
-import org.mmtk.utility.Constants;
 
 import org.vmmagic.unboxed.*;
 import org.vmmagic.pragma.*;
 
-/*
- * @author Perry Cheng  
- */  
-
+/**
+ * This class implements basic memory copying, setting and clearing
+ * operations.
+ *
+ * NOTE: Most of the operations in this class are performed at teh
+ * granularity of a Java integer (ie 4-byte units)
+ *
+ * FIXME: Why can't these operations be performed at word-granularity?
+ *
+ *  $Id$
+ *
+ * @author Perry Cheng
+ * @author <a href="http://cs.anu.edu.au/~Steve.Blackburn">Steve Blackburn</a>
+ * @version $Revision$
+ * @date $Date$
+ */
 public class Memory implements Uninterruptible, Constants {
 
-  /* Inlining this loop into the uninterruptible code can cause/encourage the
-   GCP into moving a get_obj_tib into the interruptible region where the tib
-   is being installed via an int_store 
-  */
-  private static boolean isSetHelper(Address start, int size, boolean verbose, 
-				     Word v) throws NoInlinePragma {
-    if (Assert.VERIFY_ASSERTIONS) Assert._assert((size & (BYTES_IN_WORD-1)) == 0);
-    for (int i=0; i < size; i += BYTES_IN_WORD) 
-      if (start.loadWord(Offset.fromInt(i)).NE(v)) {
+  /****************************************************************************
+   *
+   * Class variables
+   */
+
+  /** zero operations greater than this size are done using the
+   * underlying OS implementation of zero() */
+  private static final int SMALL_REGION_THRESHOLD = 1<<8; // empirically chosen
+
+
+  /****************************************************************************
+   *
+   * Basic memory setting and zeroing operations
+   */
+
+  /**
+   * Zero a region of memory
+   *
+   * @param start The start of the region to be zeroed (must be 4-byte aligned)
+   * @param bytes The number of bytes to be zeroed (must be 4-byte aligned)
+   */
+  public static void zero(Address start, Extent bytes) throws InlinePragma {
+    if (Assert.VERIFY_ASSERTIONS) {
+      assertAligned(start.toInt());
+      assertAligned(bytes.toInt());
+    }
+    if (bytes.GT(Extent.fromIntZeroExtend(SMALL_REGION_THRESHOLD))) 
+      org.mmtk.vm.Memory.zero(start, bytes);
+    else
+      zeroSmall(start, bytes);
+  }
+
+  /**
+   * Zero a page-aligned region of memory
+   *
+   * @param start The start of the region to be zeroed (must be page aligned)
+   * @param bytes The number of bytes to be zeroed (must be page aligned)
+   */
+  public static void zeroPages(Address start, int bytes) throws InlinePragma {
+    org.mmtk.vm.Memory.zeroPages(start, bytes);
+  }
+
+  /**
+   * Zero a small region of memory
+   *
+   * @param start The start of the region to be zeroed (must be 4-byte aligned)
+   * @param bytes The number of bytes to be zeroed (must be 4-byte aligned)
+   */
+  public static void zeroSmall(Address start, Extent bytes) 
+    throws InlinePragma {
+    if (Assert.VERIFY_ASSERTIONS) {
+      assertAligned(start.toInt());
+      assertAligned(bytes.toInt());
+    }
+    Address end = start.add(bytes);
+    for (Address addr = start; addr.LT(end); addr = addr.add(BYTES_IN_INT)) 
+      addr.store(0);
+  }
+
+  /**
+   * Set a region of memory
+   *
+   * @param start The start of the region to be zeroed (must be 4-byte aligned)
+   * @param bytes The number of bytes to be zeroed (must be 4-byte aligned)
+   * @param value The value to which the integers in the region should be set
+   */
+  public static void set(Address start, int bytes, int value)
+    throws InlinePragma {
+    if (Assert.VERIFY_ASSERTIONS) {
+      assertAligned(start.toInt());
+      assertAligned(bytes);
+    }
+    Address end = start.add(bytes);
+    for (Address addr = start; addr.LT(end); addr = addr.add(BYTES_IN_INT)) 
+      addr.store(value);
+  }
+
+
+  /****************************************************************************
+   *
+   * Helper methods
+   */
+
+  /**
+   * Check that a memory range is zeroed
+   *
+   * @param start The start address of the range to be checked
+   * @param bytes The size of the region to be checked, in bytes
+   * @return True if the region is zeroed
+   */
+  public static boolean IsZeroed(Address start, int bytes)
+    throws InlinePragma {
+    return isSet(start, bytes, false, 0);
+  }
+
+  /**
+   * Assert that a memory range is zeroed.  An assertion failure will
+   * occur if the region is not zeroed.
+   *
+   * this is in the inline allocation sequence when
+   * Assert.VERIFY_ASSERTIONS is true, it is carefully written to
+   * reduce the impact on code space.
+   *
+   * @param start The start address of the range to be checked
+   * @param bytes The size of the region to be checked, in bytes
+   */
+  public static void assertIsZeroed(Address start, int bytes) 
+    throws NoInlinePragma {
+    Assert._assert(isSet(start, bytes, true, 0));
+  }
+
+  /**
+   * Verbosely check and return true if a memory range is set to some
+   * integer value
+   *
+   * @param start The start address of the range to be checked
+   * @param bytes The size of the region to be checked, in bytes
+   * @param value The value to which this region should be set
+   * @return True if the region has been correctly set
+   */
+  public static boolean isSet(Address start, int bytes, int value) 
+    throws InlinePragma {
+    return isSet(start, bytes, true, value);
+  }
+
+  /**
+   * Assert appropriate alignment, triggering an assertion failure if
+   * the value does not satisify the alignment requirement of the
+   * memory operations.
+   *
+   * @param value The value to be tested
+   */
+  private static final void assertAligned(int value) {
+    Assert._assert((value & (BYTES_IN_INT-1)) == 0);
+  }
+  
+  /**
+   * Test whether a memory range is set to a given integer value
+   *
+   * @param start The address to start checking at
+   * @param bytes The size of the region to check, in bytes
+   * @param verbose If true, produce verbose output
+   * @param value The value to which the memory should be set
+   */
+  private static boolean isSet(Address start, int bytes, boolean verbose, 
+                               int value)
+    /* Inlining this loop into the uninterruptible code can
+     *  cause/encourage the GCP into moving a get_obj_tib into the
+     * interruptible region where the tib is being installed via an
+     * int_store
+     */
+    throws NoInlinePragma {
+    if (Assert.VERIFY_ASSERTIONS) assertAligned(bytes);
+    for (int i=0; i < bytes; i += BYTES_IN_INT) 
+      if (start.loadInt(Offset.fromInt(i)) != value) {
         if (verbose) {
           Log.prependThreadId();
           Log.write("Memory range does not contain only value ");
-          Log.writeln(v);
+          Log.writeln(value);
           Log.write("Non-zero range: "); Log.write(start);
-          Log.write(" .. "); Log.writeln(start.add(size));
+          Log.write(" .. "); Log.writeln(start.add(bytes));
           Log.write("First bad value at "); Log.writeln(start.add(i));
-          dumpMemory(start, 0, size);
+          dumpMemory(start, 0, bytes);
         }
         return false;
       }
     return true;
   }
 
-  public static boolean IsZeroed(Address start, int size) throws InlinePragma {
-    return isSetHelper(start, size, false, Word.zero());
+  /**
+   * Dump the contents of memory around a given address
+   *
+   * @param addr The address around which the memory should be dumped
+   * @param beforeBytes The number of bytes before the address to be
+   * included in the dump
+   * @param afterBytes The number of bytes after the address to be
+   * included in the dump
+   */
+  public static void dumpMemory(Address addr, int beforeBytes, int afterBytes)
+  {
+    org.mmtk.vm.Memory.dumpMemory(addr, beforeBytes, afterBytes);
   }
-
-  // this is in the inline allocation sequence when VM_Interface.VerifyAssertions
-  // therefore it is very carefully written to reduce the impact on code space.
-  public static void assertIsZeroed(Address start, int size) throws NoInlinePragma {
-    Assert._assert(isSetHelper(start, size, true, Word.zero()));
-  }
-
-  public static boolean assertIsSet(Address start, int size, int v) throws InlinePragma {
-    return isSetHelper(start, size, true, Word.fromInt(v));
-  }
-
-  public static void zeroSmall(Address start, Extent len) throws InlinePragma {
-    if (Assert.VERIFY_ASSERTIONS)
-      Assert._assert((len.toInt() & (BYTES_IN_WORD-1)) == 0
-                     && (start.toInt() & (BYTES_IN_WORD-1)) == 0);
-    Address end = start.add(len);
-    for (Address i = start; i.LT(end); i = i.add(BYTES_IN_WORD)) 
-      i.store(Word.zero());
-  }
-
-  public static void set (Address start, int len, int v) throws InlinePragma {
-    if (Assert.VERIFY_ASSERTIONS)
-      Assert._assert((len & (BYTES_IN_WORD-1)) == 0
-                     && (start.toInt() & (BYTES_IN_WORD-1)) == 0);
-    for (int i=0; i < len; i += BYTES_IN_WORD) 
-      start.store(Word.fromInt(v), Offset.fromInt(i));
-  }
-
-  // start and len must both be 4-byte aligned
-  //
-  public static void zero(Address start, Extent len) throws InlinePragma {
-    if (Assert.VERIFY_ASSERTIONS)
-      Assert._assert((len.toInt() & (BYTES_IN_WORD-1)) == 0
-                     && (start.toInt() & (BYTES_IN_WORD-1)) == 0);
-    if (len.GT(Extent.fromIntZeroExtend(256))) 
-      org.mmtk.vm.Memory.zero(start, len);
-    else
-      zeroSmall(start, len);
-  }
-
-  // start and len must both be OS-page aligned
-  //
-  public static void zeroPages(Address start, int len) throws InlinePragma {
-    org.mmtk.vm.Memory.zeroPages(start, len);
-  }
-
-  public static void dumpMemory(Address addr, int before, int after) {
-    org.mmtk.vm.Memory.dumpMemory(addr, before, after);
-  }
-
 }
--- rvm/src/vm/memoryManagers/JMTk/vmInterface/ScanStatics.java
+++ rvm/src/vm/memoryManagers/JMTk/vmInterface/ScanStatics.java
@@ -1,7 +1,7 @@
 /*
  * (C) Copyright IBM Corp. 2001
  */
-//$Id$
+
 
 package org.mmtk.vm;
 
@@ -20,13 +20,24 @@ import org.vmmagic.pragma.*;
 /**
  * Class that determines all JTOC slots (statics) that hold references
  *
+ * $Id$
+ *
  * @author Perry Cheng
  */  
 public class ScanStatics implements Constants {
 
+  /****************************************************************************
+   *
+   * Class variables
+   */
+
+  /** slots are integer sized, not word sized */ // FIXME: Document why.
+  private static final int LOG_BYTES_IN_SLOT = LOG_BYTES_IN_INT;
+
   /**
-   * Scan static variables (JTOC) for object references.
-   * Executed by all GC threads in parallel, with each doing a portion of the JTOC.
+   * Scan static variables (JTOC) for object references.  Executed by
+   * all GC threads in parallel, with each doing a portion of the
+   * JTOC.
    */
   public static void scanStatics (AddressDeque rootLocations) 
     throws UninterruptiblePragma {
@@ -45,75 +56,14 @@ public class ScanStatics implements Constants {
       end = start + chunkSize;
       if (end > numSlots)
         end = numSlots;  // doing last segment of JTOC
-
       for ( slot=start; slot<end; slot++ ) {
-
         if ( ! VM_Statics.isReference(slot) ) continue;
-
         // slot contains a ref of some kind.  call collector specific
         // processPointerField, passing address of reference
         //
-        rootLocations.push(slots.add(slot << LOG_BYTES_IN_ADDRESS));
-
+        rootLocations.push(slots.add(slot << LOG_BYTES_IN_SLOT));
       }  // end of for loop
-
       start = start + stride;
-
     }  // end of while loop
-
   }  // scanStatics
-
-  /*
-  static boolean validateRefs () throws UninterruptiblePragma {
-    int numSlots = VM_Statics.getNumberOfSlots();
-    Address slots = VM_Statics.getSlots();
-    boolean result = true;
-    for ( int slot=0; slot<numSlots; slot++ ) {
-      if ( ! VM_Statics.isReference(slot) ) continue;
-      Address ref = slots.add(slot << LOG_BYTES_IN_ADDRESS).loadAddress();
-      if ( (!ref.isZero()) && !VM_GCUtil.validRef(ref) ) {
-        VM.sysWrite("\nScanStatics.validateRefs:bad ref in slot "); VM.sysWrite(slot,false); VM.sysWrite("\n");
-        VM.sysWriteHex(slot); VM.sysWrite(" ");
-        VM_GCUtil.dumpRef(ref);
-        result = false;
-      }
-    }  // end of for loop
-    return result;
-  }  // validateRefs
-
-
-  static boolean validateRefs ( int depth ) throws UninterruptiblePragma {
-    int numSlots = VM_Statics.getNumberOfSlots();
-    Address slots = VM_Statics.getSlots();
-    boolean result = true;
-    for ( int slot=0; slot<numSlots; slot++ ) {
-      if ( ! VM_Statics.isReference(slot) ) continue;
-      Address ref = slots.loadAddress(slot << LOG_BYTES_IN_ADDRESS);
-      if ( ! VM_ScanObject.validateRefs( ref, depth ) ) {
-        VM.sysWrite("ScanStatics.validateRefs: Bad Ref reached from JTOC slot ");
-        VM.sysWrite(slot,false);
-        VM.sysWrite("\n");
-        result = false;
-      }
-    }
-    return result;
-  }
-
-  static void dumpRefs ( int start, int count ) throws UninterruptiblePragma {
-    int numSlots = VM_Statics.getNumberOfSlots();
-    Address slots = VM_Statics.getSlots();
-    int last     = start + count;
-    if (last > numSlots) last = numSlots;
-    VM.sysWrite("Dumping Static References...\n");
-      for ( int slot=start; slot<last; slot++ ) {
-        if ( ! VM_Statics.isReference(slot) ) continue;
-        Address ref = slots.loadAddress(slot << LOG_BYTES_IN_ADDRESS);
-        if (!ref.isZero()) {
-          VM.sysWrite(slot,false); VM.sysWrite(" "); VM_GCUtil.dumpRef(ref);
-        }
-      }  // end of for loop
-    VM.sysWrite("Done\n");
-  }  // dumpRefs
-*/
-
-}   // VM_ScanStatics
+}
