d52de5fb292dedb1252423d887cd23a45ba64c1c
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2005-01-26 20:40:51 +0000

    Fixed constants packaging

--- rvm/src/vm/memoryManagers/JMTk/gcspy/util/Util.java
+++ rvm/src/vm/memoryManagers/JMTk/gcspy/util/Util.java
@@ -6,7 +6,7 @@
 package org.mmtk.vm.gcspy;
 
 import org.mmtk.vm.Assert;
-import org.mmtk.vm.Constants;
+import org.mmtk.utility.Constants;
 import org.mmtk.utility.Log;
 
 import com.ibm.JikesRVM.VM_Magic;
