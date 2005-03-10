479fcc939085ae82f0816d7b930e44f25a6a99ab
Matthias Hauswirth <Matthias.Hauswirth@usi.ch>
2005-03-10 06:52:00 +0000

    testing CVS commit from Eclipse (1 file)

---
+++ rvm/etc/testing/expression/AbstractExpression.java
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright IBM Corp. 2004
+ */
+// $Id$
+package com.ibm.research.pe.model.metric.expression;
+
+
+/**
+ * TODO
+ *
+ * @author Matthias.Hauswirth@Colorado.EDU
+ */
+public abstract class AbstractExpression implements Expression {
+	
+	public AbstractExpression() {
+	}
+	
+}
