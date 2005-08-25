91a96d4de34f77d672a103b342b7f95d294c355d
Dave Grove <groved@us.ibm.com>
2005-08-25 20:28:13 +0000

    updates for classpath post 0.17

--- rvm/src/vm/runtime/VM_Entrypoints.java
+++ rvm/src/vm/runtime/VM_Entrypoints.java
@@ -274,9 +274,11 @@ public class VM_Entrypoints implements VM_Constants {
   public static final VM_Field classLoaderDefinedPackages =
     getField("Ljava/lang/ClassLoader;", "definedPackages", "Ljava/util/HashMap;");
 
+  //-#if RVM_WITH_CLASSPATH_0_15 || RVM_WITH_CLASSPATH_0_17
   public static final VM_Field classLoaderLoadedClasses =
     getField("Ljava/lang/ClassLoader;", "loadedClasses", "Ljava/util/HashMap;");
-
+  //-#endif
+  
 
   static {
 
