f176c095f503dcfde96b90c8ac78a0c002acdeab
Dave Grove <groved@us.ibm.com>
2005-10-28 02:03:39 +0000

    bug fix for PPC64; don't clear operand and then break without doing anything with it (used to be harmless, but semantics of getClearOperand have changed)

--- rvm/src/vm/compilers/optimizing/ir/conversions/lir2mir/OPT_ConvertLIRtoMIR.java
+++ rvm/src/vm/compilers/optimizing/ir/conversions/lir2mir/OPT_ConvertLIRtoMIR.java
@@ -222,9 +222,9 @@ final class OPT_ConvertLIRtoMIR extends OPT_OptimizationPlanCompositeElement {
           
         case FLOAT_2LONG_opcode:
           { 
+            if (VM.BuildForPowerPC && VM.BuildFor64Addr) break; // don't reduce operator -- leave for BURS
             OPT_Operand val = Unary.getClearVal(s);
             if (VM.BuildForPowerPC) {
-              if (VM.BuildFor64Addr) break;
               // NOTE: must move constants out of sysCall before we expand it.
               //       otherwise we'll have the wrong value in the JTOC register when
               //       we try to load the constant from the JTOC!
@@ -241,9 +241,9 @@ final class OPT_ConvertLIRtoMIR extends OPT_OptimizationPlanCompositeElement {
           
         case DOUBLE_2LONG_opcode:
           { 
+            if (VM.BuildForPowerPC && VM.BuildFor64Addr) break; // don't reduce operator -- leave for BURS
             OPT_Operand val = Unary.getClearVal(s);
             if (VM.BuildForPowerPC) {
-              if (VM.BuildFor64Addr) break;
               // NOTE: must move constants out of sysCall before we expand it.
               //       otherwise we'll have the wrong value in the JTOC register when
               //       we try to load the constant from the JTOC!
