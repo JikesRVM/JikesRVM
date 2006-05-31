62da8c7a2b4b05a978e0a4f7d794c0676d8245ad
Dave Grove <groved@us.ibm.com>
2006-05-31 15:39:15 +0000

    use VM_HashMap; strip comments that appear to have been improperly copied from GNU classpath code

--- rvm/src/vm/libSupport/java/lang/VMClassLoader.java
+++ rvm/src/vm/libSupport/java/lang/VMClassLoader.java
@@ -1,42 +1,7 @@
-/* VMClassLoader.java
-   required by ClassLoader
-   Copyright (C) 1998, 2001, 2002, 2004, 2005, 2006 Free Software Foundation
-   $Id$
-   
-This file is modified from that of GNU Classpath.
-
-GNU Classpath is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-GNU Classpath is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GNU Classpath; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301 USA.
-
-Linking this library statically or dynamically with other modules is
-making a combined work based on this library.  Thus, the terms and
-conditions of the GNU General Public License cover the whole
-combination.
-
-As a special exception, the copyright holders of this library give you
-permission to link this library with independent modules to produce an
-executable, regardless of the license terms of these independent
-modules, and to copy and distribute the resulting executable under
-terms of your choice, provided that you also meet, for each linked
-independent module, the terms and conditions of the license of that
-module.  An independent module is a module which is not derived from
-or based on this library.  If you modify this library, you may extend
-this exception to your version of the library, but you are not
-obligated to do so.  If you do not wish to do so, delete this
-exception statement from your version. */
-
+/*
+ * (C) Copyright IBM Corp 2002,2005,2006
+ */
+// $Id$
 
 package java.lang;
 
@@ -59,6 +24,7 @@ import java.security.ProtectionDomain;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -69,15 +35,11 @@ import com.ibm.JikesRVM.classloader.VM_BootstrapClassLoader;
 import com.ibm.JikesRVM.classloader.VM_ClassLoader;
 import com.ibm.JikesRVM.classloader.VM_Type;
 
+import com.ibm.JikesRVM.util.VM_HashMap;
+
 /**
- * java.lang.VMClassLoader is a package-private helper for VMs to implement
- * on behalf of java.lang.ClassLoader.
- *
- * @author John Keiser
- * @author Mark Wielaard (mark@klomp.org)
- * @author Eric Blake (ebb9@email.byu.edu)
- * 
- * This class has been extended based on that developed by IBM 2002, 2005
+ * Jikes RVM impl of VMClassLoader.
+ * See GNU classpath reference impl for javadoc.
  *
  * @author Julian Dolby
  * @author Ian Rogers
@@ -88,17 +50,12 @@ final class VMClassLoader
 	* A map of maps. The first map is indexed by the classloader. The
 	* map this finds then maps String class names to classes
 	*/
-  private static final HashMap loadedClasses = new HashMap();
+  private static final VM_HashMap loadedClasses = new VM_HashMap();
 
   /** packages loaded by the bootstrap class loader */
-  static final HashMap definedPackages = new HashMap();
+  private static final VM_HashMap definedPackages = new VM_HashMap();
 
   //-#if !RVM_WITH_CLASSPATH_0_90
-  /**
-   * Converts the array string of native package names to
-   * Packages. The packages are then put into the
-   * definedPackages hashMap
-   */
   static
   {
     String[] packages = getBootPackages();
@@ -132,24 +89,6 @@ final class VMClassLoader
   }
   //-#endif
   
-  /**
-   * Helper to define a class using a string of bytes. This assumes that
-   * the security checks have already been performed, if necessary.
-   *
-   * Implementations of this method are advised to consider the
-   * situation where user code modifies the byte array after it has
-   * been passed to defineClass.  This can be handled by making a
-   * private copy of the array, or arranging to only read any given
-   * byte a single time.
-   *
-   * @param name the name to give the class, or null if unknown
-   * @param data the data representing the classfile, in classfile format
-   * @param offset the offset into the data where the classfile starts
-   * @param len the length of the classfile data in the array
-   * @param pd the protection domain
-   * @return the class that was defined
-   * @throws ClassFormatError if data is not in proper classfile format
-   */
   static final Class defineClass(ClassLoader cl, String name,
                                  byte[] data, int offset, int len,
                                  ProtectionDomain pd)
@@ -157,20 +96,15 @@ final class VMClassLoader
   {
     VM_Type vmType = VM_ClassLoader.defineClassInternal(name, data, offset, len, cl);
     Class ans = vmType.createClassForType(pd);
-    HashMap mapForCL = (HashMap)loadedClasses.get(cl);
+    VM_HashMap mapForCL = (VM_HashMap)loadedClasses.get(cl);
     if (mapForCL == null) {
-      mapForCL = new HashMap();
+      mapForCL = new VM_HashMap();
       loadedClasses.put(cl, mapForCL);
     }
     mapForCL.put(name, ans);
     return ans;
   }
 
-  /**
-   * Helper to resolve all references to other classes from this class.
-   *
-   * @param c the class to resolve
-   */
   static final void resolveClass(Class c) {
     VM_Type cls = JikesRVMSupport.getTypeForClass(c);
     cls.resolve();
@@ -178,27 +112,12 @@ final class VMClassLoader
     cls.initialize();
   }
 
-  /**
-   * Helper to load a class from the bootstrap class loader.
-   *
-   * @param name the class name to load
-   * @param resolve whether to resolve it
-   * @return the class, loaded by the bootstrap classloader or null
-   * if the class wasn't found. Returning null is equivalent to throwing
-   * a ClassNotFoundException (but a possible performance optimization).
-   */
   static final Class loadClass(String name, boolean resolve)
     throws ClassNotFoundException
   {
     return VM_BootstrapClassLoader.getBootstrapClassLoader().loadClass(name, resolve);
   }
 
-  /**
-   * Helper to load a resource from the bootstrap class loader.
-   *
-   * @param name the resource to find
-   * @return the URL to the resource
-   */
   static URL getResource(String name)
   {
     Enumeration e = getResources(name);
@@ -207,15 +126,8 @@ final class VMClassLoader
     return null;
   }
 
-  /** jars from property java.boot.class.path */
-  static final HashMap bootjars = new HashMap();
+  private static final VM_HashMap bootjars = new VM_HashMap();
   
-  /**
-   * Helper to get a list of resources from the bootstrap class loader.
-   *
-   * @param name the resource to find
-   * @return an enumeration of resources
-   */
   static Enumeration getResources(String name)
   {
     StringTokenizer st = new StringTokenizer(
@@ -277,97 +189,52 @@ final class VMClassLoader
     return v.elements();
   }
 
-
-  /**
-   * Returns a String[] of native package names. The default
-   * implementation tries to load a list of package from
-   * the META-INF/INDEX.LIST file in the boot jar file.
-   * If not found or if any exception is raised, it returns
-   * an empty array. You may decide this needs native help.
-   */
-  private static String[] getBootPackages()
-  {
+  private static String[] getBootPackages() {
     URL indexList = getResource("META-INF/INDEX.LIST");
-    if (indexList != null)
-      {
-        try
-          {
-            Set packageSet = new HashSet();
-            String line;
-            int lineToSkip = 3;
-            BufferedReader reader = new BufferedReader(
-                                                       new InputStreamReader(
-                                                                             indexList.openStream()));
-            while ((line = reader.readLine()) != null)
-              {
-                if (lineToSkip == 0)
-                  {
-                    if (line.length() == 0)
-                      lineToSkip = 1;
-                    else
-                      packageSet.add(line.replace('/', '.'));
-                  }
-                else
-                  lineToSkip--;
-              }
-            reader.close();
-            return (String[]) packageSet.toArray(new String[packageSet.size()]);
+    if (indexList != null) {
+      try {
+        Set packageSet = new HashSet();
+        String line;
+        int lineToSkip = 3;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(indexList.openStream()));
+        while ((line = reader.readLine()) != null) {
+          if (lineToSkip == 0) {
+            if (line.length() == 0) {
+              lineToSkip = 1;
+            } else {
+              packageSet.add(line.replace('/', '.'));
+            }
+          } else {
+            lineToSkip--;
           }
-        catch (IOException e)
-          {
-            return new String[0];
-          }
+        }
+        reader.close();
+        return (String[]) packageSet.toArray(new String[packageSet.size()]);
+      } catch (IOException e) {
+        return new String[0];
       }
-    else
+    } else {
       return new String[0];
+    }
   }
 
 
-  /**
-   * Helper to get a package from the bootstrap class loader.
-   *
-   * @param name the name to find
-   * @return the named package, if it exists
-   */
-  static Package getPackage(String name)
-  {
+  static Package getPackage(String name) {
     return (Package)definedPackages.get(name);
   }
 
 
   
-  /**
-   * Helper to get all packages from the bootstrap class loader.  
-   *
-   * @return all named packages, if any exist
-   */
-  static Package[] getPackages()
-  {
+  static Package[] getPackages() {
     Package[] packages = new Package[definedPackages.size()];
-    definedPackages.values().toArray(packages);
+    Iterator it = definedPackages.valueIterator();
+    int idx = 0;
+    while (it.hasNext()) {
+      packages[idx++] = (Package)it.next();
+    }
     return packages;
   }
 
-  /**
-   * Helper for java.lang.Integer, Byte, etc to get the TYPE class
-   * at initialization time. The type code is one of the chars that
-   * represents the primitive type as in JNI.
-   *
-   * <ul>
-   * <li>'Z' - boolean</li>
-   * <li>'B' - byte</li>
-   * <li>'C' - char</li>
-   * <li>'D' - double</li>
-   * <li>'F' - float</li>
-   * <li>'I' - int</li>
-   * <li>'J' - long</li>
-   * <li>'S' - short</li>
-   * <li>'V' - void</li>
-   * </ul>
-   *
-   * @param type the primitive type
-   * @return a "bogus" class representing the primitive type
-   */
   static final Class getPrimitiveClass(char type) {
     VM_Type t;
     switch (type) {
@@ -404,107 +271,43 @@ final class VMClassLoader
     return t.getClassForType();
   }
 
-  /**
-   * The system default for assertion status. This is used for all system
-   * classes (those with a null ClassLoader), as well as the initial value for
-   * every ClassLoader's default assertion status.
-   *
-   * XXX - Not implemented yet; this requires native help.
-   *
-   * @return the system-wide default assertion status
-   */
-  static final boolean defaultAssertionStatus()
-  {
+  static final boolean defaultAssertionStatus() {
     return true;
   }
 
-  /**
-   * The system default for package assertion status. This is used for all
-   * ClassLoader's packageAssertionStatus defaults. It must be a map of
-   * package names to Boolean.TRUE or Boolean.FALSE, with the unnamed package
-   * represented as a null key.
-   *
-   * XXX - Not implemented yet; this requires native help.
-   *
-   * @return a (read-only) map for the default packageAssertionStatus
-   */
-  static final Map packageAssertionStatus()
-  {
+  static final Map packageAssertionStatus() {
     return new HashMap();
   }
 
-  /**
-   * The system default for class assertion status. This is used for all
-   * ClassLoader's classAssertionStatus defaults. It must be a map of
-   * class names to Boolean.TRUE or Boolean.FALSE
-   *
-   * XXX - Not implemented yet; this requires native help.
-   *
-   * @return a (read-only) map for the default classAssertionStatus
-   */
-  static final Map classAssertionStatus()
-  {
+  static final Map classAssertionStatus() {
     return new HashMap();
   }
 
-  static ClassLoader getSystemClassLoader()
-  {
+  static ClassLoader getSystemClassLoader() {
     return VM_ClassLoader.getApplicationClassLoader();
   }
 
-  /**
-   * Find the class if this class loader previously defined this class
-   * or if this class loader has been recorded as the initiating class loader
-   * for this class.
-   */
   static Class findLoadedClass(ClassLoader cl, String name) {
-    HashMap mapForCL = (HashMap)loadedClasses.get(cl);
+    VM_HashMap mapForCL = (VM_HashMap)loadedClasses.get(cl);
     if (mapForCL == null) return null;
     return (Class)mapForCL.get(name);
   }
 
   //-#if !RVM_WITH_CLASSPATH_0_90
-  /**
-   * The Instrumentation object created by the vm when agents are defined.
-   */
   static final Instrumentation instrumenter = null;
-  //-#endif
-
-  //-#if !RVM_WITH_CLASSPATH_0_90
-  /**
-   * Call the transformers of the possible Instrumentation object. This
-   * implementation assumes the instrumenter is a
-   * <code>InstrumentationImpl</code> object. VM implementors would
-   * have to redefine this method if they provide their own implementation
-   * of the <code>Instrumentation</code> interface.
-   *
-   * @param loader the initiating loader
-   * @param name the name of the class
-   * @param data the data representing the classfile, in classfile format
-   * @param offset the offset into the data where the classfile starts
-   * @param len the length of the classfile data in the array
-   * @param pd the protection domain
-   * @return the new data representing the classfile
-   */
-  static final Class defineClassWithTransformers(ClassLoader loader,
-      String name, byte[] data, int offset, int len, ProtectionDomain pd)
-  {
+  static final Class defineClassWithTransformers(ClassLoader loader, String name, byte[] data,
+                                                 int offset, int len, ProtectionDomain pd) {
     
-    if (instrumenter != null)
-      {
-        byte[] modifiedData = new byte[len];
-        System.arraycopy(data, offset, modifiedData, 0, len);
-        modifiedData =
-          ((InstrumentationImpl)instrumenter).callTransformers(loader, name,
-            null, pd, modifiedData);
+    if (instrumenter != null) {
+      byte[] modifiedData = new byte[len];
+      System.arraycopy(data, offset, modifiedData, 0, len);
+      modifiedData =
+        ((InstrumentationImpl)instrumenter).callTransformers(loader, name, null, pd, modifiedData);
         
-        return defineClass(loader, name, modifiedData, 0, modifiedData.length,
-            pd);
-      }
-    else
-      {
-        return defineClass(loader, name, data, offset, len, pd);
-      }
+      return defineClass(loader, name, modifiedData, 0, modifiedData.length, pd);
+    } else {
+      return defineClass(loader, name, data, offset, len, pd);
+    }
   }
   //-#endif
 }
