cc10d6efcb37166a44656b5562d680589c46d3ef
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2006-06-15 20:50:05 +0000

    Added new files missed in yesterday's commit of #1506411

---
+++ rvm/src/examples/gctracingtest/TracingTest.java
@@ -0,0 +1,25 @@
+public class TracingTest {
+  public static Integer[] arr;
+  
+  public static void main(String[] args) {
+    arr = new Integer[20];
+    for (int i = 0; i < 20; i++) {
+      arr[i] = new Integer(i);
+      System.out.println(arr[i].toString());
+    }
+    System.gc();
+    for (int i = 0; i < 20; i++) {
+      arr[i] = new Integer(i * 2);
+      if (i != 0)
+        System.out.println(arr[i-1].toString());
+    }
+    System.gc();
+    arr = new Integer[10];
+    for (int i = 0; i < 10; i+=2) {
+      arr[i] = new Integer(i>>1);
+      if (i != 0)
+        System.out.println(arr[i-2].toString());
+    }
+    System.out.println("TracingTest Finished");
+  }
+}
