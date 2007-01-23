53634d0bef4ee435d553299796cff623d6b33623
Peter Donald <peter@realityforge.org>
2007-01-23 14:08:57 +0000

    Rework the ForEach task so that is uses macros rather than antcall as this is a nicer implementation choice. It also makes the build file less convoluted to read.

--- tools/ant-tasks/src/org/jikesrvm/tools/ant/ForEachTask.java
+++ tools/ant-tasks/src/org/jikesrvm/tools/ant/ForEachTask.java
@@ -8,66 +8,54 @@
  */
 package org.jikesrvm.tools.ant;
 
-import java.util.ArrayList;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
-import org.apache.tools.ant.taskdefs.CallTarget;
-import org.apache.tools.ant.taskdefs.Property;
+import org.apache.tools.ant.taskdefs.MacroDef;
+import org.apache.tools.ant.taskdefs.MacroInstance;
 
 /**
  * A looping construct for ant. The task will accept a list of space separated
- * values. The task will then invoke antcall setting property with name passed in property
- * parameter to value.
+ * values. The task will then iterate over inner macro with name passed in as a parameter.
  *
  * @author Peter Donald
  */
 public class ForEachTask
     extends Task {
   private String list;
-  private String target;
   private String property;
-  private final ArrayList<Property> properties = new ArrayList<Property>();
+  private MacroDef macroDef;
 
   public void setProperty(final String property) { this.property = property; }
 
   public void setList(final String list) { this.list = list; }
 
-  public void setTarget(final String target) { this.target = target; }
-
-  public Property createParam() {
-    final Property property = new Property();
-    properties.add(property);
-    return property;
+  public MacroDef.NestedSequential createSequential() {
+    macroDef = new MacroDef();
+    macroDef.setProject(getProject());
+    return macroDef.createSequential();
   }
 
   public void execute() {
     validate();
 
+    final MacroDef.Attribute attribute = new MacroDef.Attribute();
+    attribute.setName(property);
+    macroDef.addConfiguredAttribute(attribute);
+
     final String[] values = list.split(" ");
     for (String value : values) {
-      final CallTarget a = (CallTarget) getProject().createTask("antcall");
-      a.setOwningTarget(getOwningTarget());
-      a.init();
-      a.setTarget(target);
-      a.setInheritAll(false);
-      a.setInheritRefs(false);
-      addParam(a, property, value);
-      for (final Property p : properties) {
-        addParam(a, p.getName(), p.getValue());
-      }
-      a.execute();
+      final MacroInstance i = new MacroInstance();
+      i.setProject(getProject());
+      i.setOwningTarget(getOwningTarget());
+      i.setMacroDef(macroDef);
+      i.setDynamicAttribute(property, value);
+      i.execute();
     }
-  }
-
-  private void addParam(final CallTarget a, final String key, final String value) {
-    final Property param = a.createParam();
-    param.setName(key);
-    param.setValue(value);
   }
 
   private void validate() {
     if (null == list) throw new BuildException("List not set.");
-    if (null == target) throw new BuildException("Target not set.");
+    if (null == macroDef) throw new BuildException("Must specify a sequential task to iterate over.");
     if (null == property) throw new BuildException("Property not set.");
   }
 }
