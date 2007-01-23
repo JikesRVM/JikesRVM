9a1529389e2edb544600c4224ac96ef8f3415010
Peter Donald <peter@realityforge.org>
2007-01-23 10:59:38 +0000

    Accurately record when a test goes overtime by an explicit result value. This needs a customization of the exec task to achieve this goal.

---
+++ tools/ant-tasks/src/org/jikesrvm/tools/ant/TimeoutRecordingExecTask.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Jikes RVM (http://jikesrvm.sourceforge.net).
+ * The Jikes RVM project is distributed under the Common Public License (CPL).
+ * A copy of the license is included in the distribution, and is also
+ * available at http://www.opensource.org/licenses/cpl1.0.php
+ *
+ * (C) Copyright Peter Donald. 2007
+ */
+package org.jikesrvm.tools.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.ExecTask;
+import org.apache.tools.ant.taskdefs.ExecuteWatchdog;
+import org.apache.tools.ant.taskdefs.Property;
+
+/**
+ * ExecTask extension that sets a proeprty when watchdog kills task.
+ *
+ * @author Peter Donald
+ */
+public class TimeoutRecordingExecTask
+    extends ExecTask {
+  private String timeoutProperty;
+  private ExecuteWatchdog watchdog;
+
+  public void setTimeoutProperty(final String timeoutProperty) {
+    this.timeoutProperty = timeoutProperty;
+  }
+
+  public void execute() throws BuildException {
+    super.execute();
+    if (null != watchdog && watchdog.killedProcess()) {
+      final Property property = (Property) getProject().createTask("property");
+      property.setOwningTarget(getOwningTarget());
+      property.init();
+      property.setName(timeoutProperty);
+      property.setValue("true");
+      property.execute();
+    }
+  }
+
+  protected ExecuteWatchdog createWatchdog() throws BuildException {
+    watchdog = super.createWatchdog();
+    return watchdog;
+  }
+}
