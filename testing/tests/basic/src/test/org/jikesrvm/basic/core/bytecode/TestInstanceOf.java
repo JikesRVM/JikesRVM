1224a4c84e0f7fa8477c9684280c82b498679a7e
Peter Donald <peter@realityforge.org>
2007-02-03 23:40:40 +0000

    Reduce verbosity of test

--- testing/tests/basic/src/test/org/jikesrvm/basic/core/bytecode/TestInstanceOf.java
+++ testing/tests/basic/src/test/org/jikesrvm/basic/core/bytecode/TestInstanceOf.java
@@ -19,63 +19,54 @@ class TestInstanceOf {
   static class Magic extends Science {}
 
   public static void main(String args[]) {
-
     runTest("Magic()", new Magic());
     runTest("Magic[2]", new Magic[2]);
-    runTest("Object[][]{new Magic[4],new Magic[4]}", new Object[][]{new Magic[4],new Magic[4]});
-    runTest("Magic[][]{new Magic[4],new Magic[4]}", new Magic[][]{new Magic[4],new Magic[4]});
+    runTest("Object[][]{new Magic[4],new Magic[4]}", new Object[][]{new Magic[4], new Magic[4]});
+    runTest("Magic[][]{new Magic[4],new Magic[4]}", new Magic[][]{new Magic[4], new Magic[4]});
     runTest("int[2]", new int[2]);
   }
 
   private static void runTest(final String name, final Object x3) {
-    System.out.println("Testing new " + name);
+    System.out.println("Testing " + name + " - instanceof: ");
     testInstanceOf(x3);
+    System.out.println(" casts: ");
     testCasts(x3);
+    System.out.println();
   }
 
+  @SuppressWarnings({"UnusedDeclaration", "RedundantCast"})
   private static void testCasts(final Object x) {
-    System.out.print("Cast to Object: ");
     try { final Object o = (Object) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Science: ");
     try { final Science o = (Science) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Magic: ");
     try { final Magic o = (Magic) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Object[]: ");
     try { final Object[] o = (Object[]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Magic[]: ");
     try { final Magic[] o = (Magic[]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Science[]: ");
     try { final Science[] o = (Science[]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Object[][]: ");
     try { final Object[][] o = (Object[][]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Magic[][]: ");
     try { final Magic[][] o = (Magic[][]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Science[][]: ");
     try { final Science[][] o = (Science[][]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to int[]: ");
     try { final int[] o = (int[]) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Serializable: ");
     try { final Serializable o = (Serializable) x; success(); }
     catch (final ClassCastException cce) { failure(); }
-    System.out.print("Cast to Cloneable: ");
     try { final Cloneable o = (Cloneable) x; success(); }
     catch (final ClassCastException cce) { failure(); }
   }
 
-  private static void failure() {System.out.println("Failed");}
+  private static void failure() {System.out.print("0");}
 
-  private static void success() {System.out.println("Succeeded");}
+  private static void success() {System.out.print("1");}
 
+  @SuppressWarnings({"ConstantConditions"})
   private static void testInstanceOf(final Object x) {
     io(Object.class, (x instanceof Object));
     io(Science.class, (x instanceof Science));
@@ -91,7 +82,8 @@ class TestInstanceOf {
     io(Cloneable.class, (x instanceof Cloneable));
   }
 
+  @SuppressWarnings({"UnusedDeclaration"})
   private static void io(final Class type, final boolean test) {
-    System.out.println("instanceof " + type.getName() + " = " + test);
+    System.out.print(test ? "1" : "0");
   }
 }
