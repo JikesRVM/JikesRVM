aaa190caf62014154f227fa504747f1f460b1e89
Peter Donald <peter@realityforge.org>
2007-02-11 10:02:19 +0000

    Add test to reproduce bug described in [ 1644449 ] Floating point display breaks on IA32

---
+++ testing/tests/basic/src/test/org/jikesrvm/basic/bugs/R1644449.java
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Jikes RVM (http://jikesrvm.sourceforge.net).
+ * The Jikes RVM project is distributed under the Common Public License (CPL).
+ * A copy of the license is included in the distribution, and is also
+ * available at http://www.opensource.org/licenses/cpl1.0.php
+ *
+ * (C) Copyright Peter Donald. 2007
+ */
+package test.org.jikesrvm.basic.bugs;
+
+/**
+ * [ 1644449 ] Floating point display breaks on IA32.
+ *
+ * @author Peter Donald
+ */
+public class R1644449 {
+  public static float b = 0.009765625f;
+  public static float a = 10.57379f;
+
+  public static void main(String[] args) {
+    System.out.println(((float) (a * b)));
+  }
+}
