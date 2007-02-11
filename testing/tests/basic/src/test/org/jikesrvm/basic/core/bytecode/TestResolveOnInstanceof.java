9475c9deae2b691d7fe94f2f14348c38d63141a2
Peter Donald <peter@realityforge.org>
2007-02-11 09:42:13 +0000

    Add in some unit tests to check that types are resolved at the correct location.
    [ 1147107 ]  identifies a situation in which the RVM does not behave in the correct manner and this failure is identified in the test TestResolveOnInvokeInterface

---
+++ testing/tests/basic/src/test/org/jikesrvm/basic/core/bytecode/TestResolveOnInstanceof.java
@@ -0,0 +1,24 @@
+package test.org.jikesrvm.basic.core.bytecode;
+
+/**
+ * According the the definition of instanceof the method must be resolved prior to any other checks.
+ * See http://java.sun.com/docs/books/jvms/second_edition/html/Instructions2.doc6.html#instanceof.Description
+ *
+ * Also described in [ 1147107 ]  unresolved instanceof etc. on &lt;null&gt; not compliant.
+ *
+ * @author Peter Donald
+ */
+public class TestResolveOnInstanceof {
+
+  static interface A {
+    String genString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(doInstanceOf(new Object()));
+  }
+
+  static boolean doInstanceOf(final Object a) {
+    return a instanceof A;
+  }
+}
