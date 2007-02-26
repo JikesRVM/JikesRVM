70131ab526d43f5cafcff2f680b47333e5265257
Peter Donald <peter@realityforge.org>
2007-02-26 00:51:13 +0000

    Make the IfTask into a real ant task rather than just a bean adapted to task interface. This is to ensure that the class gets access to certain ant specific artifacts such as OwningTarget and this can be in turn passed onto child elements. This is required to have ant/antcalls within the iftask.
    The easiest way to achieve was to force all conditions to be contained within a child element of if task rather than directly within if task. Usages of task have been updated to reflect this change.

--- tools/ant-tasks/src/org/jikesrvm/tools/ant/IfTask.java
+++ tools/ant-tasks/src/org/jikesrvm/tools/ant/IfTask.java
@@ -1,6 +1,8 @@
 package org.jikesrvm.tools.ant;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Target;
+import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.MacroDef;
 import org.apache.tools.ant.taskdefs.MacroInstance;
 import org.apache.tools.ant.taskdefs.condition.Condition;
@@ -13,8 +15,11 @@ import org.apache.tools.ant.taskdefs.condition.ConditionBase;
  * @author Peter Donald
  */
 public class IfTask
-    extends ConditionBase {
+    extends Task {
   private MacroDef macroDef;
+  private ConditionSet conditions = new ConditionSet();
+
+  public ConditionSet createConditions() { return conditions; }
 
   public MacroDef.NestedSequential createSequential() {
     macroDef = new MacroDef();
@@ -25,17 +30,22 @@ public class IfTask
   public void execute() {
     validate();
 
-    final Condition condition = (Condition) getConditions().nextElement();
-    if (condition.eval()) {
+    if (conditions.getCondition().eval()) {
       final MacroInstance i = new MacroInstance();
       i.setProject(getProject());
+      i.setOwningTarget(getOwningTarget());
       i.setMacroDef(macroDef);
       i.execute();
     }
   }
 
   private void validate() {
-    if (1 != countConditions()) throw new BuildException("Must specify exactly one condition.");
+    if (!conditions.containsSingleCondition()) throw new BuildException("Must specify exactly one condition.");
     if (null == macroDef) throw new BuildException("Must specify a sequential task to execute.");
+  }
+
+  public static class ConditionSet extends ConditionBase {
+    public boolean containsSingleCondition() { return 1 == super.countConditions(); }
+    public Condition getCondition() { return (Condition) getConditions().nextElement(); }
   }
 }
