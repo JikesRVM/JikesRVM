93de19d295d1da35e539b20c3ce95082e127d78a
Peter Donald <peter@realityforge.org>
2007-02-26 03:54:46 +0000

    Minor code cleanups prior to moving class

--- rvm/src/com/ibm/jikesrvm/adaptive/VM_PriorityQueue.java
+++ rvm/src/com/ibm/jikesrvm/adaptive/VM_PriorityQueue.java
@@ -13,7 +13,7 @@ import com.ibm.jikesrvm.VM;
 
 /**
  * This class implements a priority queue using the standard
- * (balanced partially-ordered tree, i.e., "heap") algorithm.  
+ * (balanced partially-ordered tree, i.e., "heap") algorithm.
  * Smaller priority objects are in the front of the queue.
  *
  * @author Michael Hind
@@ -23,16 +23,16 @@ class VM_PriorityQueue {
   /**
    * the queue, we use elements 1..queue.length
    */
-  private VM_PriorityQueueNode[] queue; 
+  private VM_PriorityQueueNode[] queue;
 
   /**
    * the number of elements actually in the queue
    */
   private int numElements = 0;
-  
+
   VM_PriorityQueue() {
     queue = new VM_PriorityQueueNode[20];
-    
+
     // We don't use element #0
     for (int i=1; i<queue.length; i++) {
       queue[i] = new VM_PriorityQueueNode();
@@ -58,7 +58,7 @@ class VM_PriorityQueue {
   /**
    * Starting at the position passed, swap with parent until heap condition
    * is satisfied, i.e., bubble up
-   * @param startingElement the position to start at 
+   * @param startingElement the position to start at
    */
   private void reheapify(int startingElement) {
     int current = startingElement;
@@ -71,7 +71,7 @@ class VM_PriorityQueue {
       VM_PriorityQueueNode tmp = queue[parent];
       queue[parent] = queue[current];
       queue[current] = tmp;
-      
+
       //        System.out.println("Contents after: "+ this);
       // go up 1 level
       current = parent;
@@ -95,10 +95,10 @@ class VM_PriorityQueue {
       }
       queue = tmp;
     }
-    
+
     queue[numElements].data = _data;
     queue[numElements].priority = _priority;
-    
+
     // re-heapify
     reheapify(numElements);
   }
@@ -115,15 +115,15 @@ class VM_PriorityQueue {
     queue[1].priority = queue[numElements].priority;
     queue[1].data = queue[numElements].data;
     numElements--;
-    
+
     // reheapify!!!
     int current = 1;
-    
-    // The children live at 2*current and  2*current+1 
+
+    // The children live at 2*current and  2*current+1
     int child1 = 2 * current;
     while (child1 <= numElements) {
       int child2 = 2 * current + 1;
-      
+
       // find the smaller of the two children
       int smaller;
       if (child2 <= numElements && queue[child2].priority > queue[child1].priority) {
@@ -131,7 +131,7 @@ class VM_PriorityQueue {
       } else {
         smaller = child1;
       }
-      
+
       if (queue[smaller].priority <= queue[current].priority) {
         break;
       }
@@ -140,7 +140,7 @@ class VM_PriorityQueue {
         VM_PriorityQueueNode tmp = queue[smaller];
         queue[smaller] = queue[current];
         queue[current] = tmp;
-        
+
         // go down 1 level
         current = smaller;
         child1 = 2 * current;
@@ -164,39 +164,37 @@ class VM_PriorityQueue {
    *  @return the queue contents
    */
   synchronized public String toString() {
-    StringBuffer sb = new StringBuffer(" --> ");
-    sb.append("Dumping Queue with "+ numElements +" elements:\n");
-    if (numElements >= 1) {
-      sb.append("\t");
-    }
+    final StringBuilder sb = new StringBuilder(" --> ");
+    sb.append("Dumping Queue with ");
+    sb.append(numElements);
+    sb.append(" elements:\n");
+    if (numElements >= 1) sb.append("\t");
 
     for (int i=1; i<=numElements; i++) {
       sb.append(queue[i].toString());
-      if (i<numElements)
-        sb.append("\n\t");
+      if (i<numElements) sb.append("\n\t");
     }
     return sb.toString();
   }
-}
-
-/**
- * A local class that holds the nodes of the priority tree
- */
-class VM_PriorityQueueNode {
-
+  
   /**
-   * the value to compare on, larger is better
+   * A local class that holds the nodes of the priority tree
    */
-  public double priority; 
+  private static class VM_PriorityQueueNode {
 
-  /**
-   * the associated data 
-   */
-  public Object data;     
+    /**
+     * the value to compare on, larger is better
+     */
+    public double priority;
 
-  public String toString() {
-    return (new StringBuffer(data +" ... ["+ priority +"]")).toString();
+    /**
+     * the associated data
+     */
+    public Object data;
+
+    public String toString() {
+      return data + " ... [" + priority + "]";
+    }
   }
-
 }
 
