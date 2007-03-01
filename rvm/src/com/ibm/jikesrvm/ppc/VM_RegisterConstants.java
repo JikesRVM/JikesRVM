946669c8e48f36d0698f82ffc803810b73200864
Peter Donald <peter@realityforge.org>
2007-03-01 10:38:27 +0000

    Remove redundant specifier

--- rvm/src/com/ibm/jikesrvm/ppc/VM_RegisterConstants.java
+++ rvm/src/com/ibm/jikesrvm/ppc/VM_RegisterConstants.java
@@ -104,7 +104,7 @@ public interface VM_RegisterConstants extends VM_SizeConstants {
    * it is awkward to write 'nice' code to initialize the register names
    * based on the values of the constants.
    */
-  static class RegisterConstantsHelper {
+  class RegisterConstantsHelper {
     static String[] gprNames() {
       String[] names = { "R0", "R1", "R2", "R3", "R4", "R5",
                          "R6", "R7","R8", "R9", "R10", "R11",
