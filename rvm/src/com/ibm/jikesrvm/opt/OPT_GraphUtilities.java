d98482511e804f4781f353ccd1986b20dc8b2cdd
Peter Donald <peter@realityforge.org>
2007-03-01 05:03:38 +0000

    Remove some unused graph utilities

--- rvm/src/com/ibm/jikesrvm/opt/OPT_GraphUtilities.java
+++ rvm/src/com/ibm/jikesrvm/opt/OPT_GraphUtilities.java
@@ -8,8 +8,6 @@
  */
 package com.ibm.jikesrvm.opt;
 
-import  java.util.*;
-
 /**
  * This class implements miscellaneous utilities for graphs.
  *
@@ -36,7 +34,7 @@ class OPT_GraphUtilities {
       return enumerateTopSortInternal(G, new OPT_FilteredDFSenumerateByFinish(G, ie, f));
     }
 
-  public static OPT_GraphNodeEnumeration enumerateTopSortInternal(OPT_Graph G, 
+  private static OPT_GraphNodeEnumeration enumerateTopSortInternal(OPT_Graph G, 
                                                                   OPT_GraphNodeEnumeration e) { 
       final OPT_GraphNode[] elts = new OPT_GraphNode[ G.numberOfNodes() ];
 
@@ -58,102 +56,4 @@ class OPT_GraphUtilities {
         }
       };
     }
-
-  /**
-   * Sort the nodes in a graph by decreasing DFS finish time
-   */
-  public static TreeSet<OPT_GraphNode> sortByDecreasingFinishTime(OPT_Graph net, OPT_DFS dfs) {
-    FinishTimeComparator f = new FinishTimeComparator(dfs);
-    TreeSet<OPT_GraphNode> result = new TreeSet<OPT_GraphNode>(f);
-    for(Enumeration<OPT_GraphNode> e = net.enumerateNodes(); e.hasMoreElements();) {
-      OPT_GraphNode v = e.nextElement();
-      result.add(v);
-    }
-    return  result;
-  }
-
-  static class FinishTimeComparator implements Comparator<Object> {
-      private final OPT_DFS dfs;
-
-      FinishTimeComparator(OPT_DFS dfs) {
-        this.dfs = dfs;
-      }
-
-      public int compare(Object o1, Object o2) {
-        OPT_GraphNode v1 = (OPT_GraphNode)o1;
-        OPT_GraphNode v2 = (OPT_GraphNode)o2;
-        int f1 = dfs.getFinish(v1);
-        int f2 = dfs.getFinish(v2);
-        return  f2 - f1;
-      }
-    }
-
-  /**
-   * Checks if sorted defines a topological order of graph nodes
-   *
-   * @param sorted the ordered list of elements of the graph
-   * @param forward true if all the edges of the nodes in sorted should 
-   * point toward nodes at a higher index in sorted, false if all the edges
-   * should point toward nodes at a lower index in sorted
-   *
-   * @return true if sorted is a topological order.  (Note that will
-   * always return false for a cyclic graph, since no such order is
-   * possible). 
-   *
-   * Note: The equals() method must be defined properly for uniqueness of the
-   * OPT_GraphNodes to be checked correctly.
-   */
-  public static boolean isTopologicalOrder(Enumeration<OPT_GraphNode> sorted, boolean forward) {
-    while (sorted.hasMoreElements()) {
-      OPT_GraphNode cur = sorted.nextElement();
-      HashSet<OPT_GraphNode> s = new HashSet<OPT_GraphNode>();
-      if (!s.add(cur))
-        return  false;
-      Enumeration<OPT_GraphNode> e = cur.outNodes();
-      while (e.hasMoreElements()) {
-        OPT_GraphNode out = e.nextElement();
-        if (forward == s.contains(out))
-          return  false;
-      }
-    }
-    return  true;
-  }
-  public static boolean isTopologicalOrder(Iterable<OPT_GraphNode> sorted, boolean forward) {
-    for (OPT_GraphNode cur : sorted) {
-      HashSet<OPT_GraphNode> s = new HashSet<OPT_GraphNode>();
-      if (!s.add(cur))
-        return  false;
-      Enumeration<OPT_GraphNode> e = cur.outNodes();
-      while (e.hasMoreElements()) {
-        OPT_GraphNode out = e.nextElement();
-        if (forward == s.contains(out))
-          return  false;
-      }
-    }
-    return  true;
-  }
-
-  /**
-   *  Gets the nodes of a graph sorted in topological order. Returns null
-   *  if a cycle is detected in the graph.
-   *
-   *  @param G the graph
-   *
-   *  @return the nodes sorted in topological order if the graph contains no 
-   *  cycles, otherwise null
-   *
-   *  Note: Duplicated code in enumerateTopSort in order to avoid doing the 
-   *  topological sort twice.
-   */
-  public static Enumeration<OPT_GraphNode> enumerateTopSortWithCheck(OPT_Graph G) {
-    OPT_Stack<OPT_GraphNode> order = new OPT_Stack<OPT_GraphNode>();
-    OPT_GraphNodeEnumeration e = new OPT_DFSenumerateByFinish(G);
-    while (e.hasMoreElements())
-      order.push(e.nextElement());
-    if (isTopologicalOrder(order, true)) {
-      return  OPT_GraphNodeEnumerator.create(order);
-    } 
-    else 
-      return  null;
-  }
 }
