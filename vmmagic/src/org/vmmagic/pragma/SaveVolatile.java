87391bbe35fa6fba45f5d579579038c331e6e3ed
Peter Donald <peter@realityforge.org>
2007-03-01 03:35:56 +0000

    Remove unused imports

--- vmmagic/src/org/vmmagic/pragma/SaveVolatile.java
+++ vmmagic/src/org/vmmagic/pragma/SaveVolatile.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 
 /**
  * Methods of a class that implements this interface
--- vmmagic/src/org/vmmagic/pragma/SynchronizedObject.java
+++ vmmagic/src/org/vmmagic/pragma/SynchronizedObject.java
@@ -10,7 +10,6 @@
 package org.vmmagic.pragma;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
