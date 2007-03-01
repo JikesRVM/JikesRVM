bf1d868d73f5c22835827a674b34582e5b77f1fd
Peter Donald <peter@realityforge.org>
2007-03-01 06:21:20 +0000

    Minor munging to move top level class into separate file

--- rvm/src/com/ibm/jikesrvm/ppc/VM_RegisterConstants.java
+++ rvm/src/com/ibm/jikesrvm/ppc/VM_RegisterConstants.java
@@ -8,7 +8,6 @@
  */
 package com.ibm.jikesrvm.ppc;
 
-import com.ibm.jikesrvm.ArchitectureSpecific;
 import com.ibm.jikesrvm.VM;
 import com.ibm.jikesrvm.VM_SizeConstants;
 
@@ -44,6 +43,7 @@ public interface VM_RegisterConstants extends VM_SizeConstants {
   // Jikes RVM's general purpose register usage (32 or 64 bits wide based on VM.BuildFor64Addr).
   //
   int REGISTER_ZERO              = 0; // special instruction semantics on this register
+  
   int FRAME_POINTER              = 1; // same as AIX/OSX/Linux
   int FIRST_VOLATILE_GPR         = FIRST_OS_PARAMETER_GPR;
   //                                            ...
@@ -89,39 +89,34 @@ public interface VM_RegisterConstants extends VM_SizeConstants {
 
   // Register mnemonics (for use by debugger/machine code printers).
   //
-  String [] GPR_NAMES = RegisterConstantsHelper.gprNames();
-  
-  String [] FPR_NAMES = RegisterConstantsHelper.fprNames();
-}
+  String[] GPR_NAMES = RegisterConstantsHelper.gprNames();
 
-/**
- * This class exists only to kludge around the fact that we can't
- * put static clinit blocks in interfaces.  As a result,
- * it is awkward to write 'nice' code to initialize the register names
- * based on the values of the constants.
- */
-class RegisterConstantsHelper implements ArchitectureSpecific.VM_RegisterConstants {
-  static String[] gprNames() {
-    String[] names = { "R0", "R1", "R2", "R3", "R4", "R5",
-                       "R6", "R7","R8", "R9", "R10", "R11",
-                       "R12", "R13","R14", "R15","R16", "R17",
-                       "R18", "R19", "R20", "R21", "R22", "R23",
-                       "R24", "R25", "R26", "R27", "R28", "R29", "R30", "R31"
-    };    
-    names[FRAME_POINTER] = "FP";
-    names[JTOC_POINTER] = "JT";
-    names[PROCESSOR_REGISTER] = "PR";
-    return names;
-  }
-
-  static String[] fprNames() {
-    return new String[] {
+  String [] FPR_NAMES = {
       "F0",  "F1",  "F2",  "F3",  "F4",  "F5",  "F6", "F7",
       "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15",
       "F16",  "F17",  "F18",  "F19",  "F20",  "F21",  "F22",  "F23",
       "F24",  "F25",  "F26",  "F27",  "F28",  "F29",  "F30",  "F31"
     };
+  
+  /**
+   * This class exists only to kludge around the fact that we can't
+   * put static clinit blocks in interfaces.  As a result,
+   * it is awkward to write 'nice' code to initialize the register names
+   * based on the values of the constants.
+   */
+  static class RegisterConstantsHelper {
+    static String[] gprNames() {
+      String[] names = { "R0", "R1", "R2", "R3", "R4", "R5",
+                         "R6", "R7","R8", "R9", "R10", "R11",
+                         "R12", "R13","R14", "R15","R16", "R17",
+                         "R18", "R19", "R20", "R21", "R22", "R23",
+                         "R24", "R25", "R26", "R27", "R28", "R29", "R30", "R31"
+      };    
+      names[FRAME_POINTER] = "FP";
+      names[JTOC_POINTER] = "JT";
+      names[PROCESSOR_REGISTER] = "PR";
+      return names;
+    }
   }
 }
 
-          
