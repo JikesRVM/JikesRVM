b15f0652560b585cf65751df6ceb3fd9e653f4e0
Peter Donald <peter@realityforge.org>
2007-03-24 03:14:34 +0000

    Introduce annotations that exist at a higher meta level. Based on 2 of the 3 annotations that Steve had suggessted earlier (ie Intrinsic and Unboxed but not AddressWidth). Also introduced a Pragma annotation as this is a kind of intrinsic that is prevalent in the codebase and needs to be handled specially.

---
+++ vmmagic/src/org/vmmagic/Intrinsic.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of Jikes RVM (http://jikesrvm.sourceforge.net).
+ * The Jikes RVM project is distributed under the Common Public License (CPL).
+ * A copy of the license is included in the distribution, and is also
+ * available at http://www.opensource.org/licenses/cpl1.0.php
+ *
+ * (C) Copyright Peter Donald. 2007
+ */
+package org.vmmagic;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ * An intrinsic method is a method that is built in (that is, intrinsic) to the compiler 
+ * rather than part of a class library. Intrinsic methods are an escape hatch to provide
+ * "magic" that is inexpressible int the Java programming language. A compiler intrinsic 
+ * will usually generate a specific code sequence that is inlined and optimized as part 
+ * by the compiler. Unless an intrinsic method is expected to be executed in a non-intrinsic 
+ * aware compiler, the body of the method should be empty.
+ * 
+ * <p>If the Intrinsic annotation is applied to a method then then the method is a compiler 
+ * intrinsic.</p> 
+ * 
+ * <p>NOTE: At the current time the Intrinsic annotation is only used for documentation 
+ * purposes but in the near future it is expected that the semantics of the annotation will 
+ * be enforced by the compiler.</p> 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Intrinsic { }
