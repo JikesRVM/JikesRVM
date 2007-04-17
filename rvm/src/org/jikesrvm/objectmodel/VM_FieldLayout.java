1d57b5184e9f242b831d7046745b9b222c40585b
Robin Garner <robin.garner@scu.edu.au>
2007-04-17 04:22:05 +0000

    Change copright notice from ANU to IBM.

--- rvm/src/org/jikesrvm/objectmodel/VM_FieldLayout.java
+++ rvm/src/org/jikesrvm/objectmodel/VM_FieldLayout.java
@@ -4,7 +4,7 @@
  * A copy of the license is included in the distribution, and is also
  * available at http://www.opensource.org/licenses/cpl1.0.php
  *
- * (C) Copyright Australian National University 2007
+ * (C) Copyright IBM Corp. 2001
  */
 package org.jikesrvm.objectmodel;
 
