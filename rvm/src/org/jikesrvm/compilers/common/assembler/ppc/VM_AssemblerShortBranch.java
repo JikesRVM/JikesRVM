4e91512facbce8b5a25807e8857577df918eb089
Peter Donald <peter@realityforge.org>
2007-05-17 05:34:02 +0000

    Fix formatting error

--- rvm/src/org/jikesrvm/compilers/common/assembler/ppc/VM_AssemblerShortBranch.java
+++ rvm/src/org/jikesrvm/compilers/common/assembler/ppc/VM_AssemblerShortBranch.java
@@ -10,6 +10,12 @@
  *  See the COPYRIGHT.txt file distributed with this work for information
  *  regarding copyright ownership.
  */
+package org.jikesrvm.compilers.common.assembler.ppc;
+
+import org.jikesrvm.compilers.common.assembler.VM_AbstractAssembler;
+import org.jikesrvm.compilers.common.assembler.VM_ForwardReference;
+
+/** 
  * This class was formerly a private static inner class of VM_Assembler, but has
  * been pulled out to work around a bug whereby the system seems to have gotten
  * confused by the relationship between:
@@ -18,11 +24,6 @@
  *
  * This problem does not exist under IA32 since there is no need for VM_Assembler.ShortBranch
  */
-package org.jikesrvm.compilers.common.assembler.ppc;
-
-import org.jikesrvm.compilers.common.assembler.VM_AbstractAssembler;
-import org.jikesrvm.compilers.common.assembler.VM_ForwardReference;
-
 class VM_AssemblerShortBranch extends VM_ForwardReference.ShortBranch {
   final int spTopOffset;
 
