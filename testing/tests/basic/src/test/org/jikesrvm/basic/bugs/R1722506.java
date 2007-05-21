ff7e1e95025bde7b208aaef86e3a5e414ec3a8b6
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2007-05-21 10:45:33 +0000

    Added test to go with tracker item 1722506.  Seems like a horrible bug in java.io for classpath > 0.92

---
+++ testing/tests/basic/src/test/org/jikesrvm/basic/bugs/R1722506.java
@@ -0,0 +1,40 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Common Public License (CPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/cpl1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package test.org.jikesrvm.basic.bugs;
+
+import java.io.*;
+
+/**
+ * [ 1722506 ] dacapo eclipse fails EOF exceptions 
+ * http://sourceforge.net/tracker/index.php?func=detail&aid=1722506&group_id=128805&atid=712768
+ */
+public class R1722506 {
+  public static void main(String[] args) {
+    String filename = System.getProperty("java.io.tmpdir")+"/filetest.tmp";
+    try {    
+      File newIndexFile = new File(filename);
+      DataOutputStream stream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(newIndexFile, false), 2048));
+      for (int i = 0; i < 50; i++)
+	stream.writeInt(i);
+      stream.close();
+      long streamLen = (new File(filename)).length();
+      RandomAccessFile raFile = new RandomAccessFile(filename, "rw");
+      System.out.println(streamLen == (new File(filename)).length());
+      raFile.close();
+    } catch (Exception e) {
+      System.err.println("Caught exception: "+e);
+    } finally {
+      (new File(filename)).delete();
+    }
+  }
+}
