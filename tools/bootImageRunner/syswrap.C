770b3129733078f4128cb71c20d645b0b827fc50
Daniel Frampton <Daniel.Frampton@anu.edu.au>
2007-06-15 11:16:45 +0000

    Reverting Ian's change 12651 related to classpath. I can't seem to get this to work on any of the ANU machines. It seems to either crash or send the system off in an infinite hang. This includes after a full rebuild of classpath. I think we need to test this more before we make this change. I have kicked the regression machine so it restarts with the commit runs tomorrow.

--- tools/bootImageRunner/syswrap.C
+++ tools/bootImageRunner/syswrap.C
@@ -313,3 +313,79 @@ poll(struct pollfd *ufds, long unsigned int nfds, int timeout)
 
     return ready;
 }
+
+// Wrapper for pthread_mutex_lock
+// If the lock can't be obtained then yield and try again
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+  int err;
+  if (VERBOSE_WRAPPERS)
+    printf("Calling pthread_lock wrapper\n");
+  err = pthread_mutex_trylock(mutex);
+  if(err == EBUSY) {
+    JNIEnv *env;
+    GetEnv(NULL, (void**) &env, JNI_VERSION_1_1);
+    // Check env has been initialised (ie VM has started)
+    if(env != NULL) {
+      jclass thread_class;
+      jmethodID thread_yield_mth;
+      thread_class = env->FindClass ("java/lang/Thread");
+      thread_yield_mth = env->GetStaticMethodID (thread_class, "yield", "()V");
+      do {
+        env->CallStaticVoidMethod (thread_class,
+                                   thread_yield_mth);
+        err = pthread_mutex_trylock(mutex);
+      } while(err == EBUSY);
+    }
+    else {
+      do {
+        err = pthread_mutex_trylock(mutex);
+      } while(err == EBUSY);
+    }
+  }
+}
+
+// Wrapper for pthread_cond_wait
+// If the cond wait is exceeds the timeout then yield and try again
+int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
+{
+  struct timeval now;
+  struct timespec timeout;
+  int err;
+
+  if (VERBOSE_WRAPPERS)
+    printf("Calling pthread_cond_wait wrapper\n");
+
+  // timeout of 1second from now
+  gettimeofday(&now, NULL);
+  timeout.tv_sec = now.tv_sec + 1;
+  timeout.tv_nsec = now.tv_usec * 1000;
+
+  err = pthread_cond_timedwait(cond, mutex, &timeout);
+  if (err == ETIMEDOUT) { /* timeout */
+    JNIEnv *env;
+    GetEnv(NULL, (void**) &env, JNI_VERSION_1_1);
+    // Check env has been initialised (ie VM has started)
+    if(env != NULL) {
+      jclass thread_class;
+      jmethodID thread_yield_mth;
+      thread_class = env->FindClass ("java/lang/Thread");
+      thread_yield_mth = env->GetStaticMethodID (thread_class, "yield", "()V");
+      do {
+        env->CallStaticVoidMethod (thread_class,
+                                   thread_yield_mth);
+        // timeout of 1second later
+        gettimeofday(&now, NULL);
+        timeout.tv_sec = now.tv_sec + 1;
+        timeout.tv_nsec = now.tv_usec * 1000;
+        err = pthread_cond_timedwait(cond, mutex, &timeout);
+      } while(err == ETIMEDOUT);
+    }
+    else {
+      do {
+        err = pthread_cond_timedwait(cond, mutex, &timeout);
+      } while(err == ETIMEDOUT);
+    }
+  }
+  return err;
+}
