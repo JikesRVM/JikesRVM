a402c7418cc877526f6d4cd4e499e7cdcdb9082b
Dave Grove <groved@us.ibm.com>
2007-07-01 20:15:31 +0000

    new compiler DNA for ia32-linux; working on gathering ppc32-aix next

--- rvm/src/org/jikesrvm/adaptive/recompilation/VM_CompilerDNA.java
+++ rvm/src/org/jikesrvm/adaptive/recompilation/VM_CompilerDNA.java
@@ -50,9 +50,9 @@ public class VM_CompilerDNA implements VM_Constants {
   private static int numCompilers;
 
   /**
-   * Average bytecodes compiled per millisec
-   * These numbers were from a shadow on July 22, 2004 on munchkin (AIX/PPC)
-   * and Dec 2nd, 2004 on wormtongue (Linux/IA32) using unweighted compilation rate.
+   * Average bytecodes compiled per millisecond.
+   * These numbers were measured on July 22, 2004 on munchkin (AIX/PPC)
+   * and July 1, 2007 on legato (Linux/IA32) using unweighted compilation rate.
    */
   private static final double[] compilationRates;
 
@@ -61,8 +61,8 @@ public class VM_CompilerDNA implements VM_Constants {
       compilationRates = new double[]{359.17,             // base
                                       10.44, 4.69, 1.56}; // opt 0...2
     } else if (VM.BuildForIA32) {
-      compilationRates = new double[]{696.58,             // base
-                                      18.19, 8.90, 3.90}; // opt 0...2
+      compilationRates = new double[]{1246.22,              // base
+                                      30.03, 14.60, 14.59}; // opt 0...2
     } else {
       if (VM.VerifyAssertions) VM._assert(NOT_REACHED);
       compilationRates = null;
@@ -72,7 +72,7 @@ public class VM_CompilerDNA implements VM_Constants {
   /**
    * What is the execution rate of each compiler normalized to the 1st compiler
    * These numbers were from a shadow on July 22, 2004 on munchkin (AIX/PPC)
-   * and Dec 2nd, 2004 on wormtongue (Linux/IA32) using unweighted compilation rate.
+   * and July 1, 2007 on legato (Linux/IA32) using unweighted compilation rate.
    */
   private static final double[] speedupRates;
 
@@ -82,7 +82,7 @@ public class VM_CompilerDNA implements VM_Constants {
                                   4.73, 6.65, 7.39};  // opt 0...2
     } else if (VM.BuildForIA32) {
       speedupRates = new double[]{1.00,               // base
-                                  4.56, 7.13, 7.35};  // opt 0...2
+                                  4.86, 6.39, 6.40};  // opt 0...2
     } else {
       if (VM.VerifyAssertions) VM._assert(NOT_REACHED);
       speedupRates = null;
