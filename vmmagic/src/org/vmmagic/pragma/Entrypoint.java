a1a506a8c7a75df1c73a0e238480caad96aa39b7
Peter Donald <peter@realityforge.org>
2007-07-11 03:18:08 +0000

    Fix documentation and disallow annotation on types

--- vmmagic/src/org/vmmagic/pragma/Entrypoint.java
+++ vmmagic/src/org/vmmagic/pragma/Entrypoint.java
@@ -7,9 +7,11 @@ import java.lang.annotation.ElementType;
 import org.vmmagic.Pragma;
 
 /**
- * The entrypoint annotation indicates that the method, field or type
+ * The entrypoint annotation indicates that the method or field is
+ * directly accessed by the compiler. We cache resolved values for
+ * these elements in VM_Entrypoints.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD})
 @Pragma
 public @interface Entrypoint { }
