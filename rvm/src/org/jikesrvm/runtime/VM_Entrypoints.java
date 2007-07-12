3ab12c4472cfead09cb6b1af7fe843ba6fa9aaeb
Peter Donald <peter@realityforge.org>
2007-07-12 23:24:55 +0000

    Remove some unused fields

--- rvm/src/org/jikesrvm/runtime/VM_Entrypoints.java
+++ rvm/src/org/jikesrvm/runtime/VM_Entrypoints.java
@@ -24,8 +24,6 @@ import static org.jikesrvm.runtime.VM_EntrypointHelper.getMethod;
  * compiler-generated machine code or C runtime code.
  */
 public class VM_Entrypoints {
-
-  public static final String arch = VM.BuildForIA32 ? "ia32" : "ppc";
   public static final VM_NormalMethod bootMethod = getMethod("Lorg/jikesrvm/VM;", "boot", "()V");
 
   public static final VM_Method java_lang_reflect_Method_invokeMethod =
@@ -71,8 +69,6 @@ public class VM_Entrypoints {
       getMethod("Lorg/jikesrvm/runtime/VM_Runtime;",
                 "resolvedNewArray",
                 "(III[Ljava/lang/Object;IIII)Ljava/lang/Object;");
-  public static final VM_NormalMethod newArrayArrayMethod =
-      getMethod("Lorg/jikesrvm/" + arch + "/VM_MultianewarrayHelper;", "newArrayArray", "(IIII)Ljava/lang/Object;");
   public static final VM_Field gcLockField = getField("Ljava/lang/VMRuntime;", "gcLock", "I");
 
   public static final VM_Field sysWriteLockField = getField("Lorg/jikesrvm/VM;", "sysWriteLock", "I");
@@ -165,27 +161,6 @@ public class VM_Entrypoints {
       getField("Lorg/jikesrvm/runtime/VM_Math;",
                "I2Dconstant",
                "D");//  special double value for use in int <--> double conversions
-  public static final VM_Field FPUControlWordField =
-      (VM.BuildForIA32) ? getField("Lorg/jikesrvm/" + arch + "/VM_MachineSpecificIA;", "FPUControlWord", "I") : null;
-
-  public static final String ArchCodeArrayName = "Lorg/jikesrvm/ArchitectureSpecific$VM_CodeArray;";
-  public static final VM_Field reflectiveMethodInvokerInstructionsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_OutOfLineMachineCode;",
-               "reflectiveMethodInvokerInstructions",
-               ArchCodeArrayName);
-  public static final VM_Field saveThreadStateInstructionsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_OutOfLineMachineCode;", "saveThreadStateInstructions", ArchCodeArrayName);
-  public static final VM_Field threadSwitchInstructionsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_OutOfLineMachineCode;", "threadSwitchInstructions", ArchCodeArrayName);
-  public static final VM_Field restoreHardwareExceptionStateInstructionsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_OutOfLineMachineCode;",
-               "restoreHardwareExceptionStateInstructions",
-               ArchCodeArrayName);
-  public static final VM_Field invokeNativeFunctionInstructionsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_OutOfLineMachineCode;",
-               "invokeNativeFunctionInstructions",
-               ArchCodeArrayName);
-
   public static final VM_Field scratchStorageField =
       getField("Lorg/jikesrvm/scheduler/VM_Processor;", "scratchStorage", "D");
   public static final VM_Field timeSliceExpiredField =
@@ -292,23 +267,6 @@ public class VM_Entrypoints {
   public static final VM_NormalMethod modifyCheckMethod =
       getMethod("Lorg/jikesrvm/memorymanagers/mminterface/MM_Interface;", "modifyCheck", "(Ljava/lang/Object;)V");
 
-  public static final VM_Field registersIPField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_Registers;", "ip", "Lorg/vmmagic/unboxed/Address;");
-  public static final VM_Field registersFPRsField = getField("Lorg/jikesrvm/" + arch + "/VM_Registers;", "fprs", "[D");
-  public static final VM_Field registersGPRsField =
-      getField("Lorg/jikesrvm/" + arch + "/VM_Registers;", "gprs", "Lorg/vmmagic/unboxed/WordArray;");
-  public static final VM_Field registersInUseField = getField("Lorg/jikesrvm/" + arch + "/VM_Registers;", "inuse", "Z");
-  public static final VM_Field registersLRField =
-      (VM.BuildForPowerPC) ? getField("Lorg/jikesrvm/" + arch + "/VM_Registers;",
-                                      "lr",
-                                      "Lorg/vmmagic/unboxed/Address;") : null;
-  public static final VM_Field toSyncProcessorsField =
-      (VM.BuildForPowerPC) ? getField("Lorg/jikesrvm/scheduler/VM_Scheduler;", "toSyncProcessors", "I") : null;
-  public static final VM_Field registersFPField =
-      (VM.BuildForIA32) ? getField("Lorg/jikesrvm/" + arch + "/VM_Registers;",
-                                   "fp",
-                                   "Lorg/vmmagic/unboxed/Address;") : null;
-
   public static final VM_Field outputLockField = getField("Lorg/jikesrvm/scheduler/VM_Scheduler;", "outputLock", "I");
 
   public static final VM_Field processorsField =
@@ -346,10 +304,6 @@ public class VM_Entrypoints {
       getField("Lorg/jikesrvm/jni/VM_JNIEnvironment;", "pendingException", "Ljava/lang/Throwable;");
   public static final VM_Field JNIExternalFunctionsField =
       getField("Lorg/jikesrvm/jni/VM_JNIEnvironment;", "externalJNIFunctions", "Lorg/vmmagic/unboxed/Address;");
-  public static final VM_Field JNIEnvSavedJTOCField =
-      (VM.BuildForPowerPC) ? getField("Lorg/jikesrvm/jni/VM_JNIEnvironment;",
-                                      "savedJTOC",
-                                      "Lorg/vmmagic/unboxed/Address;") : null;
 
   public static final VM_Field the_boot_recordField =
       getField("Lorg/jikesrvm/runtime/VM_BootRecord;", "the_boot_record", "Lorg/jikesrvm/runtime/VM_BootRecord;");
@@ -436,7 +390,6 @@ public class VM_Entrypoints {
       sysArrayCopy = null;
     }
   }
-
 
   public static final VM_Field classLoaderDefinedPackages =
       getField("Ljava/lang/ClassLoader;", "definedPackages", "Ljava/util/HashMap;");
