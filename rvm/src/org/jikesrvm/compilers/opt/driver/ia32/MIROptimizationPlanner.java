0933f671660e3422a5bec33521a7857c590fb377
Dave Grove <groved@us.ibm.com>
2008-03-05 01:06:17 +0000

    move MIROptimizationPlanner to arch-specific subpackages of org.jikesrvm.compilers.opt.driver

--- rvm/src/org/jikesrvm/compilers/opt/driver/ia32/MIROptimizationPlanner.java
+++ rvm/src/org/jikesrvm/compilers/opt/driver/ia32/MIROptimizationPlanner.java
@@ -10,23 +10,25 @@
  *  See the COPYRIGHT.txt file distributed with this work for information
  *  regarding copyright ownership.
  */
-package org.jikesrvm.compilers.opt.ppc;
+package org.jikesrvm.compilers.opt.driver.ia32;
 
 import java.util.ArrayList;
+
 import org.jikesrvm.compilers.opt.ConvertLIRtoMIR;
 import org.jikesrvm.compilers.opt.ConvertMIRtoMC;
 import org.jikesrvm.compilers.opt.LiveAnalysis;
 import org.jikesrvm.compilers.opt.MIRBranchOptimizations;
 import org.jikesrvm.compilers.opt.MutateSplits;
 import org.jikesrvm.compilers.opt.OptOptions;
-import org.jikesrvm.compilers.opt.PrePassScheduler;
 import org.jikesrvm.compilers.opt.SplitBasicBlock;
-import org.jikesrvm.compilers.opt.regalloc.ExpandCallingConvention;
-import org.jikesrvm.compilers.opt.regalloc.PrologueEpilogueCreator;
-import org.jikesrvm.compilers.opt.regalloc.RegisterAllocator;
 import org.jikesrvm.compilers.opt.driver.IRPrinter;
 import org.jikesrvm.compilers.opt.driver.OptimizationPlanElement;
 import org.jikesrvm.compilers.opt.driver.OptimizationPlanner;
+import org.jikesrvm.compilers.opt.regalloc.ExpandCallingConvention;
+import org.jikesrvm.compilers.opt.regalloc.PrologueEpilogueCreator;
+import org.jikesrvm.compilers.opt.regalloc.RegisterAllocator;
+import org.jikesrvm.compilers.opt.regalloc.ia32.ExpandFPRStackConvention;
+import org.jikesrvm.compilers.opt.regalloc.ia32.MIRSplitRanges;
 
 /**
  * This class specifies the order in which CompilerPhases are
@@ -34,10 +36,10 @@ import org.jikesrvm.compilers.opt.driver.OptimizationPlanner;
  * The methods LIR2MIR, MIROptimizations, and MIR2MC each specify the
  * elements that make up the main compilation stages.
  */
-public abstract class MIROptimizationPlanner extends OptimizationPlanner {
+public class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
-   * Initialize the "master plan" for the PowerPC backend of the opt compiler.
+   * Initialize the "master plan" for the IA32 backend of the opt compiler.
    */
   public static void intializeMasterPlan(ArrayList<OptimizationPlanElement> temp) {
     LIR2MIR(temp);
@@ -47,24 +49,25 @@ public abstract class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed to convert LIR to PowerPC MIR.
+   * are to be performed to convert LIR to IA32 MIR.
    *
    * @param p the plan under construction
    */
   private static void LIR2MIR(ArrayList<OptimizationPlanElement> p) {
     composeComponents(p, "Convert LIR to MIR", new Object[]{
+        // Split very large basic blocks into smaller ones.
+        new SplitBasicBlock(),
         // Optional printing of final LIR
         new IRPrinter("Final LIR") {
           public boolean shouldPerform(OptOptions options) {
             return options.PRINT_FINAL_LIR;
           }
         },
-        // Split very large basic blocks into smaller ones.
-        new SplitBasicBlock(),
         // Change operations that split live ranges to moves
         new MutateSplits(),
-        // Instruction selection
+        // Instruction Selection
         new ConvertLIRtoMIR(),
+
         // Optional printing of initial MIR
         new IRPrinter("Initial MIR") {
           public boolean shouldPerform(OptOptions options) {
@@ -75,57 +78,36 @@ public abstract class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed on PowerPC MIR.
+   * are to be performed on IA32 MIR.
    *
    * @param p the plan under construction
    */
   private static void MIROptimizations(ArrayList<OptimizationPlanElement> p) {
-    ////////////////////
-    // MIR OPTS(1) (before register allocation)
-    ////////////////////
-    // INSTRUCTION SCHEDULING (PRE-PASS --- PRIOR TO REGISTER ALLOCATION)
-    addComponent(p, new PrePassScheduler());
-    ////////////////////
-    // GCMapping part1 and RegisterAllocation
-    ////////////////////
-
-    composeComponents(p, "Register Mapping", new Object[]{
-        // MANDATORY: Expand calling convention
-        new ExpandCallingConvention(),
-        // MANDATORY: Perform Live analysis and create GC maps
-        new LiveAnalysis(true, false),
-        // MANDATORY: Perform register allocation
-        new RegisterAllocator(),
-        // MANDATORY: Add prologue and epilogue
-        new PrologueEpilogueCreator(),});
-    ////////////////////
-    // MIR OPTS(2) (after register allocation)
-    // NOTE: GCMapping part 1 has created the GC maps already.
-    //       From now until the end of compilation, we cannot change
-    //       the set of live references at a GC point
-    //       without updating the GCMaps.
-    //       Effectively this means that we can only do the
-    //       most trivial optimizations from
-    //       here on out without having to some potentially complex bookkeeping.
-    ////////////////////
+    // Register Allocation
+    composeComponents(p, "Register Mapping", new Object[]{new MIRSplitRanges(),
+                                                          // MANDATORY: Expand calling convention
+                                                          new ExpandCallingConvention(),
+                                                          // MANDATORY: Insert defs/uses due to floating-point stack
+                                                          new ExpandFPRStackConvention(),
+                                                          // MANDATORY: Perform Live analysis and create GC maps
+                                                          new LiveAnalysis(true, false),
+                                                          // MANDATORY: Perform register allocation
+                                                          new RegisterAllocator(),
+                                                          // MANDATORY: Add prologue and epilogue
+                                                          new PrologueEpilogueCreator(),});
     // Peephole branch optimizations
     addComponent(p, new MIRBranchOptimizations(1));
   }
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed to convert PowerPC MIR into
+   * are to be performed to convert IA32 MIR into
    * ready-to-execute machinecode (and associated mapping tables).
    *
    * @param p the plan under construction
    */
   private static void MIR2MC(ArrayList<OptimizationPlanElement> p) {
     // MANDATORY: Final assembly
-    addComponent(p, new IRPrinter("Final MIR") {
-      public boolean shouldPerform(OptOptions options) {
-        return options.PRINT_FINAL_MIR;
-      }
-    });
     addComponent(p, new ConvertMIRtoMC());
   }
 
--- rvm/src/org/jikesrvm/compilers/opt/driver/ppc/MIROptimizationPlanner.java
+++ rvm/src/org/jikesrvm/compilers/opt/driver/ppc/MIROptimizationPlanner.java
@@ -10,25 +10,23 @@
  *  See the COPYRIGHT.txt file distributed with this work for information
  *  regarding copyright ownership.
  */
-package org.jikesrvm.compilers.opt.ia32;
+package org.jikesrvm.compilers.opt.driver.ppc;
 
 import java.util.ArrayList;
-
 import org.jikesrvm.compilers.opt.ConvertLIRtoMIR;
 import org.jikesrvm.compilers.opt.ConvertMIRtoMC;
 import org.jikesrvm.compilers.opt.LiveAnalysis;
 import org.jikesrvm.compilers.opt.MIRBranchOptimizations;
 import org.jikesrvm.compilers.opt.MutateSplits;
 import org.jikesrvm.compilers.opt.OptOptions;
+import org.jikesrvm.compilers.opt.PrePassScheduler;
 import org.jikesrvm.compilers.opt.SplitBasicBlock;
-import org.jikesrvm.compilers.opt.driver.IRPrinter;
-import org.jikesrvm.compilers.opt.driver.OptimizationPlanElement;
-import org.jikesrvm.compilers.opt.driver.OptimizationPlanner;
 import org.jikesrvm.compilers.opt.regalloc.ExpandCallingConvention;
 import org.jikesrvm.compilers.opt.regalloc.PrologueEpilogueCreator;
 import org.jikesrvm.compilers.opt.regalloc.RegisterAllocator;
-import org.jikesrvm.compilers.opt.regalloc.ia32.ExpandFPRStackConvention;
-import org.jikesrvm.compilers.opt.regalloc.ia32.MIRSplitRanges;
+import org.jikesrvm.compilers.opt.driver.IRPrinter;
+import org.jikesrvm.compilers.opt.driver.OptimizationPlanElement;
+import org.jikesrvm.compilers.opt.driver.OptimizationPlanner;
 
 /**
  * This class specifies the order in which CompilerPhases are
@@ -36,10 +34,10 @@ import org.jikesrvm.compilers.opt.regalloc.ia32.MIRSplitRanges;
  * The methods LIR2MIR, MIROptimizations, and MIR2MC each specify the
  * elements that make up the main compilation stages.
  */
-public class MIROptimizationPlanner extends OptimizationPlanner {
+public abstract class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
-   * Initialize the "master plan" for the IA32 backend of the opt compiler.
+   * Initialize the "master plan" for the PowerPC backend of the opt compiler.
    */
   public static void intializeMasterPlan(ArrayList<OptimizationPlanElement> temp) {
     LIR2MIR(temp);
@@ -49,25 +47,24 @@ public class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed to convert LIR to IA32 MIR.
+   * are to be performed to convert LIR to PowerPC MIR.
    *
    * @param p the plan under construction
    */
   private static void LIR2MIR(ArrayList<OptimizationPlanElement> p) {
     composeComponents(p, "Convert LIR to MIR", new Object[]{
-        // Split very large basic blocks into smaller ones.
-        new SplitBasicBlock(),
         // Optional printing of final LIR
         new IRPrinter("Final LIR") {
           public boolean shouldPerform(OptOptions options) {
             return options.PRINT_FINAL_LIR;
           }
         },
+        // Split very large basic blocks into smaller ones.
+        new SplitBasicBlock(),
         // Change operations that split live ranges to moves
         new MutateSplits(),
-        // Instruction Selection
+        // Instruction selection
         new ConvertLIRtoMIR(),
-
         // Optional printing of initial MIR
         new IRPrinter("Initial MIR") {
           public boolean shouldPerform(OptOptions options) {
@@ -78,36 +75,57 @@ public class MIROptimizationPlanner extends OptimizationPlanner {
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed on IA32 MIR.
+   * are to be performed on PowerPC MIR.
    *
    * @param p the plan under construction
    */
   private static void MIROptimizations(ArrayList<OptimizationPlanElement> p) {
-    // Register Allocation
-    composeComponents(p, "Register Mapping", new Object[]{new MIRSplitRanges(),
-                                                          // MANDATORY: Expand calling convention
-                                                          new ExpandCallingConvention(),
-                                                          // MANDATORY: Insert defs/uses due to floating-point stack
-                                                          new ExpandFPRStackConvention(),
-                                                          // MANDATORY: Perform Live analysis and create GC maps
-                                                          new LiveAnalysis(true, false),
-                                                          // MANDATORY: Perform register allocation
-                                                          new RegisterAllocator(),
-                                                          // MANDATORY: Add prologue and epilogue
-                                                          new PrologueEpilogueCreator(),});
+    ////////////////////
+    // MIR OPTS(1) (before register allocation)
+    ////////////////////
+    // INSTRUCTION SCHEDULING (PRE-PASS --- PRIOR TO REGISTER ALLOCATION)
+    addComponent(p, new PrePassScheduler());
+    ////////////////////
+    // GCMapping part1 and RegisterAllocation
+    ////////////////////
+
+    composeComponents(p, "Register Mapping", new Object[]{
+        // MANDATORY: Expand calling convention
+        new ExpandCallingConvention(),
+        // MANDATORY: Perform Live analysis and create GC maps
+        new LiveAnalysis(true, false),
+        // MANDATORY: Perform register allocation
+        new RegisterAllocator(),
+        // MANDATORY: Add prologue and epilogue
+        new PrologueEpilogueCreator(),});
+    ////////////////////
+    // MIR OPTS(2) (after register allocation)
+    // NOTE: GCMapping part 1 has created the GC maps already.
+    //       From now until the end of compilation, we cannot change
+    //       the set of live references at a GC point
+    //       without updating the GCMaps.
+    //       Effectively this means that we can only do the
+    //       most trivial optimizations from
+    //       here on out without having to some potentially complex bookkeeping.
+    ////////////////////
     // Peephole branch optimizations
     addComponent(p, new MIRBranchOptimizations(1));
   }
 
   /**
    * This method defines the optimization plan elements that
-   * are to be performed to convert IA32 MIR into
+   * are to be performed to convert PowerPC MIR into
    * ready-to-execute machinecode (and associated mapping tables).
    *
    * @param p the plan under construction
    */
   private static void MIR2MC(ArrayList<OptimizationPlanElement> p) {
     // MANDATORY: Final assembly
+    addComponent(p, new IRPrinter("Final MIR") {
+      public boolean shouldPerform(OptOptions options) {
+        return options.PRINT_FINAL_MIR;
+      }
+    });
     addComponent(p, new ConvertMIRtoMC());
   }
 
