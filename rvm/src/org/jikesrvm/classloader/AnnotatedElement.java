a61da1affa461b4ba7345552eef1d1d90f603fcf
Ian Rogers <rogers.email@gmail.com>
2008-08-10 17:17:28 +0000

    Revert r14864 as we can't write proxy objects. r14867 prevents bootstrap problem when baseline compiling reflective calls (checking for a proxy creates a proxy via reflection).

--- rvm/src/org/jikesrvm/classloader/AnnotatedElement.java
+++ rvm/src/org/jikesrvm/classloader/AnnotatedElement.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.lang.annotation.Annotation;
 
 import org.vmmagic.pragma.Uninterruptible;
+import org.jikesrvm.VM;
 
 /**
  * A common abstract super class for all elements that can be
@@ -97,6 +98,9 @@ public abstract class AnnotatedElement implements java.lang.reflect.AnnotatedEle
   }
 
   final Annotation[] getDeclaredAnnotationsInternal() {
+    if (!VM.runningVM) {
+      return toAnnotations(declaredAnnotationDatas);
+    }
     if (null == declaredAnnotations) {
       declaredAnnotations = toAnnotations(declaredAnnotationDatas);
     }
