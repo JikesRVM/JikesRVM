477ef96a62d723138b11391a6365e645109aaaa1
Dave Grove <groved@us.ibm.com>
2009-04-24 15:00:43 +0000

    RVM-817 WIP: Change from CPL to EPL in individual file license information.

--- MMTk/harness/src/org/mmtk/harness/lang/type/BooleanType.java
+++ MMTk/harness/src/org/mmtk/harness/lang/type/BooleanType.java
@@ -1,11 +1,11 @@
 /*
  *  This file is part of the Jikes RVM project (http://jikesrvm.org).
  *
- *  This file is licensed to You under the Common Public License (CPL);
+ *  This file is licensed to You under the Eclipse Public License (EPL);
  *  You may not use this file except in compliance with the License. You
  *  may obtain a copy of the License at
  *
- *      http://www.opensource.org/licenses/cpl1.0.php
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
  *
  *  See the COPYRIGHT.txt file distributed with this work for information
  *  regarding copyright ownership.
