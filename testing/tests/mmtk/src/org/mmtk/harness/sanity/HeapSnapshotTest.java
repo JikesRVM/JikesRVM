7b7cec79963e8480296a0fc576b789a22e93d143
Dave Grove <groved@us.ibm.com>
2009-07-29 19:58:29 +0000

    Add standard license header.

--- testing/tests/mmtk/src/org/mmtk/harness/sanity/HeapSnapshotTest.java
+++ testing/tests/mmtk/src/org/mmtk/harness/sanity/HeapSnapshotTest.java
@@ -1,3 +1,16 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+
 package org.mmtk.harness.sanity;
 
 import static org.junit.Assert.*;
