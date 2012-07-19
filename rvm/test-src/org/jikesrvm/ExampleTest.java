5125cdfb8b15c8f1b8e1040ee21b41f98a46c886
Dave Grove <groved@us.ibm.com>
2012-07-19 09:18:27 -0400

    RVM-970 : environment for unit-tests (GSoC project from Joao Reys Santos)

---
+++ rvm/test-src/org/jikesrvm/ExampleTest.java
@@ -0,0 +1,25 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package org.jikesrvm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+  @Test
+  public void testSimpleAssert() {
+    assertTrue(true);
+  }
+}
