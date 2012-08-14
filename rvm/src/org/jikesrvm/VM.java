84cc5fb9245d86e56692ad5fd32098540442eb21
Dave Grove <groved@us.ibm.com>
2012-08-13 22:04:00 -0400

    When using OpenJDK as the class libraries, re-run the static initializer for java.util.BitSet during booting to workaround a difference in the class libraries used in OpenJDK and in the IBM SDK.  This fix enables Jikes RVM built with an IBM JVM to successfully run HelloWorld using the OpenJDK class libraries.

--- rvm/src/org/jikesrvm/VM.java
+++ rvm/src/org/jikesrvm/VM.java
@@ -443,6 +443,9 @@ public class VM extends Properties implements Constants, ExitStatus {
       runClassInitializer("java.io.ObjectInputStream");
       runClassInitializer("java.security.MessageDigest");
     }
+    if (VM.BuildForOpenJDK) {
+      runClassInitializer("java.util.BitSet"); // needed when using IBM SDK as host JVM
+    }
     if (VM.BuildForGnuClasspath) {
       runClassInitializer("java.lang.VMDouble");
     }
