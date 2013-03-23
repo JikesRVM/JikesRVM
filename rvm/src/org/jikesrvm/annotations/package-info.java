dddb267f14adade5916eccb1901cd921736c8384
Erik Brangs <erik.brangs@gmx.de>
2013-03-23 12:29:45 +0100

    RVM-1023 : Add a package-level comment for the org.jikesrvm.annotations package.

---
+++ rvm/src/org/jikesrvm/annotations/package-info.java
@@ -0,0 +1,20 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+
+/**
+ * Provides classes that are used during the build process to generate
+ * implementations for methods in {@link org.jikesrvm.runtime.SysCall}.<p>
+ *
+ * This code is not needed at runtime and will not be contained in the bootimage.
+ */
+package org.jikesrvm.annotations;
