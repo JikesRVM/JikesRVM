c5f88d917b0d990ffbe229135e92c2cd491a99fc
Erik Brangs <erik.brangs@gmx.de>
2014-02-04 13:01:46 +0100

    RVM-1023 : Add package JavaDoc for the packages org.jikesrvm.tools.oth, org.jikesrvm.compilers.opt.ssa and org.jikesrvm.util.

---
+++ rvm/src/org/jikesrvm/tools/oth/package-info.java
@@ -0,0 +1,18 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+
+/**
+ * Test harness for the optimizing compiler (see the section "OptTestHarness" in the
+ * user guide).
+ */
+package org.jikesrvm.tools.oth;
