e3ab412a3cf15952d4fff676a2dccc093ecfc71b
Erik Brangs <erik.brangs@gmx.de>
2014-07-03 10:22:03 +0200

    RVM-1078 : Add a unit test for SplitBasicBlock.

---
+++ rvm/test-src/org/jikesrvm/compilers/opt/lir2mir/SplitBasicBlockTest.java
@@ -0,0 +1,193 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package org.jikesrvm.compilers.opt.lir2mir;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.jikesrvm.compilers.opt.driver.OptConstants.EPILOGUE_BLOCK_BCI;
+import static org.jikesrvm.compilers.opt.driver.OptConstants.PROLOGUE_BLOCK_BCI;
+import static org.jikesrvm.compilers.opt.ir.Operators.FENCE;
+import static org.jikesrvm.compilers.opt.ir.Operators.INT_IFCMP;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+
+import java.util.Enumeration;
+
+import org.jikesrvm.VM;
+import org.jikesrvm.compilers.opt.OptOptions;
+import org.jikesrvm.compilers.opt.ir.BasicBlock;
+import org.jikesrvm.compilers.opt.ir.ControlFlowGraph;
+import org.jikesrvm.compilers.opt.ir.Empty;
+import org.jikesrvm.compilers.opt.ir.IR;
+import org.jikesrvm.compilers.opt.ir.IfCmp;
+import org.jikesrvm.compilers.opt.ir.Instruction;
+import org.jikesrvm.junit.runners.VMRequirements;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VMRequirements.class)
+public class SplitBasicBlockTest {
+
+  private SplitBasicBlock splitPhase;
+
+  @Before
+  public void createPhase() {
+    splitPhase = new SplitBasicBlock();
+  }
+
+  @Test
+  public void doesNotThrowExceptionsOnCFGWithoutInstructions() {
+    int maxInstPerBlock = 0;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    splitPhase.perform(ir);
+  }
+
+  @Test
+  public void doesNotAddInstructionsToEmptyCFG() {
+    int maxInstPerBlock = 0;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    splitPhase.perform(ir);
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+  @Test
+  public void doesNotSplitEmptyCFG() {
+    int maxInstPerBlock = 1;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    int nodeNumberBefore = ir.cfg.numberOfNodes();
+    splitPhase.perform(ir);
+    int nodeNumberAfter = ir.cfg.numberOfNodes();
+    assertThat(nodeNumberAfter, is(nodeNumberBefore));
+  }
+
+  private void assertThatInstructionCountForEachBlockIsAtMost(IR ir, int maxInstrPerBlock) {
+    Enumeration<BasicBlock> blocks = ir.getBasicBlocks();
+    assertThat(blocks.hasMoreElements(), is(true));
+    while (blocks.hasMoreElements()) {
+      BasicBlock block = blocks.nextElement();
+
+      // Note: This is slow but it's required to determine if the compiler phase
+      // works correctly
+      int numberOfRealInstructions = block.getNumberOfRealInstructions();
+
+      assertThat(numberOfRealInstructions, lessThanOrEqualTo(maxInstrPerBlock));
+    }
+  }
+
+  private IR createIRWithEmptyCFG(int maxInstPerBlock) {
+    // The IR constructor will fail if the optimizing compiler isn't available
+    // and assertions are enabled
+    assumeThat(VM.BuildForOptCompiler, is(true));
+
+    OptOptions opts = new OptOptions();
+    IR ir = new IR(null, null, opts);
+    opts.L2M_MAX_BLOCK_SIZE = maxInstPerBlock;
+    ir.cfg = new ControlFlowGraph(0);
+    BasicBlock prologue = new BasicBlock(PROLOGUE_BLOCK_BCI, null, ir.cfg);
+    BasicBlock epilogue = new BasicBlock(EPILOGUE_BLOCK_BCI, null, ir.cfg);
+    ir.cfg.addLastInCodeOrder(prologue);
+    ir.cfg.addLastInCodeOrder(epilogue);
+    BasicBlock exit = ir.cfg.exit();
+    epilogue.insertOut(exit);
+    return ir;
+  }
+
+  @Ignore("fails because of an off-by-one error")
+  @Test
+  public void doesNotCreateNewBlocksWhenNumberOfInstructionsMatchesLimit() {
+    int maxInstPerBlock = 2;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+
+    int nodeNumberBefore = ir.cfg.numberOfNodes();
+
+    addNumberOfInstructionsToBlock(ir, 2);
+
+    splitPhase.perform(ir);
+
+    int nodeNumberAfter = ir.cfg.numberOfNodes();
+    assertThat(nodeNumberAfter, is(nodeNumberBefore));
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+  @Test
+  public void splitsAllBlocksThatHaveTooManyInstructions() {
+    int maxInstPerBlock = 2;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    addNumberOfInstructionsToBlock(ir, 13);
+    splitPhase.perform(ir);
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+  @Ignore("fails because of an off-by-one error")
+  @Test(timeout = 1000)
+  public void worksCorrectlyForALimitOfOneInstructionPerBlock() {
+    int maxInstPerBlock = 1;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    int nodeNumberBefore = ir.cfg.numberOfNodes();
+    addNumberOfInstructionsToBlock(ir, 1);
+    splitPhase.perform(ir);
+    int nodeNumberAfter = ir.cfg.numberOfNodes();
+    assertThat(nodeNumberAfter, is(nodeNumberBefore));
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+  private void addNumberOfInstructionsToBlock(IR ir, int count) {
+    Enumeration<BasicBlock> blocks = ir.getBasicBlocks();
+    while (blocks.hasMoreElements()) {
+      BasicBlock bb = blocks.nextElement();
+      for (int currInstCount = 0; currInstCount < count; currInstCount++) {
+        bb.appendInstruction(Empty.create(FENCE));
+      }
+    }
+  }
+
+  @Ignore("fails because limit is invalid and not checked")
+  @Test(timeout = 1000)
+  public void doesNotCheckSanityOfInstructionLimit() {
+    int maxInstPerBlock = 0;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    int nodeNumberBefore = ir.cfg.numberOfNodes();
+    addNumberOfInstructionsToBlock(ir, 1);
+    splitPhase.perform(ir);
+    int nodeNumberAfter = ir.cfg.numberOfNodes();
+    assertThat(nodeNumberAfter, is(nodeNumberBefore));
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+  @Ignore("fails because of off-by-one error")
+  @Test
+  public void branchesAtTheEndOfTheBlockDoNotCountAsIntructions() {
+    int maxInstPerBlock = 3;
+    IR ir = createIRWithEmptyCFG(maxInstPerBlock);
+    int nodeNumberBefore = ir.cfg.numberOfNodes();
+
+    Enumeration<BasicBlock> blocks = ir.getBasicBlocks();
+
+    while (blocks.hasMoreElements()) {
+      BasicBlock bb = blocks.nextElement();
+      bb.appendInstruction(Empty.create(FENCE));
+      bb.appendInstruction(Empty.create(FENCE));
+      Instruction i = IfCmp.create(INT_IFCMP, null, null, null, null, null, null);
+      bb.appendInstruction(i);
+    }
+
+    splitPhase.perform(ir);
+
+    int nodeNumberAfter = ir.cfg.numberOfNodes();
+    assertThat(nodeNumberAfter, is(nodeNumberBefore));
+    assertThatInstructionCountForEachBlockIsAtMost(ir, maxInstPerBlock);
+  }
+
+}
