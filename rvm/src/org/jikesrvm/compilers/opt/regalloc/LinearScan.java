904a85d96b6ac7e1d67ed0a1755587d64988cce5
Erik Brangs <erik.brangs@gmx.de>
2014-09-08 18:39:00 +0200

    RVM-1087 : Move the static nested class IncreasingStartIntervalSet to its own file.

--- rvm/src/org/jikesrvm/compilers/opt/regalloc/LinearScan.java
+++ rvm/src/org/jikesrvm/compilers/opt/regalloc/LinearScan.java
@@ -1582,41 +1582,6 @@ public final class LinearScan extends OptimizationPlanCompositeElement {
   }
 
   /**
-   * Implements a set of Basic Intervals, sorted by start number.
-   * This version uses container-mapping as a function in the comparator.
-   */
-  static class IncreasingStartMappedIntervalSet extends IntervalSet {
-    /** Support for Set serialization */
-    static final long serialVersionUID = -975667667343524421L;
-
-    private static class StartComparator implements Comparator<BasicInterval> {
-      @Override
-      public int compare(BasicInterval b1, BasicInterval b2) {
-        int result = b1.getBegin() - b2.getBegin();
-        if (result == 0) {
-          result = b1.getEnd() - b2.getEnd();
-        }
-        if (result == 0) {
-          if (b1 instanceof MappedBasicInterval) {
-            if (b2 instanceof MappedBasicInterval) {
-              MappedBasicInterval mb1 = (MappedBasicInterval) b1;
-              MappedBasicInterval mb2 = (MappedBasicInterval) b2;
-              return mb1.container.getRegister().number - mb2.container.getRegister().number;
-            }
-          }
-        }
-        return result;
-      }
-    }
-
-    static final StartComparator c = new StartComparator();
-
-    IncreasingStartMappedIntervalSet() {
-      super(c);
-    }
-  }
-
-  /**
    * Implements a set of Basic Intervals, sorted by end number.
    * This version uses container-mapping as a function in the comparator.
    */
