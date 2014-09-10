98af541629ddfe50c6a999e6d9e224bce316c28e
Erik Brangs <erik.brangs@gmx.de>
2014-09-10 15:49:43 +0200

    Add equals() and hashCode() to ReorderingPhase.Edge to fix FindBugs warnings and ensure that (x.compareTo(y)==0) == (x.equals(y)).

--- rvm/src/org/jikesrvm/compilers/opt/controlflow/ReorderingPhase.java
+++ rvm/src/org/jikesrvm/compilers/opt/controlflow/ReorderingPhase.java
@@ -354,6 +354,7 @@ public final class ReorderingPhase extends CompilerPhase {
   }
 
   private static final class Edge implements Comparable<Edge> {
+
     final BasicBlock source;
     final BasicBlock target;
     final float weight;
@@ -367,6 +368,40 @@ public final class ReorderingPhase extends CompilerPhase {
     @Override
     public String toString() {
       return weight + ": " + source.toString() + " -> " + target.toString();
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((source == null) ? 0 : source.hashCode());
+      result = prime * result + ((target == null) ? 0 : target.hashCode());
+      result = prime * result + Float.floatToIntBits(weight);
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Edge other = (Edge) obj;
+      if (source == null) {
+        if (other.source != null)
+          return false;
+      } else if (!source.equals(other.source))
+        return false;
+      if (target == null) {
+        if (other.target != null)
+          return false;
+      } else if (!target.equals(other.target))
+        return false;
+      if (Float.floatToIntBits(weight) != Float.floatToIntBits(other.weight))
+        return false;
+      return true;
     }
 
     @Override
