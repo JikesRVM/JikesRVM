a870115f43df384a5a2905571139363aa9e180dd
Erik Brangs <erik.brangs@gmx.de>
2015-05-05 20:26:07 +0200

    RVM-1107 : Cleanup register usage and documentation in PPC RegisterConstants in preparation for removal of support for AIX.
    The 64-bit PowerPC ELF ABI also reserves r13 for use for system threads (see e.g. http://refspecs.linuxfoundation.org/ELF/ppc64/PPC-elf64abi-1.9.html#REG ).

--- rvm/src/org/jikesrvm/ppc/RegisterConstants.java
+++ rvm/src/org/jikesrvm/ppc/RegisterConstants.java
@@ -32,7 +32,7 @@ public interface RegisterConstants {
   int LAST_OS_PARAMETER_GPR = 10;
   int FIRST_OS_VOLATILE_GPR = 3;
   int LAST_OS_VOLATILE_GPR = 12;
-  int FIRST_OS_NONVOLATILE_GPR = (VM.BuildForAix && VM.BuildFor64Addr) ? 14 : 13;
+  int FIRST_OS_NONVOLATILE_GPR = (VM.BuildForPowerOpenABI) ? 14 : 13;
   int LAST_OS_NONVOLATILE_GPR = 31;
   int FIRST_OS_PARAMETER_FPR = 1;
   int LAST_OS_PARAMETER_FPR = VM.BuildForLinux ? 8 : 13;
@@ -53,7 +53,7 @@ public interface RegisterConstants {
   int LAST_VOLATILE_GPR = LAST_OS_PARAMETER_GPR;
   int FIRST_SCRATCH_GPR = LAST_VOLATILE_GPR + 1;
   int LAST_SCRATCH_GPR = LAST_OS_VOLATILE_GPR;
-  // AIX 64 bit ABI reserves R13 for use by libpthread; therefore Jikes RVM doesn't touch it.
+  // PowerPC 64 ELF ABI reserves R13 for use by libpthread; therefore Jikes RVM doesn't touch it.
   int FIRST_RVM_RESERVED_NV_GPR = VM.BuildFor64Addr ? 14 : 13;
   int THREAD_REGISTER = FIRST_RVM_RESERVED_NV_GPR;
 
