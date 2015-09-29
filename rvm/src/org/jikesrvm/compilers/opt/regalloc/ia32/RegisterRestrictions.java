4a5f01ba19350cf812452077a92dda9e966e8211
Erik Brangs <erik.brangs@gmx.de>
2015-09-29 14:19:21 +0200

    RVM-1063 : Add missing register restrictions for MOVSXQ and MOVZXQ (original changes by Da Feng).
    This change is part of Da Feng's x64 patch set.
    
    I've added a few restrictions that aren't present in Da Feng's patch.

--- rvm/src/org/jikesrvm/compilers/opt/regalloc/ia32/RegisterRestrictions.java
+++ rvm/src/org/jikesrvm/compilers/opt/regalloc/ia32/RegisterRestrictions.java
@@ -53,8 +53,12 @@ import static org.jikesrvm.compilers.opt.ir.Operators.IA32_FCOMI_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_FNINIT;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_IMUL2_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVD_opcode;
+import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVSXQ__B_opcode;
+import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVSXQ__W_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVSX__B_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVSX__W_opcode;
+import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVZXQ__B_opcode;
+import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVZXQ__W_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVZX__B_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MOVZX__W_opcode;
 import static org.jikesrvm.compilers.opt.ir.Operators.IA32_MULSD_opcode;
@@ -151,7 +155,9 @@ public class RegisterRestrictions extends GenericRegisterRestrictions
         }
         break;
         case IA32_MOVZX__B_opcode:
-        case IA32_MOVSX__B_opcode: {
+        case IA32_MOVSX__B_opcode:
+        case IA32_MOVZXQ__B_opcode:
+        case IA32_MOVSXQ__B_opcode: {
           if (MIR_Unary.getVal(s).isRegister()) {
             RegisterOperand val = MIR_Unary.getVal(s).asRegister();
             restrictTo8Bits(val.getRegister());
@@ -382,13 +388,17 @@ public class RegisterRestrictions extends GenericRegisterRestrictions
       }
       break;
       case IA32_MOVZX__B_opcode:
-      case IA32_MOVSX__B_opcode: {
+      case IA32_MOVSX__B_opcode:
+      case IA32_MOVZXQ__B_opcode:
+      case IA32_MOVSXQ__B_opcode: {
         RegisterOperand op = MIR_Unary.getResult(s).asRegister();
         if (op.getRegister() == r) return true;
       }
       break;
       case IA32_MOVZX__W_opcode:
-      case IA32_MOVSX__W_opcode: {
+      case IA32_MOVSX__W_opcode:
+      case IA32_MOVZXQ__W_opcode:
+      case IA32_MOVSXQ__W_opcode: {
         RegisterOperand op = MIR_Unary.getResult(s).asRegister();
         if (op.getRegister() == r) return true;
       }
@@ -445,7 +455,9 @@ public class RegisterRestrictions extends GenericRegisterRestrictions
     // Look at 8-bit restrictions.
     switch (s.getOpcode()) {
       case IA32_MOVZX__B_opcode:
-      case IA32_MOVSX__B_opcode: {
+      case IA32_MOVSX__B_opcode:
+      case IA32_MOVZXQ__B_opcode:
+      case IA32_MOVSXQ__B_opcode: {
         if (MIR_Unary.getVal(s).isRegister()) {
           RegisterOperand val = MIR_Unary.getVal(s).asRegister();
           if (val.getRegister() == symb) {
