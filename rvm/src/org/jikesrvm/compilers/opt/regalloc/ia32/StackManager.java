JikesRVM/JikesRVM

commit efcb8fe9370f8975db54ec600fe39ba888e15acb

RVM-1063 : Move responsibility for determining type of values from GenericStackManager to architecture-specific StackManagers.
Defining architecture-independent value types unnecessarily constrains the architecture-specific stack managers. Moreover, the types of values that are interesting are architecture-dependent. For example, the x64 StackManager needs to distinguish between 32-bit values and 64-bit values in order to make sure that non-quad instructions are not falsely promoted to quad instructions.

For now, the code can just rely on the types provided by Register which provide all the information that the StackManagers for the currently supported architectures require.

rvm/src/org/jikesrvm/compilers/opt/regalloc/ia32/StackManager.java
@@ -117,17 +117,17 @@ public final class StackManager extends GenericStackManager {
    * @return the size of a type of value, in bytes.
    * NOTE: For the purpose of register allocation, an x87 FLOAT_VALUE is 64 bits!
    */
-  private static byte getSizeOfType(byte type) {
-    switch (type) {
-      case INT_VALUE:
-        return (byte) (WORDSIZE);
-      case FLOAT_VALUE:
-        if (SSE2_FULL) return (byte) BYTES_IN_FLOAT;
-      case DOUBLE_VALUE:
-        return (byte) BYTES_IN_DOUBLE;
-      default:
-        OptimizingCompilerException.TODO("getSizeOfValue: unsupported");
-        return 0;
+  private static byte getSizeOfType(Register type) {
+    if (type.isNatural()) {
+      return (byte) (WORDSIZE);
+    } else if (type.isFloat()) {
+      if (SSE2_FULL) return (byte) BYTES_IN_FLOAT;
+      return (byte) BYTES_IN_DOUBLE;
+    } else if (type.isDouble()) {
+      return (byte) BYTES_IN_DOUBLE;
+    } else {
+      OptimizingCompilerException.TODO("getSizeOfValue: unsupported: " + type);
+      return (byte) -1;
     }
   }
 
@@ -135,18 +135,16 @@ public final class StackManager extends GenericStackManager {
    * @param type one of INT_VALUE, FLOAT_VALUE, or DOUBLE_VALUE
    * @return the move operator for a type of value.
    */
-  private static Operator getMoveOperator(byte type) {
-    switch (type) {
-      case INT_VALUE:
-        return IA32_MOV;
-      case DOUBLE_VALUE:
-        if (SSE2_FULL) return IA32_MOVSD;
-      case FLOAT_VALUE:
-        if (SSE2_FULL) return IA32_MOVSS;
-        return IA32_FMOV;
-      default:
-        OptimizingCompilerException.TODO("getMoveOperator: unsupported");
-        return null;
+  private static Operator getMoveOperator(Register type) {
+    if (type.isNatural()) {
+      return IA32_MOV;
+    } else if (type.isDouble()) {
+      return IA32_MOVSD;
+    } else if (type.isFloat()) {
+      return IA32_MOVSS;
+    } else {
+      OptimizingCompilerException.TODO("getMoveOperator: unsupported: " + type);
+      return null;
     }
   }
 
@@ -163,41 +161,32 @@ public final class StackManager extends GenericStackManager {
   }
 
   @Override
-  public void insertSpillBefore(Instruction s, Register r, byte type, int location) {
-
+  public void insertSpillBefore(Instruction s, Register r, Register type, int location) {
     Operator move = getMoveOperator(type);
     byte size = getSizeOfType(type);
     RegisterOperand rOp;
-    switch (type) {
-      case FLOAT_VALUE:
-        rOp = F(r);
-        break;
-      case DOUBLE_VALUE:
-        rOp = D(r);
-        break;
-      default:
-        rOp = new RegisterOperand(r, PRIMITIVE_TYPE_FOR_WORD);
-        break;
+    if (type.isFloat()) {
+      rOp = F(r);
+    } else if (type.isDouble()) {
+      rOp = D(r);
+    } else {
+      rOp = new RegisterOperand(r, PRIMITIVE_TYPE_FOR_WORD);
     }
     StackLocationOperand spill = new StackLocationOperand(true, -location, size);
     s.insertBefore(MIR_Move.create(move, spill, rOp));
   }
 
   @Override
-  public void insertUnspillBefore(Instruction s, Register r, byte type, int location) {
+  public void insertUnspillBefore(Instruction s, Register r, Register type, int location) {
     Operator move = getMoveOperator(type);
     byte size = getSizeOfType(type);
     RegisterOperand rOp;
-    switch (type) {
-      case FLOAT_VALUE:
-        rOp = F(r);
-        break;
-      case DOUBLE_VALUE:
-        rOp = D(r);
-        break;
-      default:
-        rOp = new RegisterOperand(r, PRIMITIVE_TYPE_FOR_WORD);
-        break;
+    if (type.isFloat()) {
+      rOp = F(r);
+    } else if (type.isDouble()) {
+      rOp = D(r);
+    } else {
+      rOp = new RegisterOperand(r, PRIMITIVE_TYPE_FOR_WORD);
     }
     StackLocationOperand spill = new StackLocationOperand(true, -location, size);
     s.insertBefore(MIR_Move.create(move, rOp, spill));

rvm/src/org/jikesrvm/compilers/opt/regalloc/ppc/StackManager.java
@@ -195,14 +195,14 @@ public final class StackManager extends GenericStackManager {
    * @param location the spill location
    */
   @Override
-  public void insertSpillBefore(Instruction s, Register r, byte type, int location) {
+  public void insertSpillBefore(Instruction s, Register r, Register type, int location) {
 
     Register FP = ir.regpool.getPhysicalRegisterSet().getFP();
-    if (type == FLOAT_VALUE) {
+    if (type.isFloat()) {
       s.insertBefore(MIR_Store.create(PPC_STFS, F(r), A(FP), IC(location + BYTES_IN_ADDRESS - BYTES_IN_FLOAT)));
-    } else if (type == DOUBLE_VALUE) {
+    } else if (type.isDouble()) {
       s.insertBefore(MIR_Store.create(PPC_STFD, D(r), A(FP), IC(location)));
-    } else if (type == INT_VALUE) {      // integer or half of long
+    } else if (type.isNatural()) {      // integer or half of long
       s.insertBefore(MIR_Store.create(PPC_STAddr, A(r), A(FP), IC(location)));
     } else {
       throw new OptimizingCompilerException("insertSpillBefore", "unsupported type " + type);
@@ -234,17 +234,17 @@ public final class StackManager extends GenericStackManager {
    * @param location the spill location
    */
   @Override
-  public void insertUnspillBefore(Instruction s, Register r, byte type, int location) {
+  public void insertUnspillBefore(Instruction s, Register r, Register type, int location) {
     PhysicalRegisterSet phys = ir.regpool.getPhysicalRegisterSet().asPPC();
     Register FP = phys.getFP();
-    if (type == CONDITION_VALUE) {
+    if (type.isCondition()) {
       Register temp = phys.getTemp();
       s.insertBefore(MIR_Load.create(PPC_LWZ, I(temp), A(FP), IC(location + BYTES_IN_ADDRESS - BYTES_IN_INT)));
-    } else if (type == DOUBLE_VALUE) {
+    } else if (type.isDouble()) {
       s.insertBefore(MIR_Load.create(PPC_LFD, D(r), A(FP), IC(location)));
-    } else if (type == FLOAT_VALUE) {
+    } else if (type.isFloat()) {
       s.insertBefore(MIR_Load.create(PPC_LFS, F(r), A(FP), IC(location + BYTES_IN_ADDRESS - BYTES_IN_FLOAT)));
-    } else if (type == INT_VALUE) { // integer or half of long
+    } else if (type.isNatural()) { // integer or half of long
       s.insertBefore(MIR_Load.create(PPC_LAddr, A(r), A(FP), IC(location)));
     } else {
       throw new OptimizingCompilerException("insertUnspillBefore", "unknown type:" + type);

rvm/src/org/jikesrvm/compilers/opt/regalloc/GenericStackManager.java
@@ -43,16 +43,6 @@ import org.jikesrvm.compilers.opt.ir.operand.RegisterOperand;
  */
 public abstract class GenericStackManager extends IRTools {
 
-  /*
-   * Types of values stored in physical registers;
-   * These affect instruction selection for accessing
-   * the data
-   */
-  public static final byte INT_VALUE = 0;
-  public static final byte DOUBLE_VALUE = 1;
-  public static final byte FLOAT_VALUE = 2;
-  public static final byte CONDITION_VALUE = 3;
-
   protected static final boolean DEBUG = false;
   protected static final boolean VERBOSE = false;
   protected static final boolean VERBOSE_DEBUG = false;
@@ -346,7 +336,7 @@ public abstract class GenericStackManager extends IRTools {
     Register scratchContents = scratch.getCurrentContents();
     if (scratchContents != null) {
       int location = regAllocState.getSpill(scratchContents);
-      insertSpillBefore(s, scratch.scratch, getValueType(scratchContents), location);
+      insertSpillBefore(s, scratch.scratch, scratchContents, location);
     }
 
   }
@@ -362,7 +352,7 @@ public abstract class GenericStackManager extends IRTools {
     if (scratch.hadToSpill()) {
       // Restore the live contents into the scratch register.
       int location = regAllocState.getSpill(scratch.scratch);
-      insertUnspillBefore(s, scratch.scratch, getValueType(scratch.scratch), location);
+      insertUnspillBefore(s, scratch.scratch, scratch.scratch, location);
     }
   }
 
@@ -534,7 +524,7 @@ public abstract class GenericStackManager extends IRTools {
       int location = regAllocState.getSpill(current);
       int location2 = regAllocState.getSpill(symb);
       if (location != location2) {
-        insertSpillBefore(s, sr.scratch, getValueType(current), location);
+        insertSpillBefore(s, sr.scratch, current, location);
       }
     }
 
@@ -712,7 +702,7 @@ public abstract class GenericStackManager extends IRTools {
       // since symbReg must have been previously spilled, get the spill
       // location previous assigned to symbReg
       int location = regAllocState.getSpill(symb);
-      insertUnspillBefore(s, sr.scratch, getValueType(symb), location);
+      insertUnspillBefore(s, sr.scratch, symb, location);
 
       // we have not yet written to sr, so mark it 'clean'
       sr.setDirty(false);
@@ -756,11 +746,11 @@ public abstract class GenericStackManager extends IRTools {
       // Since this is a new scratch register, spill the old contents of
       // r if necessary.
       if (activeSet == null) {
-        insertSpillBefore(s, r, (byte) type, spillLocation);
+        insertSpillBefore(s, r, r, spillLocation);
         sr.setHadToSpill(true);
       } else {
         if (!isDeadBefore(r, s)) {
-          insertSpillBefore(s, r, (byte) type, spillLocation);
+          insertSpillBefore(s, r, r, spillLocation);
           sr.setHadToSpill(true);
         }
       }
@@ -1196,22 +1186,20 @@ public abstract class GenericStackManager extends IRTools {
    *
    * @param s the instruction before which the spill should occur
    * @param r the register (should be physical) to spill
-   * @param type one of INT_VALUE, FLOAT_VALUE, DOUBLE_VALUE, or
-   *                    CONDITION_VALUE
+   * @param type the register that's contained in the physical register
    * @param location the spill location
    */
-  public abstract void insertSpillBefore(Instruction s, Register r, byte type, int location);
+  public abstract void insertSpillBefore(Instruction s, Register r, Register type, int location);
 
   /**
    * Insert a spill of a physical register after instruction s.
    *
    * @param s the instruction after which the spill should occur
    * @param r the register (should be physical) to spill
-   * @param type one of INT_VALUE, FLOAT_VALUE, DOUBLE_VALUE, or
-   *                    CONDITION_VALUE
+   * @param type the register that's contained in the physical register
    * @param location the spill location
    */
-  public final void insertSpillAfter(Instruction s, Register r, byte type, int location) {
+  public final void insertSpillAfter(Instruction s, Register r, Register type, int location) {
     insertSpillBefore(s.nextInstructionInCodeOrder(), r, type, location);
   }
 
@@ -1221,11 +1209,10 @@ public abstract class GenericStackManager extends IRTools {
    *
    * @param s the instruction before which the spill should occur
    * @param r the register (should be physical) to spill
-   * @param type one of INT_VALUE, FLOAT_VALUE, DOUBLE_VALUE, or
-   *                    CONDITION_VALUE
+   * @param type the register that's contained in the physical register
    * @param location the spill location
    */
-  public abstract void insertUnspillBefore(Instruction s, Register r, byte type, int location);
+  public abstract void insertUnspillBefore(Instruction s, Register r, Register type, int location);
 
   /**
    * Insert a load of a physical register from a spill location before
@@ -1233,11 +1220,10 @@ public abstract class GenericStackManager extends IRTools {
    *
    * @param s the instruction before which the spill should occur
    * @param r the register (should be physical) to spill
-   * @param type one of INT_VALUE, FLOAT_VALUE, DOUBLE_VALUE, or
-   *                    CONDITION_VALUE
+   * @param type the register that's contained in the physical register
    * @param location the spill location
    */
-  public final void insertUnspillAfter(Instruction s, Register r, byte type, int location) {
+  public final void insertUnspillAfter(Instruction s, Register r, Register type, int location) {
     insertUnspillBefore(s.nextInstructionInCodeOrder(), r, type, location);
   }
 
@@ -1464,28 +1450,6 @@ l   */
       }
     }
     return null;
-  }
-
-  /**
-   * Given a symbolic register, return a code that indicates the type
-   * of the value stored in the register.
-   * Note: This routine returns INT_VALUE for longs
-   *
-   * @param r a symbolic register
-   * @return one of INT_VALUE, FLOAT_VALUE, DOUBLE_VALUE, CONDITION_VALUE
-   */
-  public final byte getValueType(Register r) {
-    if (r.isInteger() || r.isLong() || r.isAddress()) {
-      return INT_VALUE;
-    } else if (r.isCondition()) {
-      return CONDITION_VALUE;
-    } else if (r.isDouble()) {
-      return DOUBLE_VALUE;
-    } else if (r.isFloat()) {
-      return FLOAT_VALUE;
-    } else {
-      throw new OptimizingCompilerException("getValueType: unsupported " + r);
-    }
   }
 
   protected static int align(int number, int alignment) {

commit 17b1e00613e3627e0cf01f55c9c7c27890094d05

Fix development builds without SSE that I broke in efcb8fe9370f8975db54ec600fe39ba888e15acb .

rvm/src/org/jikesrvm/compilers/opt/regalloc/ia32/StackManager.java
@@ -145,9 +145,9 @@ public final class StackManager extends GenericStackManager {
     if (type.isNatural()) {
       return IA32_MOV;
     } else if (type.isDouble()) {
-      return IA32_MOVSD;
+      return SSE2_FULL ? IA32_MOVSD : IA32_FMOV;
     } else if (type.isFloat()) {
-      return IA32_MOVSS;
+      return SSE2_FULL ? IA32_MOVSS : IA32_FMOV;
     } else {
       OptimizingCompilerException.TODO("getMoveOperator: unsupported: " + type);
       return null;