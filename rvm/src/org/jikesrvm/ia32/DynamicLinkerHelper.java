76e332b2ae4032fbca9197520c821e3608e045d2
Erik Brangs <erik.brangs@gmx.de>
2016-12-20 18:15:15 +0100

    Make code of IA32 DynamicLinkerHelper slightly more readable.
    It is confusing to refer to the receiver object as the first parameter because both the bridge method and the method that is being bridged to may have parameters. The T0_SAVE_OFFSET, on the other hand, is only present for DynamicBridge frames and contains the receiver object of the target method so it's a little bit clearer what the code does.

--- rvm/src/org/jikesrvm/ia32/DynamicLinkerHelper.java
+++ rvm/src/org/jikesrvm/ia32/DynamicLinkerHelper.java
@@ -12,7 +12,7 @@
  */
 package org.jikesrvm.ia32;
 
-import static org.jikesrvm.ia32.BaselineConstants.STACKFRAME_FIRST_PARAMETER_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.T0_SAVE_OFFSET;
 import static org.jikesrvm.ia32.RegisterConstants.NUM_PARAMETER_GPRS;
 
 import org.jikesrvm.VM;
@@ -30,7 +30,8 @@ public abstract class DynamicLinkerHelper {
   /**
    * Reach up two stack frames into a frame that is compiled
    * with the DynamicBridge register protocol and grab
-   * the receiver object of the invoke (ie the first param).
+   * the receiver object of the invoke.
+   * <p>
    * NOTE: assumes that caller has disabled GC.
    *
    * @return the receiver object for the method invocation
@@ -41,7 +42,7 @@ public abstract class DynamicLinkerHelper {
     callingFrame = Magic.getCallerFramePointer(callingFrame);
     Address location = Address.zero();
     if (0 < NUM_PARAMETER_GPRS) {
-      location = callingFrame.plus(STACKFRAME_FIRST_PARAMETER_OFFSET).loadAddress();
+      location = callingFrame.plus(T0_SAVE_OFFSET).loadAddress();
 
     } else {
       VM.sysFail("DynamicLinerHelper: assumes at least one param passed in registers");
