b08dfdc0ee1f161b2bfc7cff987af3b1269f8f56
Erik Brangs <erik.brangs@gmx.de>
2018-06-10 15:30:29 +0200

    RVM-549 : Update license headers for OpenJDK replacement classes to refer to the GPL.

--- libraryInterface/OpenJDK/GPL/src/org/jikesrvm/classlibrary/openjdk/replacements/java_lang_ClassLoader.java
+++ libraryInterface/OpenJDK/GPL/src/org/jikesrvm/classlibrary/openjdk/replacements/java_lang_ClassLoader.java
@@ -1,10 +1,26 @@
 /*
- * This file is part of the Jikes RVM project (http://jikesrvm.org). This file
- * is licensed to You under the Eclipse Public License (EPL); You may not use
- * this file except in compliance with the License. You may obtain a copy of the
- * License at http://www.opensource.org/licenses/eclipse-1.0.php See the
- * COPYRIGHT.txt file distributed with this work for information regarding
- * copyright ownership.
+ * Copyright (c) 1994, 2009, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package org.jikesrvm.classlibrary.openjdk.replacements;
 
