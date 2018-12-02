6d9fdc30d29edc4543e1c056d8abe37dcb6f94ef
Erik Brangs <erik.brangs@gmx.de>
2018-12-02 09:23:10 +0100

    RVM-549 : Add implementations for java.lang.Object and java.lang.Class
    For java.lang.Object, we can't use the replacement class mechanism. An attempt to do so causes bootstrap problems because the classloading will attempt to do annotation processing which requires that java.lang.Object is resolved.
    
    For java.lang.Class, the boot image writer needs to be able to set up the RVMType when copying the objects. This is achieved by naming the RVMType field in java.lang.Class as "type" as in GNU Classpath. This leads to re-using the logic from GNU Classpath in the boot image writer.

--- libraryInterface/OpenJDK/GPL/src/org/jikesrvm/classlibrary/openjdk/replacements/java_lang_Class.java
+++ libraryInterface/OpenJDK/GPL/src/org/jikesrvm/classlibrary/openjdk/replacements/java_lang_Class.java
@@ -24,13 +24,131 @@
  */
 package org.jikesrvm.classlibrary.openjdk.replacements;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.jikesrvm.VM;
 import org.jikesrvm.classloader.TypeReference;
 import org.vmmagic.pragma.ReplaceClass;
 import org.vmmagic.pragma.ReplaceMember;
 
+import sun.reflect.ConstantPool;
+
 @ReplaceClass(className = "java.lang.Class")
-public class java_lang_Class {
+public class java_lang_Class<T> {
+
+  @ReplaceMember
+  private static void registerNatives() {
+    // nothing to do
+  }
+
+  @ReplaceMember
+  private static Class<?> forName0(String name, boolean initialize, ClassLoader loader, Class<?> caller)
+      throws ClassNotFoundException {
+    VM.sysFail("forName0");
+    return null;
+  }
+
+  @ReplaceMember
+  public boolean isInstance(Object obj) {
+    VM.sysFail("isInstance: " + obj);
+    return false;
+  }
+
+  @ReplaceMember
+  public boolean isAssignableFrom(Class<?> cls) {
+    VM.sysFail("isAssignableFrom: " + cls);
+    return false;
+  }
+
+  @ReplaceMember
+  public boolean isInterface() {
+    VM.sysFail("isInterface");
+    return false;
+  }
+
+  @ReplaceMember
+  public boolean isArray() {
+    VM.sysFail("isArray");
+    return false;
+  }
+
+  @ReplaceMember
+  public boolean isPrimitive() {
+    VM.sysFail("isPrimitive");
+    return false;
+  }
+
+  @ReplaceMember
+  private String getName0() {
+    VM.sysFail("getName");
+    return null;
+  }
+
+  @ReplaceMember
+  ClassLoader getClassLoader0() {
+    VM.sysFail("getClassLoader0");
+    return null;
+  }
+
+  @ReplaceMember
+  public Class<? super T> getSuperclass() {
+    VM.sysFail("getSuperclass");
+    return null;
+  }
+
+  @ReplaceMember
+  public Class<?>[] getInterfaces() {
+    VM.sysFail("getInterfaces");
+    return null;
+  }
+
+  @ReplaceMember
+  public Class<?> getComponentType() {
+    VM.sysFail("getComponentType");
+    return null;
+  }
+
+  @ReplaceMember
+  public int getModifiers() {
+    VM.sysFail("getModifiers");
+    return 0;
+  }
+
+  @ReplaceMember
+  public Object[] getSigners() {
+    VM.sysFail("getSigners");
+    return null;
+  }
+
+  @ReplaceMember
+  void setSigners(Object[] signers) {
+    VM.sysFail("setSigners");
+  }
+
+  @ReplaceMember
+  private Object[] getEnclosingMethod0() {
+    VM.sysFail("getEnclosingMethod0");
+    return null;
+  }
+
+  @ReplaceMember
+  private Class<?> getDeclaringClass0() {
+    VM.sysFail("getDeclaringClass0");
+    return null;
+  }
+
+  @ReplaceMember
+  private java.security.ProtectionDomain getProtectionDomain0() {
+    VM.sysFail("getProtectionDomain0");
+    return null;
+  }
+
+  @ReplaceMember
+  void setProtectionDomain0(java.security.ProtectionDomain pd) {
+    VM.sysFail("setProtectionDomain0");
+  }
 
   @ReplaceMember
   static Class<?> getPrimitiveClass(String className) {
@@ -40,6 +158,54 @@ public class java_lang_Class {
     }
     VM.sysFail("Unknown primitive type name: " + className);
     return null;
+  }
+
+  @ReplaceMember
+  private String getGenericSignature() {
+    VM.sysFail("getGenericSignature");
+    return null;
+  }
+
+  @ReplaceMember
+  byte[] getRawAnnotations() {
+    VM.sysFail("getRawAnnotations");
+    return null;
+  }
+
+  @ReplaceMember
+  ConstantPool getConstantPool() {
+    VM.sysFail("getConstantPool");
+    return null;
+  }
+
+  @ReplaceMember
+  private Field[] getDeclaredFields0(boolean publicOnly) {
+    VM.sysFail("getDeclaredFields0");
+    return null;
+  }
+
+  @ReplaceMember
+  private Method[] getDeclaredMethods0(boolean publicOnly) {
+    VM.sysFail("getDeclaredMethods0");
+    return null;
+  }
+
+  @ReplaceMember
+  private Constructor<T>[] getDeclaredConstructors0(boolean publicOnly) {
+    VM.sysFail("getDeclaredConstructors0");
+    return null;
+  }
+
+  @ReplaceMember
+  private Class<?>[] getDeclaredClasses0() {
+    VM.sysFail("getDeclaredClasses0");
+    return null;
+  }
+
+  @ReplaceMember
+  private static boolean desiredAssertionStatus0(Class<?> clazz) {
+    VM.sysFail("desiredAssertionStatus0");
+    return false;
   }
 
 }
--- rvm/src/org/jikesrvm/classlibrary/ClassLibraryHelpers.java
+++ rvm/src/org/jikesrvm/classlibrary/ClassLibraryHelpers.java
@@ -19,12 +19,22 @@ import org.jikesrvm.classloader.MemberReference;
 import org.jikesrvm.classloader.RVMClass;
 import org.jikesrvm.classloader.RVMField;
 import org.jikesrvm.classloader.RVMMethod;
+import org.jikesrvm.classloader.RVMType;
 import org.jikesrvm.classloader.TypeReference;
 import org.jikesrvm.runtime.Reflection;
 import org.jikesrvm.runtime.RuntimeEntrypoints;
 
 // TODO decide where to put this code before merging the OpenJDK branch
 public class ClassLibraryHelpers {
+
+  /**
+   * Field name for the field in {@link Class} that contains the {@link RVMType}
+   * for the class.
+   * <p>
+   * For now, we use the same name for this field as in GNU Classpath in order to
+   * reuse the code for setup (e.g. in the boot image writer when copying Class objects).
+   */
+  private static final String RVM_TYPE_FIELD_NAME_FOR_JAVA_LANG_CLASS = "type";
 
   public static RVMField rvmThreadField;
   public static RVMField rvmTypeField;
@@ -72,7 +82,7 @@ public class ClassLibraryHelpers {
     } else if (typeRef == TypeReference.findOrCreate(java.lang.Class.class)) {
       // TODO this really should be final, but that would mean we'd have to add a constructor, too
       short modifiers = ACC_SYNTHETIC | ACC_PRIVATE;
-      Atom fieldName = Atom.findOrCreateUnicodeAtom("rvmType");
+      Atom fieldName = Atom.findOrCreateUnicodeAtom(RVM_TYPE_FIELD_NAME_FOR_JAVA_LANG_CLASS);
       Atom fieldDescriptor = Atom.findOrCreateUnicodeAtom("Lorg/jikesrvm/classloader/RVMType;");
       MemberReference memRef = MemberReference.findOrCreate(typeRef, fieldName, fieldDescriptor);
       RVMField newField = RVMField.createSyntheticFieldForReplacementClass(typeRef, modifiers, fieldName, null, memRef);
@@ -80,7 +90,7 @@ public class ClassLibraryHelpers {
       System.arraycopy(declaredFields, 0, newDeclaredFields, 0, declaredFields.length);
       newDeclaredFields[newDeclaredFields.length - 1] = newField;
       ClassLibraryHelpers.rvmTypeField = newField;
-      if (VM.TraceClassLoading) VM.sysWriteln("Added rvmType field to java.lang.Class");
+      if (VM.TraceClassLoading) VM.sysWriteln("Added " + RVM_TYPE_FIELD_NAME_FOR_JAVA_LANG_CLASS + " field to java.lang.Class");
       return newDeclaredFields;
     }
 
