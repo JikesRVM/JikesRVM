4c79be43cc5c13706cfdbd1e814ec03c3b5c740e
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-05-22 23:23:58 -0400

    increasing heapsize again

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 
 #ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
 
-ant  -Dconfig.include.aos=true -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none 
+ant  -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=3000 -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none 
