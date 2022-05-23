07d0d6c7525c66599fc2d31e276b936bafa02a7b
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-05-22 22:54:30 -0400

    disable perfevent

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-!bin/bash
+#!bin/bash
 
-ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=true
+ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
