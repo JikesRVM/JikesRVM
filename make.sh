a9289a3429386a075d40581122a39f3189fc3e3a
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-05-22 23:18:08 -0400

    removing default heapsize

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 #!bin/bash
 
-ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
+#ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
+
+ant  -Dconfig.include.aos=true -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none 
