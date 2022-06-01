c1884a28e5634424e6ebb5e519e32939cbbceba8
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-05-31 21:57:04 -0400

    decreasing max heapsize build successful

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
+ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=2047 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=false
