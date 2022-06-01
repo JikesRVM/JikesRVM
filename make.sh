0d7ad473c1741729e68b7b7a3628831d4af78d7e
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-05-31 21:32:06 -0400

    make.sh addedg

---
+++ make.sh
@@ -0,0 +1,1 @@
+ant -Dconfig.include.aos=true -Dconfig.default-heapsize.maximum=5000 -Dconfig.runtime.compiler=opt -Dconfig.bootimage.compiler=opt -Dconfig.assertions=none -Dconfig.include.perfevent=true
