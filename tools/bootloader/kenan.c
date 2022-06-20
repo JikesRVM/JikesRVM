ec5374d14510cd8f5eb129c08b88c67bad4aaae7
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-06-20 15:26:43 -0400

    restoring freqavailable in kenan.c

--- tools/bootloader/kenan.c
+++ tools/bootloader/kenan.c
@@ -624,22 +624,21 @@ EXTERNAL void FreqAvailable(int *freq){
     int rc;
     size_t data_length, data_written;
 
-		// f = fopen(filename, "r");
-		// if (f == NULL) {
-		// 	//LOGI("Failed to open %s: %s", filename, strerror(errno));
-		// }
+	f = fopen(filename, "r");
+	if (f == NULL) {
+		//LOGI("Failed to open %s: %s", filename, strerror(errno));
+	}
 
-//	int freq[20];
+	int freq[20];
 	int string;
 	int count = 0;
 	int temp = 100;
 
-	// while (fscanf (f, "%d", &string) != EOF){
-	// 		freq[count] = string;
-	// 		count++;
-
-	// }
-    // rc = fclose(f);
+	while (fscanf (f, "%d", &string) != EOF){
+			freq[count] = string;
+			count++;
+	}
+    rc = fclose(f);
     if (rc != 0) {
         //LOGI("Failed to close %s: %s", filename, strerror(rc));
     }
