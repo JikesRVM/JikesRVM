209fbf10705587720983b07a57ca7ad7b724caad
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-08-03 10:56:50 -0400

    generate settings edit

--- replace.sh
+++ replace.sh
@@ -1,10 +1,4 @@
-sed -i .bak 's/\$\$\$\$\$/\./' sunflow_settings
-sed -i .bak 's/\$\$\$\$\$/\./' luindex_settings
-sed -i .bak 's/\$\$\$\$\$/\./' pmd_settings
-sed -i .bak 's/\$\$\$\$\$/\./' avrora_settings
-sed -i .bak 's/\$\$\$\$\$/\./' bloat_settings
-sed -i .bak 's/\$\$\$\$\$/\./' fop_settings
-sed -i .bak 's/\$\$\$\$\$/\./' antlr_settings
-sed -i .bak 's/\$\$\$\$\$/\./' jython_settings
+sed -i 's/\$\$\$\$\$/\./' $1
+
 
 
