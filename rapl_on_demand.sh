d7a7d20be7a0c2220d72edf825775c65efb4915b
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-08-05 15:24:55 -0400

    rapl_on_demand.sh dupate

--- rapl_on_demand.sh
+++ rapl_on_demand.sh
@@ -57,5 +57,5 @@ itercount=$(echo $itercount | cut -d' ' -f 1)
 
 sudo mv iteration_times counter_based_sampling_iteration_times_$i
 sudo mv kenan.csv counter_based_sampling_kenan.${i}.csv
-sudo mv kenan_energy_$i
+sudo mv kenan_energy kenan_energy_$i
 sudo mv execution_time execution_time_${i}.csv
