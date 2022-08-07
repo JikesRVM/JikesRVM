6ee21f9af0256cbdd6dd4520f3d49927a5a3175f
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-08-07 02:16:30 -0400

    adding all benchmarks for profile_all_benchmarks.sh

--- profile_all_benchmarks.sh
+++ profile_all_benchmarks.sh
@@ -16,11 +16,11 @@ mkdir $expname
 # sudo bash profile.sh avrora new $iters 32 2 $expname
 
 
-# sudo bash run_experiments.sh fop old $iters 32 2 $expname
-# sudo bash run_experiments.sh pmd old $((iters)) 32 2 $expname
-# sudo bash run_experiments.sh jython old $((iters)) 32 2 $expname
-# sudo bash run_experiments.sh bloat old $((iters)) 32 2 $expname
-# sudo bash run_experiments.sh antlr old $((iters)) 32 2 $expname
-# sudo bash run_experiments.sh luindex new $iters 32 2 $expname
+sudo bash run_experiments.sh fop old $iters 32 2 $expname
+sudo bash run_experiments.sh pmd old $((iters)) 32 2 $expname
+sudo bash run_experiments.sh jython old $((iters)) 32 2 $expname
+sudo bash run_experiments.sh bloat old $((iters)) 32 2 $expname
+sudo bash run_experiments.sh antlr old $((iters)) 32 2 $expname
+sudo bash run_experiments.sh luindex new $iters 32 2 $expname
 sudo bash run_experiments.sh sunflow new 10 8 2  $expname
-# sudo bash run_experiments.sh avrora new $iters 32 2 $expname
+sudo bash run_experiments.sh avrora new $iters 32 2 $expname
