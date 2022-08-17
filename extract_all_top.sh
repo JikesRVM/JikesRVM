7917acde7a505459c08465cc9440a7e6d1ed6cad
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-08-16 21:08:39 -0400

    fixed dir name format error extract_all_top.sh

--- extract_all_top.sh
+++ extract_all_top.sh
@@ -1,9 +1,9 @@
 experiment_dir=$1
 
 array=(pmd avrora jython fop sunflow antlr bloat luindex)
-
+#TODO edit proifile scripts to change kenan_bench dir format
 for i in "${array[@]}"
 do
-	bash extract_top.sh $1/$i
-	cp Package_top.csv $1/$i/top5.csv 
+	bash extract_top.sh $experiment_dir/"kenan_$i"
+	cp Package_top.csv $experiment_dir/"kenan_$i"/top5.csv 
 done
