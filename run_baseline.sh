3eb1dc0485850ffaf6bba83d6f1547b26c87fcc5
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-09-06 22:12:48 -0400

    baseline iters update

--- run_baseline.sh
+++ run_baseline.sh
@@ -4,13 +4,13 @@ iters=$2
 mkdir $expname
 
 sudo bash run_benchmark_baseline.sh sunflow new 10   $expname
-sudo bash run_benchmark_baseline.sh jython old 40 $expname
-sudo bash run_benchmark_baseline.sh pmd old 40  $expname
-sudo bash run_benchmark_baseline.sh fop old 40 $expname
+sudo bash run_benchmark_baseline.sh jython old $iters $expname
+sudo bash run_benchmark_baseline.sh pmd old $iters  $expname
+sudo bash run_benchmark_baseline.sh fop old $iters $expname
 # sudo bash run_benchmark_baseline.sh avrora new 40 $expname
-sudo bash run_benchmark_baseline.sh luindex new 40  $expname
-sudo bash run_benchmark_baseline.sh antlr old 40 $expname
-sudo bash run_benchmark_baseline.sh bloat old 40 $expname
+sudo bash run_benchmark_baseline.sh luindex new $iters  $expname
+sudo bash run_benchmark_baseline.sh antlr old $iters $expname
+sudo bash run_benchmark_baseline.sh bloat old $iters $expname
 
 mv *execution_time $expname
 mv *kenan_energy   $expname
