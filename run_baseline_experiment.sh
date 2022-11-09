257467f25d5d6b48aa683e9fae13e2e34e885303
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2022-11-09 12:04:12 +0900

    baseline proifling scripts

--- run_baseline_experiment.sh
+++ run_baseline_experiment.sh
@@ -8,14 +8,14 @@ iters=$2
 
 mkdir $expname
 
-sudo bash run_benchmark_baseline.sh sunflow new $iters   $expname
-sudo bash run_benchmark_baseline.sh jython old $iters $expname
-sudo bash run_benchmark_baseline.sh pmd old $iters  $expname
-sudo bash run_benchmark_baseline.sh fop old $iters $expname
-sudo bash run_benchmark_baseline.sh avrora new $iters $expname
-sudo bash run_benchmark_baseline.sh luindex new $iters  $expname
-sudo bash run_benchmark_baseline.sh antlr old $iters $expname
-sudo bash run_benchmark_baseline.sh bloat old $iters $expname
+# sudo bash vincent_scripts/baseline_profiling.sh sunflow new $iters   $expname
+# sudo bash vincent_scripts/baseline_profiling.sh jython old $iters $expname
+# sudo bash vincent_scripts/baseline_profiling.sh pmd old $iters  $expname
+# sudo bash vincent_scripts/baseline_profiling.sh fop old $iters $expname
+# sudo bash vincent_scripts/baseline_profiling.sh avrora new $iters $expname
+sudo bash vincent_scripts/baseline_profiling.sh luindex new $iters  $expname
+# sudo bash vincent_scripts/baseline_profiling.sh antlr old $iters $expname
+# sudo bash vincent_scripts/baseline_profiling.sh bloat old $iters $expname
 
 mv *execution_time $expname
 mv *kenan_energy   $expname
